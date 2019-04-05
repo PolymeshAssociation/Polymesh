571d918a0db47f442e8a4f32f1f8b804dfa744d3
Pablo Ruiz <pabloruiz55@gmail.com>
2019-04-05 20:19:24 -0300

    multi erc20 asset implementation

--- polymesh_substrate/runtime/src/asset.rs
+++ polymesh_substrate/runtime/src/asset.rs
@@ -10,10 +10,9 @@
 
 use rstd::prelude::*;
 use parity_codec::Codec;
-use runtime_io;
-use runtime_primitives::traits::{As, Member,SimpleArithmetic, CheckedSub, CheckedAdd, CheckedDiv, CheckedMul, Hash};
-use support::{Parameter,decl_module, decl_storage, decl_event, StorageMap, StorageValue, dispatch::Result, ensure};
-use system::ensure_signed;
+use support::{dispatch::Result, Parameter, StorageMap, StorageValue, decl_storage, decl_module, decl_event, ensure};
+use runtime_primitives::traits::{CheckedSub, CheckedAdd, Member, SimpleArithmetic, As};
+use system::{self, ensure_signed};
 
 /// The module's configuration trait.
 pub trait Trait: system::Trait {
@@ -24,74 +23,113 @@ pub trait Trait: system::Trait {
     type TokenBalance: Parameter + Member + SimpleArithmetic + Codec + Default + Copy + As<usize> + As<u64>;
 }
 
+// struct to store the token details
+#[derive(parity_codec::Encode, parity_codec::Decode, Default, Clone, PartialEq, Debug)]
+pub struct Erc20Token<U> {
+    name: Vec<u8>,
+    ticker: Vec<u8>,
+    total_supply: U,
+}
+
 /// This module's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as Asset {
-        Init get(is_init): bool;
-       
-        Symbol: Vec<u8>;
-        // total supply of the token
-        //TotalSupply get(total_supply): T::TokenBalance;
-        TotalSupply get(total_supply): T::TokenBalance;
-        // mapping of balances to accounts
-        //BalanceOf get(balance_of): map T::AccountId => T::TokenBalance;
-        BalanceOf get(balance_of): map T::AccountId => T::TokenBalance;
+      // token id nonce for storing the next token id available for token initialization
+      // inspired by the AssetId in the SRML assets module
+      TokenId get(token_id): u32;
+      // details of the token corresponding to a token id
+      Tokens get(token_details): map u32 => Erc20Token<T::TokenBalance>;
+      // balances mapping for an account and token
+      BalanceOf get(balance_of): map (u32, T::AccountId) => T::TokenBalance;
+      // allowance for an account and token
+      Allowance get(allowance): map (u32, T::AccountId, T::AccountId) => T::TokenBalance;
     }
 }
 
+// public interface for this runtime module
 decl_module! {
-	/// The module declaration.
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Initializing events
-		// this is needed only if you are using events in your module
-		fn deposit_event<T>() = default;
+  pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+      // initialize the default event for this module
+      fn deposit_event<T>() = default;
 
-        // initialize the token
-        // transfers the total_supply amout to the caller
-        // the token becomes usable
-        // not part of ERC20 standard interface
-        // similar to the ERC20 smart contract constructor
-        pub fn init(origin, sender: T::AccountId, initial_supply: T::TokenBalance) -> Result {
-            let _sender = ensure_signed(origin)?;
-            ensure!(Self::is_init() == false, "Token already initialized.");
+      // initializes a new token
+      // generates an integer token_id so that all tokens are unique
+      // takes a name, ticker, total supply for the token
+      // makes the initiating account the owner of the token
+      // the balance of the owner is set to total supply
+      fn init(origin, name: Vec<u8>, ticker: Vec<u8>, total_supply: T::TokenBalance) -> Result {
+          let sender = ensure_signed(origin)?;
 
-            <BalanceOf<T>>::insert(sender, initial_supply);
-            <Init<T>>::put(true);
-            <TotalSupply<T>>::put(initial_supply);
+          // checking max size for name and ticker
+          // byte arrays (vecs) with no max size should be avoided
+          ensure!(name.len() <= 64, "token name cannot exceed 64 bytes");
+          ensure!(ticker.len() <= 32, "token ticker cannot exceed 32 bytes");
 
-            runtime_io::print("Initialized!!!");
+          let token_id = Self::token_id();
+          let next_token_id = token_id.checked_add(1).ok_or("overflow in calculating next token id")?;
+          <TokenId<T>>::put(next_token_id);
 
-            Ok(())
-        }
+          let token = Erc20Token {
+              name,
+              ticker,
+              total_supply,
+          };
 
-        // transfer tokens from one account to another
-        pub fn transfer(origin, to: T::AccountId, #[compact] value: T::TokenBalance) -> Result {
-            let _sender = ensure_signed(origin)?;
-            Self::_transfer(_sender, to, value)
-        }
+          <Tokens<T>>::insert(token_id, token);
+          <BalanceOf<T>>::insert((token_id, sender), total_supply);
 
-        pub fn set_symbol(origin, value: Vec<u8>) -> Result {
-            let _sender = ensure_signed(origin)?;
+          Ok(())
+      }
 
-            <Symbol<T>>::put(value);
+      // transfer tokens from one account to another
+      // origin is assumed as sender
+      fn transfer(_origin, token_id: u32, to: T::AccountId, value: T::TokenBalance) -> Result {
+          let sender = ensure_signed(_origin)?;
+          let validTransfer = Self::_isValidTransfer(token_id, sender.clone(), to.clone(), value);
+          Self::_transfer(token_id, sender, to, value)
+      }
 
-            // some bytes, in a vector
-            let polymesh = vec![80, 111, 108, 121, 109, 101, 115, 104];
-            <Symbol<T>>::put(polymesh);
+      // approve token transfer from one account to another
+      // once this is done, transfer_from can be called with corresponding values
+      fn approve(_origin, token_id: u32, spender: T::AccountId, value: T::TokenBalance) -> Result {
+          let sender = ensure_signed(_origin)?;
+          ensure!(<BalanceOf<T>>::exists((token_id, sender.clone())), "Account does not own this token");
 
-            Ok(())
-        }   
-	}
+          let allowance = Self::allowance((token_id, sender.clone(), spender.clone()));
+          let updated_allowance = allowance.checked_add(&value).ok_or("overflow in calculating allowance")?;
+          <Allowance<T>>::insert((token_id, sender.clone(), spender.clone()), updated_allowance);
+
+          Self::deposit_event(RawEvent::Approval(token_id, sender.clone(), spender.clone(), value));
+
+          Ok(())
+      }
+
+      // the ERC20 standard transfer_from function
+      // implemented in the open-zeppelin way - increase/decrease allownace
+      // if approved, transfer from an account to another account without owner's signature
+      pub fn transfer_from(_origin, token_id: u32, from: T::AccountId, to: T::AccountId, value: T::TokenBalance) -> Result {
+        ensure!(<Allowance<T>>::exists((token_id, from.clone(), to.clone())), "Allowance does not exist.");
+        let allowance = Self::allowance((token_id, from.clone(), to.clone()));
+        ensure!(allowance >= value, "Not enough allowance.");
+          
+        // using checked_sub (safe math) to avoid overflow
+        let updated_allowance = allowance.checked_sub(&value).ok_or("overflow in calculating allowance")?;
+        <Allowance<T>>::insert((token_id, from.clone(), to.clone()), updated_allowance);
+
+        Self::deposit_event(RawEvent::Approval(token_id, from.clone(), to.clone(), value));
+        Self::_transfer(token_id, from, to, value)
+      }
+  }
 }
 
 decl_event!(
-    pub enum Event<T> where AccountId = <T as system::Trait>::AccountId, TokenBalance = <T as self::Trait>::TokenBalance {
+    pub enum Event<T> where AccountId = <T as system::Trait>::AccountId, Balance = <T as self::Trait>::TokenBalance {
         // event for transfer of tokens
-        // from, to, value
-        Transfer(AccountId, AccountId, TokenBalance),
+        // tokenid, from, to, value
+        Transfer(u32, AccountId, AccountId, Balance),
         // event when an approval is made
-        // owner, spender, value
-        //Approval(AccountId, AccountId, TokenBalance),
+        // tokenid, owner, spender, value
+        Approval(u32, AccountId, AccountId, Balance),
     }
 );
 
@@ -103,26 +141,38 @@ decl_event!(
 /// All functions in the impl module section are not part of public interface because they are not part of the Call enum
 impl<T: Trait> Module<T> {
 
-    // internal transfer function for ERC20 interface
-    fn _transfer(
+    fn _isValidTransfer(
+        token_id: u32,
         from: T::AccountId,
         to: T::AccountId,
         value: T::TokenBalance,
     ) -> Result {
-        ensure!(<BalanceOf<T>>::exists(from.clone()), "Account does not own this token");
-        let sender_balance = Self::balance_of(from.clone());
+        Ok(())
+    }
+
+    // the ERC20 standard transfer function
+    // internal
+    fn _transfer(
+        token_id: u32,
+        from: T::AccountId,
+        to: T::AccountId,
+        value: T::TokenBalance,
+    ) -> Result {
+        ensure!(<BalanceOf<T>>::exists((token_id, from.clone())), "Account does not own this token");
+        let sender_balance = Self::balance_of((token_id, from.clone()));
         ensure!(sender_balance >= value, "Not enough balance.");
+
         let updated_from_balance = sender_balance.checked_sub(&value).ok_or("overflow in calculating balance")?;
-        let receiver_balance = Self::balance_of(to.clone());
+        let receiver_balance = Self::balance_of((token_id, to.clone()));
         let updated_to_balance = receiver_balance.checked_add(&value).ok_or("overflow in calculating balance")?;
         
         // reduce sender's balance
-        <BalanceOf<T>>::insert(from.clone(), updated_from_balance);
+        <BalanceOf<T>>::insert((token_id, from.clone()), updated_from_balance);
 
         // increase receiver's balance
-        <BalanceOf<T>>::insert(to.clone(), updated_to_balance);
+        <BalanceOf<T>>::insert((token_id, to.clone()), updated_to_balance);
 
-        Self::deposit_event(RawEvent::Transfer(from, to, value));
+        Self::deposit_event(RawEvent::Transfer(token_id, from, to, value));
         Ok(())
     }
 }
