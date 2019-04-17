4178b45a661a5695884efa68598c565ae89c5da3
Stan Drozd <drozdziak1@gmail.com>
2019-04-17 17:43:23 +0200

    Improve hook init and fix format checking
    This commit makes sure init_hooks.sh runs in repo root and fixes the
    runtime crate's formatting issue.

--- scripts/pre-commit-substrate.sh
+++ scripts/pre-commit-substrate.sh
@@ -5,12 +5,11 @@ set -e
 
 worktree=${GIT_WORK_TREE:=$PWD}
 
+# rustfmt all top-level, non-artifact `src` dirs, all of *.rs inside
+find polymesh_substrate -type d -name "src" -not -path "*/target/*" | xargs -i find {} -type f -name "*.rs" | xargs rustfmt --check
+
 pushd $GIT_WORK_TREE/polymesh_substrate
 	# Do linting, has to account for wasm gimmicks
 	./check.sh
 	echo cargo-check OK
-
-	# Check formatting
-	cargo fmt --all -- --check
-	echo cargo-fmt OK
 popd
