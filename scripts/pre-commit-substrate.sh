224796a319d3f0692243c660ea49b8a4aa592c38
Stan Drozd <drozdziak1@gmail.com>
2019-04-17 17:53:13 +0200

    Move formatting on to nightly

--- scripts/pre-commit-substrate.sh
+++ scripts/pre-commit-substrate.sh
@@ -6,7 +6,7 @@ set -e
 worktree=${GIT_WORK_TREE:=$PWD}
 
 # rustfmt all top-level, non-artifact `src` dirs, all of *.rs inside
-find polymesh_substrate -type d -name "src" -not -path "*/target/*" | xargs -i find {} -type f -name "*.rs" | xargs rustfmt --check
+find polymesh_substrate -type d -name "src" -not -path "*/target/*" | xargs -i find {} -type f -name "*.rs" | xargs rustfmt +nightly --check
 
 pushd $GIT_WORK_TREE/polymesh_substrate
 	# Do linting, has to account for wasm gimmicks
