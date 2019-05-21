b687b65649fac67dbfd1401e5ed8a575a5393a8d
Stan Drozd <drozdziak1@gmail.com>
2019-05-21 10:38:25 +0200

    scripts/: Add a cargo-test step for the pre-commit hook

---
+++ scripts/cargo-test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+script_dir=$(dirname $0)
+pushd $script_dir/../polymesh_substrate/runtime 1>/dev/null
+	if [ -z "${VERBOSE-}" ] ; then
+		cargo test || (echo "cargo-test FAIL" && false)
+		echo cargo-test OK
+	else # rustfmt output not suppresed
+		cargo test -- --nocapture || (echo "cargo-test FAIL" && false)
+		echo cargo-test OK
+	fi
+popd 1>/dev/null
