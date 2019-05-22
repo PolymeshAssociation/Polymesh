63cc1547d7af0694956268d2aeccada8242e1bd6
Stan Drozd <drozdziak1@gmail.com>
2019-05-22 10:30:23 +0200

    Comment all runtime module tests

--- polymesh_substrate/runtime/src/asset.rs
+++ polymesh_substrate/runtime/src/asset.rs
@@ -474,62 +474,64 @@ impl<T: Trait> Module<T> {
 /// tests for this module
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    use primitives::{Blake2Hasher, H256};
-    use runtime_io::with_externalities;
-    use runtime_primitives::{
-        testing::{Digest, DigestItem, Header},
-        traits::{BlakeTwo256, IdentityLookup},
-        BuildStorage,
-    };
-    use support::{assert_ok, impl_outer_origin};
-
-    impl_outer_origin! {
-        pub enum Origin for Test {}
-    }
-
-    // For testing the module, we construct most of a mock runtime. This means
-    // first constructing a configuration type (`Test`) which `impl`s each of the
-    // configuration traits of modules we want to use.
-    #[derive(Clone, Eq, PartialEq)]
-    pub struct Test;
-    impl system::Trait for Test {
-        type Origin = Origin;
-        type Index = u64;
-        type BlockNumber = u64;
-        type Hash = H256;
-        type Hashing = BlakeTwo256;
-        type Digest = Digest;
-        type AccountId = u64;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Header = Header;
-        type Event = ();
-        type Log = DigestItem;
-    }
-    impl Trait for Test {
-        type Event = ();
-    }
-    type asset = Module<Test>;
-
-    // This function basically just builds a genesis storage key/value store according to
-    // our desired mockup.
-    fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
-        system::GenesisConfig::<Test>::default()
-            .build_storage()
-            .unwrap()
-            .0
-            .into()
-    }
-
-    //#[test]
-    // fn it_works_for_default_value() {
-    // 	with_externalities(&mut new_test_ext(), || {
-    // 		// Just a dummy test for the dummy funtion `do_something`
-    // 		// calling the `do_something` function with a value 42
-    // 		assert_ok!(asset::do_something(Origin::signed(1), 42));
-    // 		// asserting that the stored value is equal to what we stored
-    // 		assert_eq!(asset::something(), Some(42));
-    // 	});
-    // }
+    /*
+     *    use super::*;
+     *
+     *    use primitives::{Blake2Hasher, H256};
+     *    use runtime_io::with_externalities;
+     *    use runtime_primitives::{
+     *        testing::{Digest, DigestItem, Header},
+     *        traits::{BlakeTwo256, IdentityLookup},
+     *        BuildStorage,
+     *    };
+     *    use support::{assert_ok, impl_outer_origin};
+     *
+     *    impl_outer_origin! {
+     *        pub enum Origin for Test {}
+     *    }
+     *
+     *    // For testing the module, we construct most of a mock runtime. This means
+     *    // first constructing a configuration type (`Test`) which `impl`s each of the
+     *    // configuration traits of modules we want to use.
+     *    #[derive(Clone, Eq, PartialEq)]
+     *    pub struct Test;
+     *    impl system::Trait for Test {
+     *        type Origin = Origin;
+     *        type Index = u64;
+     *        type BlockNumber = u64;
+     *        type Hash = H256;
+     *        type Hashing = BlakeTwo256;
+     *        type Digest = Digest;
+     *        type AccountId = u64;
+     *        type Lookup = IdentityLookup<Self::AccountId>;
+     *        type Header = Header;
+     *        type Event = ();
+     *        type Log = DigestItem;
+     *    }
+     *    impl Trait for Test {
+     *        type Event = ();
+     *    }
+     *    type asset = Module<Test>;
+     *
+     *    // This function basically just builds a genesis storage key/value store according to
+     *    // our desired mockup.
+     *    fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+     *        system::GenesisConfig::<Test>::default()
+     *            .build_storage()
+     *            .unwrap()
+     *            .0
+     *            .into()
+     *    }
+     *
+     *    //#[test]
+     *    //fn it_works_for_default_value() {
+     *        //with_externalities(&mut new_test_ext(), || {
+     *            //// Just a dummy test for the dummy funtion `do_something`
+     *            //// calling the `do_something` function with a value 42
+     *            //assert_ok!(asset::do_something(Origin::signed(1), 42));
+     *            //// asserting that the stored value is equal to what we stored
+     *            //assert_eq!(asset::something(), Some(42));
+     *        //});
+     *    //}
+     */
 }
