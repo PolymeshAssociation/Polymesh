7a83350532547a8f9d0d611e5ef57800d221c116
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-01-22 11:05:30 +0000

    the ticker module

---
+++ primitives/src/ticker.rs
@@ -0,0 +1,37 @@
+//! Ticker symbol
+use codec::{Decode, Encode};
+
+const TICKER_LEN: usize = 12;
+
+/// Ticker symbol identified with a name.
+#[derive(Decode, Encode, Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Ticker(pub [u8; TICKER_LEN]);
+
+impl Default for Ticker {
+    fn default() -> Self {
+        Ticker([b'?'; TICKER_LEN])
+    }
+}
+
+impl Ticker {
+    /// Converts a byte slice to an uppercase ASCII ticker, trimming the result to 12 bytes.
+    pub fn from_slice(s: &[u8]) -> Self {
+        let mut ticker = [0u8; TICKER_LEN];
+        for (i, c) in s
+            .iter()
+            .map(|c| c.to_ascii_uppercase())
+            .enumerate()
+            .take(TICKER_LEN)
+        {
+            ticker[i] = c;
+        }
+        Ticker(ticker)
+    }
+
+    /// Converts the ticker to canonical uppercase ASCII notation.
+    pub fn canonize(&mut self) {
+        for i in 0..TICKER_LEN {
+            self.0[i] = self.0[i].to_ascii_uppercase();
+        }
+    }
+}
