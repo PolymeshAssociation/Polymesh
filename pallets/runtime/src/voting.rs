6045cdf259c603637dfe2a4b4dee7895b7717047
Vijay Kandy <vkandy@gmail.com>
2020-02-07 11:03:22 -0700

    Renamed KYCServiceProviders as KycServiceProviders

--- pallets/runtime/src/voting.rs
+++ pallets/runtime/src/voting.rs
@@ -538,7 +538,7 @@ mod tests {
         type Event = ();
         type Proposal = Call<Test>;
         type AddSignerMultiSigTarget = Test;
-        type KYCServiceProviders = Test;
+        type KycServiceProviders = Test;
         type Balances = balances::Module<Test>;
     }
 
