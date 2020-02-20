ca4ec8f0882ff586b24d2c0513469b677ba5b9b6
Mudit Gupta <guptamudit@ymail.com>
2020-02-20 22:47:55 +0530

    increased-block-time (#219)

--- pallets/common/src/constants.rs
+++ pallets/common/src/constants.rs
@@ -16,12 +16,12 @@ pub mod time {
     // mainnet
     // pub const MILLISECS_PER_BLOCK: Moment = 6000;
     // Testnet
-    pub const MILLISECS_PER_BLOCK: Moment = 1000;
+    pub const MILLISECS_PER_BLOCK: Moment = 5000;
     pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
     // mainnet
     // pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 4 * HOURS;
     // Testnet
-    pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 1 * MINUTES;
+    pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 30 * MINUTES;
 
     // These time units are defined in number of blocks.
     pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
