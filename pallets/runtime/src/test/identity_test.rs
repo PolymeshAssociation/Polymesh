9bd2dbc03a9b438da583aeb8b3d425c614fdf552
Mudit Gupta <guptamudit@ymail.com>
2020-03-03 15:31:44 +0530

    CDD requirement for master key rotation optional

--- pallets/runtime/src/test/identity_test.rs
+++ pallets/runtime/src/test/identity_test.rs
@@ -840,8 +840,50 @@ fn changing_master_key_we() {
     let new_key = AccountKey::from(AccountKeyring::Bob.public().0);
     let new_key_origin = Origin::signed(AccountKeyring::Bob.public());
 
-    let kyc_acc = AccountKey::from(AccountKeyring::Eve.public().0);
-    let kyc_did = Identity::get_identity(&kyc_acc).unwrap();
+    // Master key matches Alice's key
+    assert_eq!(Identity::did_records(alice_did).master_key, alice_key);
+
+    // Alice triggers change of master key
+    let owner_auth_id = Identity::add_auth(
+        Signatory::AccountKey(alice_key),
+        Signatory::AccountKey(new_key),
+        AuthorizationData::RotateMasterKey(alice_did),
+        None,
+    );
+
+    // Accept the authorization with the new key
+    assert_ok!(Identity::accept_master_key(
+        new_key_origin.clone(),
+        owner_auth_id.clone(),
+        None
+    ));
+
+    // Alice's master key is now Bob's
+    assert_eq!(
+        Identity::did_records(alice_did).master_key,
+        AccountKey::from(AccountKeyring::Bob.public().0)
+    );
+}
+
+#[test]
+fn changing_master_key_with_cdd_auth() {
+    ExtBuilder::default()
+        .monied(true)
+        .cdd_providers(vec![AccountKeyring::Eve.public()])
+        .build()
+        .execute_with(|| changing_master_key_with_cdd_auth_we());
+}
+
+fn changing_master_key_with_cdd_auth_we() {
+    let alice_did = register_keyring_account(AccountKeyring::Alice).unwrap();
+    let alice_key = AccountKey::from(AccountKeyring::Alice.public().0);
+
+    let _target_did = register_keyring_account(AccountKeyring::Bob).unwrap();
+    let new_key = AccountKey::from(AccountKeyring::Bob.public().0);
+    let new_key_origin = Origin::signed(AccountKeyring::Bob.public());
+
+    let cdd_acc = AccountKey::from(AccountKeyring::Eve.public().0);
+    let cdd_did = Identity::get_identity(&cdd_acc).unwrap();
 
     // Master key matches Alice's key
     assert_eq!(Identity::did_records(alice_did).master_key, alice_key);
@@ -854,18 +896,24 @@ fn changing_master_key_we() {
         None,
     );
 
-    let kyc_auth_id = Identity::add_auth(
-        Signatory::Identity(kyc_did),
+    let cdd_auth_id = Identity::add_auth(
+        Signatory::Identity(cdd_did),
         Signatory::AccountKey(new_key),
         AuthorizationData::AttestMasterKeyRotation(alice_did),
         None,
+    );
+
+    Identity::change_cdd_requirement_for_mk_rotation(frame_system::RawOrigin::Root.into(), true);
+
+    assert!(
+        Identity::accept_master_key(new_key_origin.clone(), owner_auth_id.clone(), None).is_err()
     );
 
     // Accept the authorization with the new key
     assert_ok!(Identity::accept_master_key(
         new_key_origin.clone(),
         owner_auth_id.clone(),
-        kyc_auth_id.clone()
+        Some(cdd_auth_id)
     ));
 
     // Alice's master key is now Bob's
@@ -875,7 +923,6 @@ fn changing_master_key_we() {
     );
 }
 
-#[test]
 fn cdd_register_did_test() {
     ExtBuilder::default()
         .existential_deposit(1_000)
