529b7dcf97c32490fbb2b7dd3500ff44e7ef708c
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-03-12 07:02:59 +0100

    Fix Staking UT

--- pallets/staking/src/mock.rs
+++ pallets/staking/src/mock.rs
@@ -700,6 +700,7 @@ pub fn add_nominator_claim(
         signed_claim_issuer_id,
         idendity_id,
         Claim::CustomerDueDiligence,
+        None,
         Some((now.timestamp() as u64 + 10000_u64).into()),
     ));
 }
@@ -716,6 +717,7 @@ pub fn add_nominator_claim_with_expiry(
         signed_claim_issuer_id,
         idendity_id,
         Claim::CustomerDueDiligence,
+        None,
         Some(expiry.into()),
     ));
 }
