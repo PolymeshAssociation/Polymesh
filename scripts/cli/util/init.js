5afc281316e4686f3f1586e51e9d02b8b433186a
Mudit Gupta <guptamudit@ymail.com>
2020-03-18 13:22:44 +0530

    cli fix attempt 2

--- scripts/cli/util/init.js
+++ scripts/cli/util/init.js
@@ -42,11 +42,6 @@ let n_claims = opts.claims;
 let prepend = opts.prepend;
 let fast = opts.fast;
 
-let keyring;
-cryptoWaitReady().then(() => {
-  keyring = new Keyring({ type: "sr25519" });
-});
-
 // Initialization Main is used to generate all entities e.g (Alice, Bob, Dave)
 async function initMain(api) {
   let entities = [];
@@ -60,7 +55,8 @@ async function initMain(api) {
 
 let generateEntity = async function(api, name) {
   let entity = [];
-  entity = keyring.addFromUri(`//${name}`, { name: `${name}` });
+  await cryptoWaitReady();
+  entity = new Keyring({ type: "sr25519" }).addFromUri(`//${name}`, { name: `${name}` });
   let entityRawNonce = await api.query.system.accountNonce(entity.address);
   let entity_nonce = new BN(entityRawNonce.toString());
   nonces.set(entity.address, entity_nonce);
@@ -71,9 +67,10 @@ let generateEntity = async function(api, name) {
 
 const generateKeys = async function(api, numberOfKeys, keyPrepend) {
   let keys = [];
+  await cryptoWaitReady();
   for (let i = 0; i < numberOfKeys; i++) {
     keys.push(
-      keyring.addFromUri("//" + keyPrepend + i.toString(), {
+      new Keyring({ type: "sr25519" }).addFromUri("//" + keyPrepend + i.toString(), {
         name: i.toString()
       })
     );
