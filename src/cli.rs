9d131f1b80d43b8e2a6b153362cd93b2bb38e217
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-04-16 09:06:44 +0100

    Mesh 785: extrinsic benchmarks (#301)
    * added the benchmarking cli option
    
    * fmt
    
    * removed redundant pragma
    
    * gated benchmark CLI option
    
    * corrected the version of pallet-session
    
    * register_ticker and accept_ticker_transfer benches
    
    * asset benches + WIP on identity benches
    
    * removed optional flag from runtime-benchmarking feature in identity pallet
    
    * removed conflicting node-executor dependency and added add_claims bench
    
    * cargo feature runtime-benchmarks works
    
    * added linear regression analysis to benchmark output
    
    * Fix merge error
    
    * Update lib.rs
    
    Co-authored-by: Adam Dossa <adam.dossa@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,26 @@
-use sc_cli::{RunCmd, Subcommand};
+#[cfg(feature = "runtime-benchmarks")]
+use crate::benchmarking_cli;
+use sc_cli::RunCmd;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Clone, Debug, StructOpt)]
 pub struct Cli {
+    /// Possible subcommand with parameters.
     #[structopt(subcommand)]
     pub subcommand: Option<Subcommand>,
-
+    #[allow(missing_docs)]
     #[structopt(flatten)]
     pub run: RunCmd,
+}
+
+/// Possible subcommands of the main binary.
+#[derive(Clone, Debug, StructOpt)]
+pub enum Subcommand {
+    /// A set of base subcommands handled by `sc_cli`.
+    #[structopt(flatten)]
+    Base(sc_cli::Subcommand),
+    /// The custom benchmark subcommmand benchmarking runtime pallets.
+    #[cfg(feature = "runtime-benchmarks")]
+    #[structopt(name = "benchmark", about = "Benchmark runtime pallets.")]
+    Benchmark(benchmarking_cli::BenchmarkCmd),
 }
