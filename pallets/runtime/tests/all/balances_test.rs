9439e069d76470d4d0f4eef1a1e92c8a9b6e227d
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-04-20 16:38:44 +0100

    burn_free_balance test

--- pallets/runtime/tests/all/balances_test.rs
+++ pallets/runtime/tests/all/balances_test.rs
@@ -212,6 +212,36 @@ fn issue_must_work() {
 }
 
 #[test]
+fn burn_free_balance_works() {
+    ExtBuilder::default().monied(true).build().execute_with(|| {
+        let alice_pub = AccountKeyring::Alice.public();
+        let _ = make_account(alice_pub).unwrap();
+        let total_issuance0 = Balances::total_issuance();
+        let alice_free_balance0 = Balances::free_balance(&alice_pub);
+        let burn_amount = 1_000;
+        assert_ok!(Balances::burn_free_balance(
+            Origin::signed(alice_pub),
+            burn_amount
+        ));
+        let alice_free_balance1 = Balances::free_balance(&alice_pub);
+        assert_eq!(alice_free_balance1, alice_free_balance0 - burn_amount);
+        let total_issuance1 = Balances::total_issuance();
+        assert_eq!(total_issuance1, total_issuance0 - burn_amount);
+        let fat_finger_burn_amount = u128::MAX;
+        assert_ok!(Balances::burn_free_balance(
+            Origin::signed(alice_pub),
+            fat_finger_burn_amount
+        ));
+        let alice_free_balance2 = Balances::free_balance(&alice_pub);
+        // The entire Alice's free balance is burned.
+        assert_eq!(alice_free_balance2, 0);
+        let total_issuance2 = Balances::total_issuance();
+        // Only the available Alice's free balance is burned.
+        assert_eq!(total_issuance2, total_issuance0 - alice_free_balance0);
+    });
+}
+
+#[test]
 #[ignore]
 fn should_charge_identity() {
     ExtBuilder::default()
