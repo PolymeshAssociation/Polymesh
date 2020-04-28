1c003304b3e357a33c457a1c80b289e7b1aabd33
Satyam Agrawal <satyam0499@gmail.com>
2020-04-28 19:59:33 +0530

    Mesh 893/Add cdd check to nominators (#322)
    * Tests structure updated (#308)
    
    * test structure updated
    
    * removed lints
    
    * Fix/link multisig to identity (#305)
    
    * Attach multisig to creator identity
    
    * Updated tests script
    
    * test structure updated
    
    * removed lints
    
    * renamed a variable
    
    * add cdd validation for nominators in select_validators
    
    * add cdd check for nominators
    
    * fix linting
    
    * minor fix in test
    
    Co-authored-by: Mudit Gupta <guptamudit@ymail.com>
    Co-authored-by: satyam <satyam@secureblocks.io>
    Co-authored-by: Adam Dossa <adam.dossa@gmail.com>

--- pallets/staking/src/mock.rs
+++ pallets/staking/src/mock.rs
@@ -849,6 +849,22 @@ pub fn bond_nominator(acc: u64, val: u128, target: Vec<AccountId>) {
     assert_ok!(Staking::nominate(Origin::signed(controller), target));
 }
 
+pub fn bond_nominator_with_expiry(acc: u64, val: u128, claim_expiry: u64, target: Vec<AccountId>) {
+    // a = controller
+    // a + 1 = stash
+    let controller = account_from(acc);
+    let stash = account_from(acc + 1);
+    let _ = Balances::make_free_balance_be(&(stash), val);
+    assert_ok!(Staking::bond(
+        Origin::signed(stash),
+        controller,
+        val,
+        RewardDestination::Controller
+    ));
+    create_did_and_add_claim_with_expiry(stash, claim_expiry);
+    assert_ok!(Staking::nominate(Origin::signed(controller), target));
+}
+
 pub fn add_nominator_claim(
     claim_issuer: IdentityId,
     idendity_id: IdentityId,
@@ -906,6 +922,16 @@ pub fn create_did_and_add_claim(stash: AccountId) {
         Origin::signed(account_from(1005)),
         stash,
         None,
+        vec![]
+    ));
+}
+
+pub fn create_did_and_add_claim_with_expiry(stash: AccountId, expiry: u64) {
+    Balances::make_free_balance_be(&account_from(1005), 1_000_000);
+    assert_ok!(Identity::cdd_register_did(
+        Origin::signed(account_from(1005)),
+        stash,
+        Some(expiry.into()),
         vec![]
     ));
 }
