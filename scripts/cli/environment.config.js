e88720612a3673a14ed9e5be0e10218b30dc7615
Adam Dossa <adam.dossa@gmail.com>
2020-05-07 23:46:01 +0100

    MESH-861 - More configuration updates (#362)
    * WIP
    
    * update
    
    * wip
    
    * Add telemetry
    
    * wip
    
    * wip
    
    * wip
    
    * Update live.config.js
    
    * Update configs
    
    * Fix tests
    
    * Fix test
    
    * Fix balances test
    
    * Update fix
    
    * Update network name
    
    * Fix cli tests
    
    * Update scripts/cli/log.sh
    
    Co-authored-by: Mudit Gupta <guptamudit@ymail.com>
    
    * Update scripts/cli/run.sh
    
    Co-authored-by: Mudit Gupta <guptamudit@ymail.com>
    
    * Update scripts/cli/stop.sh
    
    Co-authored-by: Mudit Gupta <guptamudit@ymail.com>
    
    * Update chain_spec.rs
    
    * Update chain_spec.rs
    
    * Update lib.rs
    
    Co-authored-by: Mudit Gupta <guptamudit@ymail.com>

--- scripts/cli/environment.config.js
+++ scripts/cli/environment.config.js
@@ -1,11 +1,12 @@
 // PM2 config file
+let chain = "v1-local";
 module.exports = {
   apps: [
     {
       name: "pmesh-primary-node",
       script:
         "../../target/release/polymesh",
-      args: "-d /tmp/pmesh-primary-node --pool-limit 100000 --ws-port 9944 --alice --validator --chain local --force-authoring",
+      args: "-d /tmp/pmesh-primary-node --pool-limit 100000 --ws-port 9944 --alice --validator --chain " + chain + " --force-authoring",
       env: {
         RUST_BACKTRACE: "1",
       }
@@ -14,7 +15,7 @@ module.exports = {
       name: "pmesh-peer-node-1",
       script:
         "../../target/release/polymesh",
-      args: "-d /tmp/pmesh-peer-node-1 --ws-port 9945 --bob --validator --chain local --force-authoring",
+      args: "-d /tmp/pmesh-peer-node-1 --ws-port 9945 --bob --validator --chain " + chain + " --force-authoring",
       env: {
         RUST_BACKTRACE: "1"
       }
@@ -23,7 +24,25 @@ module.exports = {
       name: "pmesh-peer-node-2",
       script:
         "../../target/release/polymesh",
-      args: "-d /tmp/pmesh-peer-node-2 --ws-port 9946 --charlie --validator --chain local --force-authoring",
+      args: "-d /tmp/pmesh-peer-node-2 --ws-port 9946 --charlie --validator --chain " + chain + " --force-authoring",
+      env: {
+        RUST_BACKTRACE: "1"
+      }
+    },
+    {
+      name: "pmesh-peer-node-3",
+      script:
+        "../../target/release/polymesh",
+      args: "-d /tmp/pmesh-peer-node-3 --ws-port 9947 --dave --validator --chain " + chain + " --force-authoring",
+      env: {
+        RUST_BACKTRACE: "1"
+      }
+    },
+    {
+      name: "pmesh-peer-node-4",
+      script:
+        "../../target/release/polymesh",
+      args: "-d /tmp/pmesh-peer-node-4 --ws-port 9948 --eve --validator --chain " + chain + " --force-authoring",
       env: {
         RUST_BACKTRACE: "1"
       }
