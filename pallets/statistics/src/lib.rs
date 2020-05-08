e6c71ff57fe5b210a199b5aae4522609a7e2f391
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-05-08 12:48:46 +0200

    License added

--- pallets/statistics/src/lib.rs
+++ pallets/statistics/src/lib.rs
@@ -1,6 +1,22 @@
-// Copyright 2020 Polymath Inc
+// This file is part of the Polymesh distribution (https://github.com/PolymathNetwork/Polymesh).
+// Copyright (c) 2020 Polymath
 //
-// This module centralizes any statistic information of any element of the system.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
+//! # Statistics Module
+//!
+//! This module centralizes any statistic information of any element of the system.
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use polymesh_common_utilities::balances::Trait as BalancesTrait;
