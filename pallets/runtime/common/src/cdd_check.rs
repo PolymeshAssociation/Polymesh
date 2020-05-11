3ebcd89f8168676e4118038724dd260db59c22be
Mudit Gupta <guptamudit@ymail.com>
2020-05-12 05:17:50 +0530

    Mesh 982/multisig did (#378)
    * refactored multisig did cdd/did
    
    * Updated tests
    
    Co-authored-by: Adam Dossa <adam.dossa@gmail.com>

--- pallets/runtime/common/src/cdd_check.rs
+++ pallets/runtime/common/src/cdd_check.rs
@@ -18,10 +18,6 @@ use pallet_multisig as multisig;
 use polymesh_common_utilities::traits::{balances::CheckCdd, identity::Trait as IdentityTrait};
 use polymesh_primitives::{AccountKey, IdentityId};
 
-use codec::Encode;
-use core::convert::TryFrom;
-use frame_support::StorageMap;
-
 pub struct CddChecker<R>(sp_std::marker::PhantomData<R>);
 
 impl<R> CheckCdd for CddChecker<R>
@@ -36,18 +32,6 @@ where
         if let Some(did) = identity::Module::<R>::get_identity(&key) {
             if identity::Module::<R>::has_valid_cdd(did) {
                 return Some(did);
-            }
-        }
-        // An account that is NOT a signing key or the master key of an Identity
-        // but is a signer of a multisig that is a signing/master key of an Identity
-        if <multisig::KeyToMultiSig<R>>::contains_key(&key) {
-            let ms = <multisig::KeyToMultiSig<R>>::get(&key);
-            if let Ok(ms_key) = AccountKey::try_from(ms.encode()) {
-                if let Some(did) = identity::Module::<R>::get_identity(&ms_key) {
-                    if identity::Module::<R>::has_valid_cdd(did) {
-                        return Some(did);
-                    }
-                }
             }
         }
         return None;
