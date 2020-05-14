375f88c360dceecb4b50843b6a347274c2dd929c
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-05-14 13:53:28 +0200

    Lint fix

--- pallets/treasury/src/lib.rs
+++ pallets/treasury/src/lib.rs
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
 //! # Treasury Module
 //!
 //! Treasury module contains a couple of functions to manage the treasury through the governance
