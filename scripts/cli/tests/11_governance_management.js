5f162245d048787b8db1ee3843b4fe71b4102c30
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2020-05-27 17:50:11 -0400

    updated script and fixed test error

--- scripts/cli/tests/11_governance_management.js
+++ scripts/cli/tests/11_governance_management.js
@@ -4,10 +4,6 @@ module.exports = require("../util/init.js");
 
 let { reqImports } = require("../util/init.js");
 
-const testKeyring = require('@polkadot/keyring/testing');
-
-const keyring = testKeyring.default();
-
 // Sets the default exit code to fail unless the script runs successfully
 process.exitCode = 1;
 
@@ -15,8 +11,17 @@ async function main() {
   
   const api = await reqImports.createApi();
   
-  await proposePIP( api, keyring );
+  const testEntities = await reqImports.initMain(api);
   
+  let alice = testEntities[0];
+  let bob = testEntities[1];
+ 
+  await reqImports.createIdentities( api, [bob], alice );
+ 
+  await bondPoly(api, alice, bob);
+
+  await proposePIP( api, bob );
+
   if (reqImports.fail_count > 0) {
     console.log("Failed");
   } else {
@@ -27,30 +32,31 @@ async function main() {
   process.exit();
 }
 
+async function bondPoly(api, signer, bob) {
 
-async function proposePIP(api, keyring) {
+  let nonceObj = {nonce: reqImports.nonces.get(signer.address)};
+  const transaction = await api.tx.staking.bond(bob.publicKey, 20_000, "Staked");
+  const result = await reqImports.sendTransaction(transaction, signer, nonceObj);  
+  const passed = result.findRecord('system', 'ExtrinsicSuccess');
+  if (passed) reqImports.fail_count--;
 
-  // Get the current sudo key in the system
-  const sudoKey = await api.query.sudo.key();
+  reqImports.nonces.set( signer.address, reqImports.nonces.get(signer.address).addn(1));
+}
 
-  // Lookup from keyring (assuming we have added all, on --dev this would be `//Alice`)
-  const sudoPair = keyring.getPair(sudoKey.toString());
+async function proposePIP(api, signer) {
 
   let proposal = await api.tx.pips.setProposalDuration(10);
-  let deposit = 10;
+  let deposit = 10_000_000_000;
   let url = "www.google.com";
   let description = "test proposal";
 
-  // Send the actual sudo transaction
-  const unsub = await api.tx.sudo.sudo(
-    await api.tx.pips.propose(proposal, deposit, url, description, null)
-    );
+  let nonceObj = {nonce: reqImports.nonces.get(signer.address)};
+  const transaction = await api.tx.pips.propose(proposal, deposit, url, description, null);
+  const result = await reqImports.sendTransaction(transaction, signer, nonceObj);  
+  const passed = result.findRecord('system', 'ExtrinsicSuccess');
+  if (passed) reqImports.fail_count--;
 
-    const result = await reqImports.sendTransaction(unsub, sudoPair, 0);  
-    const passed = result.findRecord('system', 'ExtrinsicSuccess');
-    if (passed) {
-      reqImports.fail_count--;
-    }
+  reqImports.nonces.set( signer.address, reqImports.nonces.get(signer.address).addn(1));
   
 }
 
