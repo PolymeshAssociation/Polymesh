ef040353597468522beb4f26596f759abed4bc24
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2020-06-12 06:55:10 -0400

    Mesh 995/add governance functional test (#410)
    * created governance script with pip propose and setProposalDuration
    
    * set release coordinator via gensis config
    
    * Misc. fixes
    
    * Fix
    
    * updated script and fixed test error
    
    * Add EnactProposalMaker
    
    * Add Origin to automatic propose
    
    * Add UT for `vote_enact_referendum`
    
    * Add EnactProposalMaker to V1 runtime
    
    * Add EnactProposalMaker
    
    * Add Origin to automatic propose
    
    * Add UT for `vote_enact_referendum`
    
    * Add EnactProposalMaker to V1 runtime
    
    * Merge issues
    
    * Add events for enact/reject referendum
    
    * added amend and fast track functions
    
    * added enactReferendum function
    
    * minor change and fixed linting issue
    
    * minor change
    
    * fixed release coordinator test case error
    
    * GC Members vote positively
    
    * Update committee referendum vote test
    
    * created voteEnactReferendum and minor changes
    
    * updated npm test in package.json with 10_bridge_transfer and 11_governance_management
    
    Co-authored-by: Adam Dossa <adam.dossa@gmail.com>
    Co-authored-by: miguel <francisco.miguel.garcia.rodriguez@gmail.com>
    Co-authored-by: Francisco Miguel García <fmiguelgarcia@users.noreply.github.com>
    Co-authored-by: Mudit Gupta <guptamudit@ymail.com>

--- pallets/runtime/common/tests/all/committee_test.rs
+++ pallets/runtime/common/tests/all/committee_test.rs
@@ -492,7 +492,10 @@ fn release_coordinator_we() {
     let bob_id = get_identity_id(AccountKeyring::Bob).expect("Bob is part of the committee");
     let charlie_id = register_keyring_account(AccountKeyring::Charlie).unwrap();
 
-    assert_eq!(Committee::release_coordinator(), None);
+    assert_eq!(
+        Committee::release_coordinator(),
+        Some(IdentityId::from(999))
+    );
 
     Context::set_current_identity::<Identity>(Some(alice_id));
     assert_err!(
