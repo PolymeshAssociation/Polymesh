6234b9155266f2fe4c25345d234beddde74135b4
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-06-18 19:29:06 +0200

    V1 build.rs fixed

--- pallets/runtime/testnet-v1/build.rs
+++ pallets/runtime/testnet-v1/build.rs
@@ -1,12 +1,10 @@
 use wasm_builder_runner::WasmBuilder;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
     WasmBuilder::new()
         .with_current_project()
-        .with_wasm_builder_from_git(
-            "https://github.com/paritytech/substrate.git",
-            "8c672e107789ed10973d937ba8cac245404377e2",
-        )
+        .with_wasm_builder_from_path("../../../utils/wasm-builder")
         .export_heap_base()
         .import_memory()
         .build()
