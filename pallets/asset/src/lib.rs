182131b702bc3dd1151215157eaa28ec21139f60
Adam Dossa <adam.dossa@gmail.com>
2020-07-01 19:48:13 +0100

    MESH-1102, MESH-1101, MESH-1100: Fix events, errors, kill_proposal and set_proposal_cool_off_period (#452)
    * Fix events, errors, kill_proposal and set_proposal_cool_off_period
    
    * Linting
    
    * Fix tests
    
    * Fix some more tests

--- pallets/asset/src/lib.rs
+++ pallets/asset/src/lib.rs
@@ -1857,6 +1857,7 @@ impl<T: Trait> Module<T> {
             <BalanceOf<T>>::contains_key(ticker, &from_did),
             Error::<T>::NotAAssetHolder
         );
+        ensure!(from_did != to_did, Error::<T>::InvalidTransfer);
         let sender_balance = Self::balance(ticker, &from_did);
         ensure!(sender_balance >= value, Error::<T>::InsufficientBalance);
 
@@ -2265,6 +2266,9 @@ impl<T: Trait> Module<T> {
         // Granularity check
         if !Self::check_granularity(&ticker, amount) {
             return Ok(INVALID_GRANULARITY);
+        }
+        if from_did == to_did {
+            return Ok(INVALID_RECEIVER_DID);
         }
         // Non-Issuance case check
         if let Some(from_id) = from_did {
