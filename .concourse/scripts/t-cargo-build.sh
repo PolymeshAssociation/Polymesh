55d03e671e97bf6112d57ed573580825a10489c7
Claus Strommer <claus@polymath.network>
2020-07-09 19:18:31 -0400

    Fix for git metatdata

--- .concourse/scripts/t-cargo-build.sh
+++ .concourse/scripts/t-cargo-build.sh
@@ -15,7 +15,19 @@ cargo build --release || cargo build -j 1 --release
 popd
 
 mkdir -p artifact
-cp ${GIT_DIR}/target/ ${CACHE_DIR}/target/release/polymesh artifact/polymesh-$(cat ${CACHE_DIR}/.git/describe_ref)
+
+POLYMESH_VERSION=""
+if [[ -f "${GIT_DIR}/.git/resource/head_sha" ]]; then
+    POLYMESH_VERSION=$(cat ${GIT_DIR}/.git/resource/head_sha)
+elif [[ -f ${GIT_DIR}/.git/describe_ref ]]; then
+    POLYMESH_VERSION=$(cat ${GIT_DIR}/.git/describe_ref)
+else
+    echo "no reference for the polymesh version found"
+    ls -l ${GIT_DIR}
+    exit 1
+fi
+
+cp ${GIT_DIR}/target//release/polymesh artifact/polymesh-${POLYMESH_VERSION}
 
 rsync -auv ${CARGO_HOME:-$HOME/.cargo}/ ${CACHE_DIR}/.cargo
 rsync -auv ${GIT_DIR}/target/ ${CACHE_DIR}/target
