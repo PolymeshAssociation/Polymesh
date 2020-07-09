66bf31c2ab18ceaba7b39cab0a99ccf8f50384bb
Claus Strommer <claus@polymath.network>
2020-07-09 17:08:33 -0400

    s3 artifact

--- .concourse/scripts/t-cargo-build.sh
+++ .concourse/scripts/t-cargo-build.sh
@@ -14,6 +14,9 @@ cargo build --release || cargo build -j 1 --release
 
 popd
 
+mkdir -p artifact
+cp ${GIT_DIR}/target/ ${CACHE_DIR}/target/release/polymesh artifact/polymesh-$(cat ${CACHE_DIR}/.git/describe_ref)
+
 rsync -auv ${CARGO_HOME:-$HOME/.cargo}/ ${CACHE_DIR}/.cargo
 rsync -auv ${GIT_DIR}/target/ ${CACHE_DIR}/target
 
