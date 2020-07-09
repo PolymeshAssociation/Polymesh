4bc2993be9cb20b01c9103032f01a54751bbd1e0
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-07-09 09:42:22 +0200

    CodeReview changes

--- pallets/confidential/src/lib.rs
+++ pallets/confidential/src/lib.rs
@@ -16,17 +16,18 @@
 #![feature(box_syntax)]
 
 use polymesh_common_utilities::{identity::Trait as IdentityTrait, Context};
-use polymesh_primitives::{AccountId, IdentityId, Ticker};
+use polymesh_primitives::{IdentityId, Ticker};
 use polymesh_primitives_derive::{SliceU8StrongTyped, VecU8StrongTyped};
 
 use pallet_identity as identity;
 
 use bulletproofs::RangeProof;
-use cryptography::asset_proofs::range_proof::{prove_within_range, verify_within_range};
+use cryptography::asset_proofs::range_proof::{
+    prove_within_range, verify_within_range, InRangeProof,
+};
 use curve25519_dalek::{ristretto::CompressedRistretto, scalar::Scalar};
 
 use codec::{Decode, Encode};
-use core::convert::TryFrom;
 use frame_support::{
     debug, decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult,
     weights::SimpleDispatchInfo,
@@ -59,7 +60,7 @@ pub trait Trait: frame_system::Trait + IdentityTrait {
 type Identity<T> = identity::Module<T>;
 
 #[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
-pub struct ProberTickerKey {
+pub struct ProverTickerKey {
     prover: IdentityId,
     ticker: Ticker,
 }
@@ -67,7 +68,7 @@ pub struct ProberTickerKey {
 decl_storage! {
     trait Store for Module<T: Trait> as Confidential {
         /// Number of investor per asset.
-        pub RangeProofs get(fn range_proof): double_map hasher(twox_64_concat) IdentityId, hasher(blake2_128_concat) ProberTickerKey => Option<TickerRangeProof>;
+        pub RangeProofs get(fn range_proof): double_map hasher(twox_64_concat) IdentityId, hasher(blake2_128_concat) ProverTickerKey => Option<TickerRangeProof>;
 
         pub RangeProofVerifications get(fn range_proof_verification): double_map hasher(blake2_128_concat) (IdentityId, Ticker), hasher(twox_64_concat) IdentityId => bool;
     }
@@ -92,18 +93,18 @@ decl_module! {
             // Create proof
             let mut rng = rng::Rng::default();
             let rand_blind = Scalar::random(&mut rng);
-            let (init_message, final_response, _range) = prove_within_range( secret_value, rand_blind, 32, &mut rng)
+            let in_range_proof = prove_within_range( secret_value, rand_blind, 32, &mut rng)
                 .map_err(|e| {
                     debug::error!("Confidential error: {:?}", e);
                     Error::<T>::InvalidRangeProof
                 })?;
 
             let ticker_range_proof = TickerRangeProof {
-                initial_message: init_message.as_bytes().into(),
-                final_response: final_response.to_bytes().into(),
+                initial_message: in_range_proof.init.as_bytes().into(),
+                final_response: in_range_proof.response.to_bytes().into(),
                 max_two_exp: 32,
             };
-            let prover_ticker_key = ProberTickerKey { prover, ticker };
+            let prover_ticker_key = ProverTickerKey { prover, ticker };
             <RangeProofs>::insert(&target_id, &prover_ticker_key, ticker_range_proof);
             Ok(())
         }
@@ -148,17 +149,21 @@ impl<T: Trait> Module<T> {
         prover: IdentityId,
         ticker: Ticker,
     ) -> DispatchResult {
-        let prover_ticker_key = ProberTickerKey { prover, ticker };
+        let prover_ticker_key = ProverTickerKey { prover, ticker };
         let mut rng = rng::Rng::default();
 
         let trp = Self::range_proof(&target, &prover_ticker_key)
             .ok_or_else(|| Error::<T>::MissingRangeProof)?;
 
-        let initial_message = CompressedRistretto::from_slice(trp.initial_message.as_slice());
-        let final_response = RangeProof::from_bytes(trp.final_response.as_slice())
+        let init = CompressedRistretto::from_slice(trp.initial_message.as_slice());
+        let response = RangeProof::from_bytes(trp.final_response.as_slice())
             .map_err(|_| Error::<T>::InvalidRangeProof)?;
+        let proof = InRangeProof {
+            init,
+            response,
+            range: 32,
+        };
 
-        verify_within_range(initial_message, final_response, trp.max_two_exp, &mut rng)
-            .map_err(|_| Error::<T>::InvalidRangeProof.into())
+        verify_within_range(&proof, &mut rng).map_err(|_| Error::<T>::InvalidRangeProof.into())
     }
 }
