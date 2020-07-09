0d19ab40028b87c85d283d18058fde22f75fdb14
Claus Strommer <claus@polymath.network>
2020-07-08 21:25:13 -0400

    try parallel tasks with shared caches

--- .concourse/scripts/t-cargo-build.sh
+++ .concourse/scripts/t-cargo-build.sh
@@ -4,5 +4,16 @@ set -e
 set -x
 set -o pipefail
 
+GIT_DIR=$1
+CACHE_DIR=$2
+
+pushd .
+cd $GIT_DIR
+
 cargo build --release || cargo build -j 1 --release
+
+popd
+
+rsync -auv $HOME/.cargo/ ${CACHE_DIR}/.cargo
+rsync -auv ${GIT_DIR}/target/ ${CACHE_DIR}/target
 
--- .concourse/scripts/t-lint-clippy.sh
+++ .concourse/scripts/t-lint-clippy.sh
@@ -4,6 +4,17 @@ set -e
 set -x
 set -o pipefail
 
+GIT_DIR=$1
+CACHE_DIR=$2
+
+mkdir -p $HOME/.cargo
+mkdir -p ${GIT_DIR}/target
+
+rsync -auv ${CACHE_DIR}/.cargo/ $HOME/.cargo
+rsync -auv ${CACHE_DIR}/target/ ${GIT_DIR}/target
+
+cd $GIT_DIR
+
 cargo +nightly clippy -j 1
 
 
