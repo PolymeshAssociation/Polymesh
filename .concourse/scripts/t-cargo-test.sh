e8287cabd30f93c68b367796fa2ed7744f5fb744
Claus Strommer <claus@polymath.network>
2020-07-09 20:04:36 -0400

    test

--- .concourse/scripts/t-cargo-test.sh
+++ .concourse/scripts/t-cargo-test.sh
@@ -15,23 +15,23 @@ rsync -auv ${CACHE_DIR}/target/ ${GIT_DIR}/target
 
 cd $GIT_DIR
 
-cargo test \
-  --package polymesh-runtime-common \
-  --package pallet-staking \
-  --package pallet-group \
-  --package polymesh-primitives \
-  --package pallet-pips-rpc \
-  --package pallet-transaction-payment \
-  --package pallet-cdd-offchain-worker \
-  --features default_identity \
-|| \
-cargo test -j 1 \
-  --package polymesh-runtime-common \
-  --package pallet-staking \
-  --package pallet-group \
-  --package polymesh-primitives \
-  --package pallet-pips-rpc \
-  --package pallet-transaction-payment \
-  --package pallet-cdd-offchain-worker \
-  --features default_identity \
+#cargo test \
+#  --package polymesh-runtime-common \
+#  --package pallet-staking \
+#  --package pallet-group \
+#  --package polymesh-primitives \
+#  --package pallet-pips-rpc \
+#  --package pallet-transaction-payment \
+#  --package pallet-cdd-offchain-worker \
+#  --features default_identity \
+#|| \
+#cargo test -j 1 \
+#  --package polymesh-runtime-common \
+#  --package pallet-staking \
+#  --package pallet-group \
+#  --package polymesh-primitives \
+#  --package pallet-pips-rpc \
+#  --package pallet-transaction-payment \
+#  --package pallet-cdd-offchain-worker \
+#  --features default_identity \
 
