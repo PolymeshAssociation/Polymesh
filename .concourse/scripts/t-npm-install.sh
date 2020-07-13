5b080ef66ee660395b581ca96fef895851d8e8a5
Claus Strommer <claus@polymath.network>
2020-07-13 09:03:13 -0400

    cache npm modules

--- .concourse/scripts/t-npm-install.sh
+++ .concourse/scripts/t-npm-install.sh
@@ -12,8 +12,13 @@ mkdir -p ${CACHE_DIR}/scripts/cli/node_modules
 
 rsync -auv ${CACHE_DIR}/scripts/cli/node_modules/ ${GIT_DIR}/scripts/cli/node_modules
 
+pushd
+
 cd $GIT_DIR/scripts/cli
 
 npm install
 
+popd
+
+rsync -auv ${GIT_DIR}/scripts/cli/node_modules/ ${CACHE_DIR}/scripts/cli/node_modules
 
