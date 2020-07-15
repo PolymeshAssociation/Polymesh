72d12c7cf7172361afa03ef6deefe7fbe313aad8
Mudit Gupta <guptamudit@ymail.com>
2020-07-15 17:11:52 +0530

    comment out cli transfer test

--- scripts/cli/tests/8_asset_transfer.js
+++ scripts/cli/tests/8_asset_transfer.js
@@ -10,7 +10,7 @@ process.exitCode = 1;
 const prepend = "DEMOAT";
 
 async function main() {
-  
+
   const api = await reqImports.createApi();
   const ticker = `token${prepend}0`.toUpperCase();
   const asset_did = reqImports.tickerToDid(ticker);
@@ -36,9 +36,10 @@ async function main() {
 
   await reqImports.createClaimRules( api, master_keys, issuer_dids, prepend );
 
-await mintingAsset( api, master_keys[0], issuer_dids[2], prepend );
+  await mintingAsset( api, master_keys[0], issuer_dids[2], prepend );
 
-  await assetTransfer( api, master_keys[0], issuer_dids[2], prepend );
+  // TODO: Use settlement module
+  // await assetTransfer( api, master_keys[0], issuer_dids[2], prepend );
 
   if (reqImports.fail_count > 0) {
     console.log("Failed");
@@ -53,24 +54,25 @@ await mintingAsset( api, master_keys[0], issuer_dids[2], prepend );
 async function mintingAsset(api, minter, did, prepend) {
   const ticker = `token${prepend}0`.toUpperCase();
   let nonceObj = {nonce: reqImports.nonces.get(minter.address)};
-  const transaction = await api.tx.asset.issue(ticker, did, 100, "");
-  const result = await reqImports.sendTransaction(transaction, minter, nonceObj);  
+  const transaction = await api.tx.asset.issue(ticker, 100);
+  const result = await reqImports.sendTransaction(transaction, minter, nonceObj);
   const passed = result.findRecord('system', 'ExtrinsicSuccess');
   if (passed) reqImports.fail_count--;
 
   reqImports.nonces.set(minter.address, reqImports.nonces.get(minter.address).addn(1));
 }
 
-async function assetTransfer(api, from_account, did, prepend) {
-    const ticker = `token${prepend}0`.toUpperCase();
-    let nonceObj = {nonce: reqImports.nonces.get(from_account)};
-    const transaction = await api.tx.asset.transfer(ticker, did, 100);
-    const result = await reqImports.sendTransaction(transaction, from_account, nonceObj);  
-    const passed = result.findRecord('system', 'ExtrinsicSuccess');
-    if (passed) reqImports.fail_count--;
+// TODO: Use settlement module
+// async function assetTransfer(api, from_account, did, prepend) {
+//     const ticker = `token${prepend}0`.toUpperCase();
+//     let nonceObj = {nonce: reqImports.nonces.get(from_account)};
+//     const transaction = await api.tx.asset.transfer(ticker, did, 100);
+//     const result = await reqImports.sendTransaction(transaction, from_account, nonceObj);
+//     const passed = result.findRecord('system', 'ExtrinsicSuccess');
+//     if (passed) reqImports.fail_count--;
 
-    reqImports.nonces.set( from_account.address, reqImports.nonces.get(from_account.address).addn(1));
-  
-}
+//     reqImports.nonces.set( from_account.address, reqImports.nonces.get(from_account.address).addn(1));
+
+// }
 
 main().catch(console.error);
