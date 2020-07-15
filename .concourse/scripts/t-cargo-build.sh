5d0eeca1674fa248ca7611efa03e962b37b2303b
Claus Strommer <claus@polymath.network>
2020-07-15 00:09:18 -0400

    Fix directory

--- .concourse/scripts/t-cargo-build.sh
+++ .concourse/scripts/t-cargo-build.sh
@@ -30,11 +30,11 @@ fi
 
 SEMVER=$(cat $SEMVER_DIR/version)
 
-cp ${GIT_DIR}/Dockerfile              $ARTIFACT_DIR/
-cp ${SEMVER_DIR}/version              $ARTIFACT_DIR/tag_file
-cp ${GIT_DIR}/target/release/polymesh $ARTIFACT_DIR/polymesh
-cp ${GIT_DIR}/target/release/polymesh $ARTIFACT_DIR/polymesh-${SEMVER}
-echo -n "latest-forked forked ${GIT_REF}" > dockerdir/additional_tags
+cp ${GIT_DIR}/Dockerfile              ${ARTIFACT_DIR}/
+cp ${SEMVER_DIR}/version              ${ARTIFACT_DIR}/tag_file
+cp ${GIT_DIR}/target/release/polymesh ${ARTIFACT_DIR}/polymesh
+cp ${GIT_DIR}/target/release/polymesh ${ARTIFACT_DIR}/polymesh-${SEMVER}
+echo -n "latest-forked forked ${GIT_REF}" > ${ARTIFACT_DIR}/additional_tags
 
 rsync -auv --size-only ${CARGO_HOME:-$HOME/.cargo}/ ${CACHE_DIR}/.cargo | grep -e "^total size" -B1 --color=never
 rsync -auv --size-only ${GIT_DIR}/target/           ${CACHE_DIR}/target | grep -e "^total size" -B1 --color=never
