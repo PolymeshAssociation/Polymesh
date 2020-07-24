67090eb0617072104da9af5a02326e7bbd43b9e3
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2020-07-24 09:48:54 -0400

    fixed formatting

--- scripts/cli/src/commands/create_account_identity.ts
+++ scripts/cli/src/commands/create_account_identity.ts
@@ -1,18 +1,17 @@
-
-
-import chalk from 'chalk';
+import chalk from "chalk";
 import { getAPI, generateEntity, getCddProvider } from "./util/init";
 import createIdentities from "./helpers/identity_helper";
 //const common = require('./common/common_functions');
 //const input = require('./IO/input');
 
-
-export default async function executeApp (entityName: string, topup: boolean) {
- // common.logAsciiBull();
+export default async function executeApp(entityName: string, topup: boolean) {
+  // common.logAsciiBull();
   console.log("********************************************");
   console.log("Welcome to the Command-Line Account Identity Generator.");
   console.log("********************************************");
-  console.log("The following script will create a new Identity according to the parameters you entered.");
+  console.log(
+    "The following script will create a new Identity according to the parameters you entered."
+  );
 
   try {
     // Get API
@@ -23,13 +22,10 @@ export default async function executeApp (entityName: string, topup: boolean) {
     let cdd_provider = getCddProvider();
 
     // Create Identity
-    let did = await createIdentities(api, [entity], cdd_provider, topup );
+    let did = await createIdentities(api, [entity], cdd_provider, topup);
 
     console.log(`DID Address: ${JSON.stringify(did)}`);
-
   } catch (err) {
     console.log(err);
   }
-};
-
-
+}
--- scripts/cli/src/commands/create_key_identity.ts
+++ scripts/cli/src/commands/create_key_identity.ts
@@ -1,17 +1,21 @@
-
-
-import chalk from 'chalk';
+import chalk from "chalk";
 import { getAPI, generateKeys, getCddProvider } from "./util/init";
 import createIdentities from "./helpers/identity_helper";
 //const common = require('./common/common_functions');
 //const input = require('./IO/input');
 
-export default async function executeApp (keyNumber: number, keyPrepend: string, topup: boolean) {
- // common.logAsciiBull();
+export default async function executeApp(
+  keyNumber: number,
+  keyPrepend: string,
+  topup: boolean
+) {
+  // common.logAsciiBull();
   console.log("********************************************");
   console.log("Welcome to the Command-Line Key Identity Generator.");
   console.log("********************************************");
-  console.log("The following script will create a new Identity according to the parameters you entered.");
+  console.log(
+    "The following script will create a new Identity according to the parameters you entered."
+  );
 
   try {
     // Get API
@@ -24,12 +28,10 @@ export default async function executeApp (keyNumber: number, keyPrepend: string,
     let keys = await generateKeys(api, keyNumber, keyPrepend);
 
     // Create Identity
-    let dids = await createIdentities(api, keys, cdd_provider, topup );
+    let dids = await createIdentities(api, keys, cdd_provider, topup);
 
     console.log(`DID Address/s: ${JSON.stringify(dids)}`);
-
   } catch (err) {
     console.log(err);
   }
-};
-
+}
