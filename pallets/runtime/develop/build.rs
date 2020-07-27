50ba18ebb2f4b33d520b3cba423d4544d1d68bb3
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-07-27 10:45:30 +0200

    CodeReview: Remove unused code

--- pallets/runtime/develop/build.rs
+++ pallets/runtime/develop/build.rs
@@ -1,7 +1,6 @@
 use wasm_builder_runner::WasmBuilder;
 
 fn main() {
-    // println!("cargo:rerun-if-changed=build.rs");
     WasmBuilder::new()
         .with_current_project()
         .with_wasm_builder_from_path("../../../utils/wasm-builder")
