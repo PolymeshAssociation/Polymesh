87b1e9dfccae5309bc0b491571ef7388ec7abe63
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-08-03 17:39:19 +0200

    Add error case on confidential tests

--- pallets/runtime/tests/src/confidential_test.rs
+++ pallets/runtime/tests/src/confidential_test.rs
@@ -18,6 +18,7 @@ use test_client::AccountKeyring;
 
 type Identity = identity::Module<TestStorage>;
 type Asset = asset::Module<TestStorage>;
+type AssetError = asset::Error<TestStorage>;
 type Confidential = confidential::Module<TestStorage>;
 type ComplianceManager = compliance_manager::Module<TestStorage>;
 type Origin = <TestStorage as frame_system::Trait>::Origin;
@@ -145,7 +146,8 @@ fn scope_claims_we() {
     let cdd_claim_1 = InvestorZKProofData::make_cdd_claim(&inv_did_1, &investor);
     let cdd_id_1 = compute_cdd_id(&cdd_claim_1).compress().to_bytes().into();
 
-    let conf_scope_claim_1 = Claim::InvestorZKProof(st_scope, scope_id, cdd_id_1, inv_1_proof);
+    let conf_scope_claim_1 =
+        Claim::InvestorZKProof(st_scope, scope_id, cdd_id_1, inv_1_proof.clone());
 
     assert_ok!(Identity::add_claim(
         Origin::signed(inv_acc_1),
@@ -175,7 +177,7 @@ fn scope_claims_we() {
     assert_ok!(Asset::transfer(Origin::signed(alice), st_id, inv_did_2, 20));
     assert_eq!(Asset::balance_of(st_id, inv_did_2), 20);
 
-    // 4. Fail case: Investor 2 cannot add a claim of the real investor.
+    // 4. ERROR: Investor 2 cannot add a claim of the real investor.
     assert_err!(
         Identity::add_claim(
             Origin::signed(inv_acc_3),
@@ -184,5 +186,49 @@ fn scope_claims_we() {
             None
         ),
         Error::<TestStorage>::ConfidentialScopeClaimNotAllowed
+    );
+
+    // 5. ERROR: Replace the scope
+    let st_2 = SecurityToken {
+        name: "ALI2_ST".as_bytes().to_owned().into(),
+        owner_did: alice_id,
+        total_supply: 1_000_000,
+        divisible: true,
+        asset_type: AssetType::default(),
+        ..Default::default()
+    };
+    let identifiers = vec![(IdentifierType::Isin, b"X123".into())];
+    let st2_id = Ticker::try_from(st_2.name.as_slice()).unwrap();
+
+    assert_ok!(Asset::create_asset(
+        Origin::signed(alice),
+        st_2.name.clone(),
+        st2_id,
+        st_2.total_supply,
+        true,
+        st_2.asset_type.clone(),
+        identifiers.clone(),
+        None,
+        None
+    ));
+
+    let st_scope = IdentityId::try_from(st2_id.as_slice()).unwrap();
+    let corrupted_scope_claim = InvestorZKProofData::make_scope_claim(&st2_id, &investor);
+    let corrupted_scope_id = compute_scope_id(&corrupted_scope_claim)
+        .compress()
+        .to_bytes()
+        .into();
+
+    let conf_scope_claim_3 =
+        Claim::InvestorZKProof(st_scope, corrupted_scope_id, cdd_id_1, inv_1_proof);
+    assert_ok!(Identity::add_claim(
+        Origin::signed(inv_acc_1),
+        inv_did_1,
+        conf_scope_claim_3.clone(),
+        None
+    ));
+    assert_err!(
+        Asset::transfer(Origin::signed(alice), st2_id, inv_did_1, 10),
+        AssetError::InvalidTransfer
     );
 }
