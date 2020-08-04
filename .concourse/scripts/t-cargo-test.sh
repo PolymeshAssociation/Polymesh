902618670dd53b4cccd8b8514c5dd2affe9627a8
Claus Strommer <claus@polymath.network>
2020-08-04 16:09:00 -0400

    DEVOPS-131/concourse-core-integration
    * Update cargo test step to fetch submodules

--- .concourse/scripts/t-cargo-test.sh
+++ .concourse/scripts/t-cargo-test.sh
@@ -8,6 +8,17 @@ GIT_DIR=$1
 
 cd $GIT_DIR
 
+# Fetch submodules.  Workaround for https://github.com/telia-oss/github-pr-resource/pull/200
+if [ -f .git/resource/head_sha ]; then
+    git submodule init
+    set +x
+    git config submodule.external/cryptography.url "https://${SUBMODULE_ACCESS_TOKEN}@github.com/PolymathNetwork/cryptography.git"
+    git config submodule.pallets/settlement.url    "https://${SUBMODULE_ACCESS_TOKEN}@github.com/PolymathNetwork/polymesh-settlement-module.git"
+    set -x
+    git submodule update pallets/settlement
+    git submodule update external/cryptography
+fi
+
 cargo test \
   --package polymesh-runtime-common \
   --package pallet-staking \
