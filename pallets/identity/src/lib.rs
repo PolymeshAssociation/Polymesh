a04ca2bd0e9817f50f715cccd0683ec190a5e410
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-08-06 16:21:46 +0100

    Revert "unused import removed"
    This reverts commit 56cf40d9cf0016bedeb4cce4b8113ab0497ade3f.

--- pallets/identity/src/lib.rs
+++ pallets/identity/src/lib.rs
@@ -127,7 +127,7 @@ use frame_support::{
     dispatch::{DispatchError, DispatchResult, DispatchResultWithPostInfo},
     ensure,
     traits::{ChangeMembers, Currency, InitializeMembers},
-    weights::{DispatchClass, GetDispatchInfo, Pays},
+    weights::{DispatchClass, GetDispatchInfo, Pays, Weight},
     StorageDoubleMap,
 };
 use frame_system::{self as system, ensure_root, ensure_signed};
