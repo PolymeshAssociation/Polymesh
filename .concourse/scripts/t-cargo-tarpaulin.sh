54fe0e5e77da37ee71fc0b413e4801fca25cc0e3
Claus Strommer <claus@polymath.network>
2020-09-02 20:35:17 -0400

    DEVOPS-175/coverage-tests
    * Add cargo-tarpaulin script
    * Run cargo-tarpaulin script

---
+++ .concourse/scripts/t-cargo-tarpaulin.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+set -o pipefail
+
+GIT_DIR=$1
+
+cd $GIT_DIR
+
+# Fetch submodules.  Workaround for https://github.com/telia-oss/github-pr-resource/pull/200
+if [ ! -z "$SUBMODULE_ACCESS_TOKEN" ]; then
+    git submodule init
+    set +x
+    git config submodule.external/cryptography.url "https://${SUBMODULE_ACCESS_TOKEN}@github.com/PolymathNetwork/cryptography.git"
+    git config submodule.pallets/settlement.url    "https://${SUBMODULE_ACCESS_TOKEN}@github.com/PolymathNetwork/polymesh-settlement-module.git"
+    set -x
+    git submodule update pallets/settlement
+    git submodule update external/cryptography
+fi
+
+cargo tarpaulin \
+  --packages polymesh-runtime-common \
+  --packages pallet-staking \
+  --packages pallet-group \
+  --packages polymesh-primitives \
+  --packages node-rpc-runtime-api \
+  --packages pallet-transaction-payment \
+  --packages pallet-cdd-offchain-worker \
+  --features default_identity \
+  --count \
+  --ignore-tests \
+  --coveralls ${COVERALLS_REPO_TOKEN} \
+
