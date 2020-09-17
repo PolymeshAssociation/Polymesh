8db185b4c32fc4c0729b52005f4c7a5567c3a564
Arash Afshar <arash@polymath.network>
2020-09-17 11:20:04 -0600

    draft of verifying the asset minting

--- pallets/runtime/tests/src/confidential_asset_test.rs
+++ pallets/runtime/tests/src/confidential_asset_test.rs
@@ -9,25 +9,26 @@ use cryptography::{
     asset_proofs::{CommitmentWitness, ElgamalSecretKey},
     mercat::{
         account::{convert_asset_ids, AccountCreator},
-        AccountCreatorInitializer, EncryptedAmount, EncryptionKeys, SecAccount,
+        asset::AssetIssuer,
+        Account, AccountCreatorInitializer, AssetTransactionIssuer, AssetTransactionVerifier,
+        EncryptedAmount, EncryptionKeys, InitializedAssetTx, PubAccountTx, SecAccount,
     },
     AssetId,
 };
 use curve25519_dalek::scalar::Scalar;
 use frame_support::IterableStorageMap;
+use frame_support::{assert_err, assert_ok, StorageDoubleMap, StorageMap};
 use pallet_asset::{self as asset};
 use pallet_confidential_asset as confidential_asset;
-use pallet_statistics as statistics;
-use rand::{rngs::StdRng, SeedableRng};
-use test_client::AccountKeyring;
-
-use frame_support::{assert_err, assert_ok, StorageDoubleMap, StorageMap};
 use pallet_identity as identity;
+use pallet_statistics as statistics;
 use polymesh_primitives::{
     AssetOwnershipRelation, AssetType, FundingRoundName, IdentifierType, SecurityToken, Ticker,
 };
+use rand::{rngs::StdRng, SeedableRng};
 use sp_core::sr25519::Public;
 use sp_runtime::AnySignature;
+use test_client::AccountKeyring;
 
 type Identity = identity::Module<TestStorage>;
 type Asset = asset::Module<TestStorage>;
@@ -49,6 +50,38 @@ fn create_confidential_token(token_name: &[u8], ticker: Ticker, keyring: Public)
         vec![],
         None,
     ));
+}
+
+fn gen_account(
+    tx_id: u32,
+    seed: [u8; 32],
+    token_name: &[u8],
+    valid_asset_ids: Vec<AssetId>,
+) -> (SecAccount, PubAccountTx) {
+    // These are the encryptions keys used by MERCAT and are different from the signing keys
+    // that Polymesh uses.
+    let mut rng = StdRng::from_seed(seed);
+    let elg_secret = ElgamalSecretKey::new(Scalar::random(&mut rng));
+    let elg_pub = elg_secret.get_public_key();
+    let enc_keys = EncryptionKeys {
+        pblc: elg_pub.into(),
+        scrt: elg_secret.into(),
+    };
+
+    let asset_id = AssetId {
+        id: *Ticker::try_from(token_name).unwrap().as_bytes(),
+    };
+    let asset_id_witness = CommitmentWitness::from((asset_id.clone().into(), &mut rng));
+    let secret_account = SecAccount {
+        enc_keys,
+        asset_id_witness,
+    };
+    let valid_asset_ids = convert_asset_ids(valid_asset_ids);
+    let mercat_account_tx = AccountCreator {}
+        .create(tx_id, &secret_account, &valid_asset_ids, &mut rng)
+        .unwrap();
+
+    (secret_account, mercat_account_tx)
 }
 
 #[test]
@@ -220,39 +253,12 @@ fn account_create_tx() {
         let alice = AccountKeyring::Alice.public();
         let alice_id = register_keyring_account(AccountKeyring::Alice).unwrap();
 
-        // These are the encryptions keys used by MERCAT and are different from the signing keys
-        // that Polymesh uses.
-        let mut rng = StdRng::from_seed([10u8; 32]);
-        let elg_secret = ElgamalSecretKey::new(Scalar::random(&mut rng));
-        let elg_pub = elg_secret.get_public_key();
-        let enc_keys = EncryptionKeys {
-            pblc: elg_pub.into(),
-            scrt: elg_secret.into(),
-        };
-
-        // Positive test: use one of the registered asset ids.
-        let asset_id = AssetId {
-            id: *Ticker::try_from(&token_names[1][..]).unwrap().as_bytes(),
-        };
-        let asset_id_witness = CommitmentWitness::from((asset_id.clone().into(), &mut rng));
-        let scrt_account = SecAccount {
-            enc_keys,
-            asset_id_witness,
-        };
-
-        let valid_asset_ids = convert_asset_ids(valid_asset_ids);
-        let account_id = 0;
-        let mercat_tx_id = 0;
-        let mercat_account_tx = AccountCreator {}
-            .create(
-                mercat_tx_id,
-                &scrt_account,
-                &valid_asset_ids,
-                account_id,
-                &mut rng,
-            )
-            .unwrap();
-
+        let (scrt_account, mercat_account_tx) = gen_account(
+            0, // Transaction id. Not used in this test.
+            [10u8; 32],
+            &token_names[1][..],
+            valid_asset_ids,
+        );
         // ------------- END: Computations that will happen in the Wallet ----------
 
         // Wallet submits the transaction to the chain for verification.
@@ -281,4 +287,65 @@ fn account_create_tx() {
         let stored_balance = scrt_account.enc_keys.scrt.decrypt(&stored_balance).unwrap();
         assert_eq!(stored_balance, 0);
     });
+}
+
+#[test]
+fn issue_tokens() {
+    ExtBuilder::default().build().execute_with(|| {
+        // ------------ Setup
+        let token_names = [[2u8], [1u8], [5u8]];
+        for token_name in token_names.iter() {
+            create_confidential_token(
+                &token_name[..],
+                Ticker::try_from(&token_name[..]).unwrap(),
+                AccountKeyring::Bob.public(),
+            );
+        }
+
+        let valid_asset_ids: Vec<AssetId> = ConfidentialAsset::confidential_tickers();
+
+        let (scrt_account, mercat_account_tx) = gen_account(
+            0, // Transaction id. Not important in this test.
+            [10u8; 32],
+            &token_names[1][..],
+            valid_asset_ids,
+        );
+
+        let alice = AccountKeyring::Alice.public();
+        let alice_id = register_keyring_account(AccountKeyring::Alice).unwrap();
+
+        ConfidentialAsset::validate_mercat_account(
+            Origin::signed(alice),
+            mercat_account_tx.clone(),
+        )
+        .unwrap();
+
+        // ------------- START: Computations that will happen in Alice's Wallet ----------
+        let amount: u32 = 11; // mercat amounts are 32 bit integers.
+        let mut rng = StdRng::from_seed([42u8; 32]);
+        let issuer_account = Account {
+            scrt: scrt_account,
+            pblc: mercat_account_tx.pub_account.clone(),
+        };
+
+        let initialized_asset_tx = AssetIssuer {}
+            .initialize_asset_transaction(
+                1, // Transaction id. Not important in this test.
+                &issuer_account,
+                &[],
+                amount,
+                &mut rng,
+            )
+            .unwrap();
+
+        // ------------- END: Computations that will happen in the Wallet ----------
+
+        // Wallet submits the transaction to the chain for verification.
+        ConfidentialAsset::mint_confidential_asset(
+            Origin::signed(alice),
+            amount.into(), // convert to u128
+            initialized_asset_tx,
+        )
+        .unwrap();
+    })
 }
