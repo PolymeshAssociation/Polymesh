7b6fa4c76140a27971959e6a6efd976715c25593
Adam Dossa <adam.dossa@gmail.com>
2020-09-18 11:12:19 +0100

    Update REWARD_CURVE for testnet (#617)

--- pallets/runtime/testnet/src/runtime.rs
+++ pallets/runtime/testnet/src/runtime.rs
@@ -338,7 +338,7 @@ impl pallet_session::historical::Trait for Runtime {
 pallet_staking_reward_curve::build! {
     const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
         min_inflation: 0_025_000,
-        max_inflation: 0_200_000,
+        max_inflation: 0_140_000,
         ideal_stake: 0_700_000,
         falloff: 0_050_000,
         max_piece_count: 40,
