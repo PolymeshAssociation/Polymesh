7b7ae562f269658d866f54aea5124097898e48ba
Mazdak Farrokhzad <twingoow@gmail.com>
2020-09-24 11:44:07 +0200

    mesh-1255/balances: pull in substrate rc6 changes (#627)
    Co-authored-by: Satyam Agrawal <satyam0499@gmail.com>

--- pallets/runtime/tests/src/staking/mock.rs
+++ pallets/runtime/tests/src/staking/mock.rs
@@ -279,6 +279,7 @@ impl balances::Trait for Test {
     type AccountStore = System;
     type Identity = identity::Module<Test>;
     type CddChecker = Test;
+    type WeightInfo = ();
 }
 
 parameter_types! {
