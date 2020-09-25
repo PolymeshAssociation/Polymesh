8ab044105bbba0cb9eec605e63c07f0daf19daa8
Mudit Gupta <guptamudit@ymail.com>
2020-09-25 23:03:07 +0530

    minor changes

--- scripts/noop.sh
+++ scripts/noop.sh
@@ -1,1 +1,1 @@
-cargo run --release -p node-bench -- ::node::import::wasm::sr25519::transfer::rocksdb::custom --transactions 100
+cargo run --release -p node-bench -- ::node::import::wasm::sr25519::noop::rocksdb::custom --transactions 100
