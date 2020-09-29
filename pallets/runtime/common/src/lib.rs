2a56233d244897cc9c386161efa3e6cd5095e88f
Mudit Gupta <guptamudit@ymail.com>
2020-09-29 20:16:36 +0530

    Update pallets/runtime/common/src/lib.rs
    Co-authored-by: Vladimir Komendantskiy <komendantsky@gmail.com>

--- pallets/runtime/common/src/lib.rs
+++ pallets/runtime/common/src/lib.rs
@@ -59,7 +59,7 @@ parameter_types! {
     /// When the read/writes are cached, they take 25, 10 microseconds
     /// but when they are uncached, they take 250, 450 microseconds.
     /// Most read/writes are cached in production
-    /// so we are taking a defensive middle number here
+    /// so we are taking a defensive middle number here.
     pub const RocksDbWeight: RuntimeDbWeight = RuntimeDbWeight {
         read: 100 * WEIGHT_PER_MICROS,   // ~100 µs @ 100,000 items
         write: 200 * WEIGHT_PER_MICROS, // ~200 µs @ 100,000 items
