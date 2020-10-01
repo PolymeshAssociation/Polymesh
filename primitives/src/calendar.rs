eedb237fdbd03d3bd349075ce7e40447cffd9df8
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-10-01 12:08:25 +0100

    removed unreachable code

--- primitives/src/calendar.rs
+++ primitives/src/calendar.rs
@@ -6,7 +6,7 @@ use sp_std::convert::TryFrom;
 
 /// Calendar units for timing recurring operations.
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
+#[derive(Encode, Decode, Clone, Copy, Debug, PartialEq, Eq)]
 pub enum CalendarUnit {
     /// A unit of one second.
     Second,
@@ -30,9 +30,25 @@ impl Default for CalendarUnit {
     }
 }
 
+/// Calendar units that have variable length.
+pub enum VariableCalendarUnit {
+    /// A unit of one month.
+    Month,
+    /// A unit of one year.
+    Year,
+}
+
+/// Either a duration in seconds or a variable length calendar unit.
+pub enum FixedOrVariableCalendarUnit {
+    /// A fixed duration in seconds UTC, not counting leap seconds.
+    Fixed(u64),
+    /// A variable length calendar unit.
+    Variable(VariableCalendarUnit),
+}
+
 /// A simple period which is a multiple of a `CalendarUnit`.
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Encode, Decode, Clone, Debug, Default, PartialEq, Eq)]
+#[derive(Encode, Decode, Clone, Copy, Debug, Default, PartialEq, Eq)]
 pub struct CalendarPeriod {
     /// The base calendar unit.
     pub unit: CalendarUnit,
@@ -42,15 +58,20 @@ pub struct CalendarPeriod {
 
 impl CalendarPeriod {
     /// For fixed length calendar periods (in UTC, without leap seconds), computes the number of
-    /// seconds they contain. For variable length periods, returns `None`.
-    pub fn as_secs(&self) -> Option<u64> {
+    /// seconds they contain. For variable length periods, returns a `VariableLengthCalendarPeriod`.
+    pub fn as_fixed_or_variable(&self) -> FixedOrVariableCalendarUnit {
         match self.unit {
-            CalendarUnit::Second => Some(self.multiplier),
-            CalendarUnit::Minute => Some(self.multiplier * 60),
-            CalendarUnit::Hour => Some(self.multiplier * 60 * 60),
-            CalendarUnit::Day => Some(self.multiplier * 60 * 60 * 24),
-            CalendarUnit::Week => Some(self.multiplier * 60 * 60 * 24 * 7),
-            _ => None,
+            CalendarUnit::Second => FixedOrVariableCalendarUnit::Fixed(self.multiplier),
+            CalendarUnit::Minute => FixedOrVariableCalendarUnit::Fixed(self.multiplier * 60),
+            CalendarUnit::Hour => FixedOrVariableCalendarUnit::Fixed(self.multiplier * 60 * 60),
+            CalendarUnit::Day => FixedOrVariableCalendarUnit::Fixed(self.multiplier * 60 * 60 * 24),
+            CalendarUnit::Week => {
+                FixedOrVariableCalendarUnit::Fixed(self.multiplier * 60 * 60 * 24 * 7)
+            }
+            CalendarUnit::Month => {
+                FixedOrVariableCalendarUnit::Variable(VariableCalendarUnit::Month)
+            }
+            CalendarUnit::Year => FixedOrVariableCalendarUnit::Variable(VariableCalendarUnit::Year),
         }
     }
 }
@@ -58,7 +79,7 @@ impl CalendarPeriod {
 /// The schedule of an asset checkpoint containing the start time `start` and the optional period
 /// `period` in case the checkpoint is to recur after `start` at regular intervals.
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Encode, Decode, Clone, Debug, Default, PartialEq, Eq)]
+#[derive(Encode, Decode, Clone, Copy, Debug, Default, PartialEq, Eq)]
 pub struct CheckpointSchedule {
     /// Unix time in seconds (UTC).
     pub start: u64,
@@ -79,78 +100,91 @@ impl CheckpointSchedule {
             // The period is empty while the start time has already passed.
             return None;
         }
-        if let Some(period_as_secs) = self.period.as_secs() {
-            // The period is of fixed length in seconds UTC.
-            let secs_since_start = now_as_secs_utc - self.start;
-            let elapsed_periods: u64 = secs_since_start / period_as_secs;
-            return Some(secs_since_start + period_as_secs * (elapsed_periods + 1));
-        }
-        // The period is of variable length.
-        let date_time_start = NaiveDateTime::from_timestamp(i64::try_from(self.start).ok()?, 0);
-        let date_start = date_time_start.date();
-        let year_start = date_start.year();
-        let month_start = date_start.month();
-        let day_start = date_start.day();
-        let date_time_now = NaiveDateTime::from_timestamp(i64::try_from(now_as_secs_utc).ok()?, 0);
-        let date_now = date_time_now.date();
-        match self.period.unit {
-            CalendarUnit::Month => {
-                // Convert the multiplier to match the type of month.
-                let multiplier = u32::try_from(multiplier).ok()?;
-                let year_diff = u32::try_from(date_now.year() - year_start).ok()?;
-                // Convert months to base 12.
-                let month_start_base12 = month_start - 1;
-                let month_now_base12 = date_now.month() - 1;
-                let elapsed_months = year_diff * 12 + month_now_base12 - month_start_base12;
-                let elapsed_periods = elapsed_months / multiplier;
-                let next_period_months = multiplier * (elapsed_periods + 1);
-                // The month of the next period counting from the beginning of `year_start`.
-                let denormalized_next_period_month = month_start_base12 + next_period_months;
-                let next_period_year =
-                    year_start + i32::try_from(denormalized_next_period_month / 12).ok()?;
-                // Month in base 12.
-                let next_period_month = denormalized_next_period_month % 12;
-                let next_period_day = if matches!(next_period_month, 3 | 5 | 8 | 10) {
-                    // Handle 30-day months.
-                    u32::min(day_start, 30)
-                } else if next_period_month == 1 {
-                    // Handle February
-                    if is_leap_year(next_period_year) {
-                        u32::min(day_start, 29)
-                    } else {
-                        u32::min(day_start, 28)
+        match self.period.as_fixed_or_variable() {
+            FixedOrVariableCalendarUnit::Fixed(period_as_secs) => {
+                // The period is of fixed length in seconds UTC.
+                let secs_since_start = now_as_secs_utc - self.start;
+                let elapsed_periods: u64 = secs_since_start / period_as_secs;
+                Some(secs_since_start + period_as_secs * (elapsed_periods + 1))
+            }
+            FixedOrVariableCalendarUnit::Variable(variable_unit) => {
+                // The period is of variable length.
+                let date_time_start =
+                    NaiveDateTime::from_timestamp(i64::try_from(self.start).ok()?, 0);
+                let date_start = date_time_start.date();
+                let year_start = date_start.year();
+                let month_start = date_start.month();
+                let day_start = date_start.day();
+                let date_time_now =
+                    NaiveDateTime::from_timestamp(i64::try_from(now_as_secs_utc).ok()?, 0);
+                let date_now = date_time_now.date();
+                match variable_unit {
+                    VariableCalendarUnit::Month => {
+                        // Convert the multiplier to match the type of month.
+                        let multiplier = u32::try_from(multiplier).ok()?;
+                        let year_diff = u32::try_from(date_now.year() - year_start).ok()?;
+                        // Convert months to base 12.
+                        let month_start_base12 = month_start - 1;
+                        let month_now_base12 = date_now.month() - 1;
+                        let elapsed_months = year_diff * 12 + month_now_base12 - month_start_base12;
+                        let elapsed_periods = elapsed_months / multiplier;
+                        let next_period_months = multiplier * (elapsed_periods + 1);
+                        // The month of the next period counting from the beginning of `year_start`.
+                        let denormalized_next_period_month =
+                            month_start_base12 + next_period_months;
+                        let next_period_year =
+                            year_start + i32::try_from(denormalized_next_period_month / 12).ok()?;
+                        // Month in base 12.
+                        let next_period_month = denormalized_next_period_month % 12;
+                        let next_period_day = if matches!(next_period_month, 3 | 5 | 8 | 10) {
+                            // Handle 30-day months.
+                            u32::min(day_start, 30)
+                        } else if next_period_month == 1 {
+                            // Handle February
+                            if is_leap_year(next_period_year) {
+                                u32::min(day_start, 29)
+                            } else {
+                                u32::min(day_start, 28)
+                            }
+                        } else {
+                            day_start
+                        };
+                        let date_next = NaiveDate::from_ymd(
+                            i32::try_from(next_period_year).ok()?,
+                            // Convert months back to calendar numbering.
+                            next_period_month + 1,
+                            next_period_day,
+                        );
+                        Some(
+                            u64::try_from(date_next.and_time(date_time_start.time()).timestamp())
+                                .ok()?,
+                        )
                     }
-                } else {
-                    day_start
-                };
-                let date_next = NaiveDate::from_ymd(
-                    i32::try_from(next_period_year).ok()?,
-                    // Convert months back to calendar numbering.
-                    next_period_month + 1,
-                    next_period_day,
-                );
-                Some(u64::try_from(date_next.and_time(date_time_start.time()).timestamp()).ok()?)
+                    VariableCalendarUnit::Year => {
+                        // Convert the multiplier to match the type of year.
+                        let multiplier = i32::try_from(multiplier).ok()?;
+                        let elapsed_periods: i32 = (date_now.year() - year_start) / multiplier;
+                        let next_period_year = year_start + multiplier * (elapsed_periods + 1);
+                        let next_period_day = if month_start == 2 && !is_leap_year(next_period_year)
+                        {
+                            // Handle February in non-leap years.
+                            u32::min(day_start, 28)
+                        } else {
+                            day_start
+                        };
+                        let date_next = NaiveDate::from_ymd(
+                            next_period_year,
+                            // Convert months back to calendar numbering.
+                            month_start,
+                            next_period_day,
+                        );
+                        Some(
+                            u64::try_from(date_next.and_time(date_time_start.time()).timestamp())
+                                .ok()?,
+                        )
+                    }
+                }
             }
-            CalendarUnit::Year => {
-                // Convert the multiplier to match the type of year.
-                let multiplier = i32::try_from(multiplier).ok()?;
-                let elapsed_periods: i32 = (date_now.year() - year_start) / multiplier;
-                let next_period_year = year_start + multiplier * (elapsed_periods + 1);
-                let next_period_day = if month_start == 2 && !is_leap_year(next_period_year) {
-                    // Handle February in non-leap years.
-                    u32::min(day_start, 28)
-                } else {
-                    day_start
-                };
-                let date_next = NaiveDate::from_ymd(
-                    next_period_year,
-                    // Convert months back to calendar numbering.
-                    month_start,
-                    next_period_day,
-                );
-                Some(u64::try_from(date_next.and_time(date_time_start.time()).timestamp()).ok()?)
-            }
-            _ => unreachable!(),
         }
     }
 }
