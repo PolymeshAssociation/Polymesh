938b72ffa8863b8b879000b0ef51380ea58721ae
Adam Dossa <adam.dossa@gmail.com>
2020-10-06 12:39:07 +0100

    Move import to test cases

--- primitives/src/valid_proof_of_investor.rs
+++ primitives/src/valid_proof_of_investor.rs
@@ -1,4 +1,4 @@
-use crate::{CddId, Claim, IdentityId, InvestorZKProofData, Scope, Ticker};
+use crate::{CddId, Claim, IdentityId, InvestorZKProofData, Scope};
 use cryptography::claim_proofs::ProofPublicKey;
 use curve25519_dalek::ristretto::CompressedRistretto;
 
@@ -55,7 +55,7 @@ impl ValidProofOfInvestor {
 mod tests {
     use super::*;
     use crate::proposition::{exists, Proposition};
-    use crate::{Claim, Context, InvestorUid, InvestorZKProofData};
+    use crate::{Claim, Context, InvestorUid, InvestorZKProofData, Ticker};
     use cryptography::claim_proofs::{compute_cdd_id, compute_scope_id};
     use sp_std::convert::{From, TryFrom};
 
