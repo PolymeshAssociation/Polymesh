99aeb38e9ee08fb8bd1bdb76df1c4b2b33fdbb1c
Mudit Gupta <guptamudit@ymail.com>
2020-10-19 12:26:47 +0530

    Set default value of portfolionumber to 1

--- primitives/src/identity_id.rs
+++ primitives/src/identity_id.rs
@@ -185,7 +185,15 @@ impl Printable for IdentityId {
 pub struct PortfolioName(pub Vec<u8>);
 
 /// The unique ID of a non-default portfolio.
-pub type PortfolioNumber = u64;
+#[derive(Decode, Encode, Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct PortfolioNumber(pub u64);
+
+impl Default for PortfolioNumber {
+    fn default() -> Self {
+        Self(1)
+    }
+}
 
 /// TBD
 #[derive(Decode, Encode, Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
@@ -222,7 +230,7 @@ impl Printable for PortfolioId {
                 sp_io::misc::print_utf8(b"default");
             }
             PortfolioKind::User(num) => {
-                sp_io::misc::print_hex(&num.to_be_bytes());
+                sp_io::misc::print_num(num.0);
             }
         }
     }
