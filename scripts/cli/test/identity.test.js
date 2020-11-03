664a4a634599874f7108012cbc0ad353cac40d0a
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2020-11-03 14:27:21 -0400

    removed duplication

--- scripts/cli/test/identity.test.js
+++ scripts/cli/test/identity.test.js
@@ -8,31 +8,19 @@ describe('Halva test', () => {
   describe('Creating Identities', () => {
 
     it('Create Identity for Key(eventNotEmitted)', async () => {
-      const api = await reqImports.createApi();
-      const testEntities = await reqImports.initMain(api);
-      const primary_dev_seed = await reqImports.generateRandomKey(api);
-      const keys = await reqImports.generateKeys(api, 2, primary_dev_seed );
-
+      let {api, testEntities, _primary_dev_seed, keys} = await getSetup();
       const tx = api.tx.identity.cddRegisterDid(keys[0].address, []);
       await eventNotEmitted(tx,  'ExtrinsicFailed', 'system', '', testEntities[0]);
     });
 
     it('Create Identity for Key(eventEmitted)', async () => {
-      const api = await reqImports.createApi();
-      const testEntities = await reqImports.initMain(api);
-      const primary_dev_seed = await reqImports.generateRandomKey(api);
-      const keys = await reqImports.generateKeys(api, 2, primary_dev_seed );
-
+      let {api, testEntities, _primary_dev_seed, keys} = await getSetup();
       const tx = api.tx.identity.cddRegisterDid(keys[0].address, []);
       await eventEmitted(tx,  'ExtrinsicSuccess', 'system', '', testEntities[0]);
     });
 
     it('Create Identity for Key(passes)', async () => {
-      const api = await reqImports.createApi();
-      const testEntities = await reqImports.initMain(api);
-      const primary_dev_seed = await reqImports.generateRandomKey(api);
-      const keys = await reqImports.generateKeys(api, 2, primary_dev_seed );
-
+      let {api, testEntities, _primary_dev_seed, keys} = await getSetup();
       const tx = api.tx.identity.cddRegisterDid(keys[0].address, []);
       await passes(tx, '', testEntities[0]);
      
@@ -40,3 +28,12 @@ describe('Halva test', () => {
 
   });
 });
+
+async function getSetup() {
+  let api = await reqImports.createApi();
+  let testEntities = await reqImports.initMain(api);
+  let primary_dev_seed = await reqImports.generateRandomKey(api);
+  let keys = await reqImports.generateKeys(api, 2, primary_dev_seed );
+
+  return {api, testEntities, primary_dev_seed, keys};
+}
