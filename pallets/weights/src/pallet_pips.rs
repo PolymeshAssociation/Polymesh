6d9d8690559ba6129ffcfbdb2b1272e3692495bf
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-11-26 11:41:12 +0000

    updated the weight file

--- pallets/weights/src/pallet_pips.rs
+++ pallets/weights/src/pallet_pips.rs
@@ -8,108 +8,108 @@ use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 pub struct WeightInfo;
 impl pallet_pips::WeightInfo for WeightInfo {
     fn set_prune_historical_pips() -> Weight {
-        (68_007_000 as Weight)
+        (67_546_000 as Weight)
             .saturating_add(DbWeight::get().reads(1 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_min_proposal_deposit() -> Weight {
-        (68_218_000 as Weight)
+        (68_599_000 as Weight)
             .saturating_add(DbWeight::get().reads(1 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_proposal_cool_off_period() -> Weight {
-        (66_154_000 as Weight)
-            .saturating_add(DbWeight::get().reads(1 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
-    }
-    fn set_default_enactment_period() -> Weight {
         (67_206_000 as Weight)
             .saturating_add(DbWeight::get().reads(1 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
+    fn set_default_enactment_period() -> Weight {
+        (67_416_000 as Weight)
+            .saturating_add(DbWeight::get().reads(1 as Weight))
+            .saturating_add(DbWeight::get().writes(1 as Weight))
+    }
     fn set_pending_pip_expiry() -> Weight {
-        (68_217_000 as Weight)
+        (69_240_000 as Weight)
             .saturating_add(DbWeight::get().reads(1 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_max_pip_skip_count() -> Weight {
-        (66_135_000 as Weight)
+        (66_935_000 as Weight)
             .saturating_add(DbWeight::get().reads(1 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_active_pip_limit() -> Weight {
-        (66_895_000 as Weight)
+        (67_066_000 as Weight)
             .saturating_add(DbWeight::get().reads(1 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn propose_from_community() -> Weight {
-        (498_484_000 as Weight)
+        (502_902_000 as Weight)
             .saturating_add(DbWeight::get().reads(13 as Weight))
             .saturating_add(DbWeight::get().writes(9 as Weight))
     }
     fn propose_from_committee() -> Weight {
-        (296_135_000 as Weight)
+        (305_221_000 as Weight)
             .saturating_add(DbWeight::get().reads(8 as Weight))
             .saturating_add(DbWeight::get().writes(5 as Weight))
     }
     fn amend_proposal() -> Weight {
-        (274_915_000 as Weight)
+        (283_621_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn cancel_proposal() -> Weight {
-        (572_062_000 as Weight)
+        (590_064_000 as Weight)
             .saturating_add(DbWeight::get().reads(8 as Weight))
             .saturating_add(DbWeight::get().writes(5 as Weight))
     }
     fn vote() -> Weight {
-        (239_439_000 as Weight)
+        (222_567_000 as Weight)
             .saturating_add(DbWeight::get().reads(5 as Weight))
             .saturating_add(DbWeight::get().writes(3 as Weight))
     }
     fn approve_committee_proposal() -> Weight {
-        (245_299_000 as Weight)
+        (252_865_000 as Weight)
             .saturating_add(DbWeight::get().reads(4 as Weight))
             .saturating_add(DbWeight::get().writes(4 as Weight))
     }
     fn reject_proposal() -> Weight {
-        (303_388_000 as Weight)
+        (309_019_000 as Weight)
             .saturating_add(DbWeight::get().reads(8 as Weight))
             .saturating_add(DbWeight::get().writes(5 as Weight))
     }
     fn prune_proposal() -> Weight {
-        (168_897_000 as Weight)
+        (172_694_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(5 as Weight))
     }
     fn reschedule_execution() -> Weight {
-        (297_075_000 as Weight)
+        (302_346_000 as Weight)
             .saturating_add(DbWeight::get().reads(8 as Weight))
             .saturating_add(DbWeight::get().writes(6 as Weight))
     }
     fn clear_snapshot() -> Weight {
-        (59_221_000 as Weight)
+        (60_313_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(2 as Weight))
     }
     fn snapshot() -> Weight {
-        (6_469_043_000 as Weight)
+        (6_764_184_000 as Weight)
             .saturating_add(DbWeight::get().reads(204 as Weight))
             .saturating_add(DbWeight::get().writes(3 as Weight))
     }
     fn enact_snapshot_results() -> Weight {
-        (299_743_388_000 as Weight)
-            .saturating_add(DbWeight::get().reads(5434 as Weight))
-            .saturating_add(DbWeight::get().writes(5430 as Weight))
+        (1_223_380_619_000 as Weight)
+            .saturating_add(DbWeight::get().reads(21007 as Weight))
+            .saturating_add(DbWeight::get().writes(21003 as Weight))
     }
     fn execute_scheduled_pip() -> Weight {
-        (11_646_657_000 as Weight)
-            .saturating_add(DbWeight::get().reads(305 as Weight))
-            .saturating_add(DbWeight::get().writes(303 as Weight))
+        (26_442_104_000 as Weight)
+            .saturating_add(DbWeight::get().reads(1205 as Weight))
+            .saturating_add(DbWeight::get().writes(1203 as Weight))
     }
     fn expire_scheduled_pip() -> Weight {
-        (6_659_690_000 as Weight)
-            .saturating_add(DbWeight::get().reads(304 as Weight))
-            .saturating_add(DbWeight::get().writes(302 as Weight))
+        (26_742_547_000 as Weight)
+            .saturating_add(DbWeight::get().reads(1204 as Weight))
+            .saturating_add(DbWeight::get().writes(1202 as Weight))
     }
 }
