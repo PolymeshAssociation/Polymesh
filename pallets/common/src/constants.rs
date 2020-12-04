1937a493c39fb11286b023fbdb9c2b9f53a411c6
Mudit Gupta <guptamudit@ymail.com>
2020-12-04 10:58:20 +0530

    fixed error codes

--- pallets/common/src/constants.rs
+++ pallets/common/src/constants.rs
@@ -56,8 +56,8 @@ pub const APP_BLOCKED_TX: u8 = 0xa6;
 pub const APP_FUNDS_LOCKED: u8 = 0xa7;
 pub const APP_FUNDS_LIMIT_REACHED: u8 = 0xa8;
 pub const PORTFOLIO_FAILURE: u8 = 0xa9;
-pub const CUSTODIAN_ERROR: u8 = 0xb0;
-pub const SCOPE_CLAIM_MISSING: u8 = 0xb1;
+pub const CUSTODIAN_ERROR: u8 = 0xaa;
+pub const SCOPE_CLAIM_MISSING: u8 = 0xab;
 
 // PIP pallet constants.
 pub const PIP_MAX_REPORTING_SIZE: usize = 1024;
