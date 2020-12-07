2c71e0a6824e4b6de8840f6100287b3d81f33a36
Mudit Gupta <guptamudit@ymail.com>
2020-12-07 20:24:00 +0530

    Backporting some minor fixes to tooling (#795)
    * More minor fixes (#793)
    
    * changed remove_default_trusted_claim_issuer to take issuer identity instead of full object
    
    * verify iu claim only for the receiver
    
    * bumped runtime version

--- pallets/asset/src/lib.rs
+++ pallets/asset/src/lib.rs
@@ -1499,11 +1499,7 @@ impl<T: Trait> Module<T> {
             return Ok((ERC1400_TRANSFERS_HALTED, T::DbWeight::get().reads(1)));
         }
 
-        if !Identity::<T>::verify_scope_claims_for_transfer(
-            ticker,
-            from_portfolio.did,
-            to_portfolio.did,
-        ) {
+        if !Identity::<T>::verify_iu_claim(*ticker, to_portfolio.did) {
             return Ok((SCOPE_CLAIM_MISSING, T::DbWeight::get().reads(2)));
         }
 
@@ -2013,11 +2009,7 @@ impl<T: Trait> Module<T> {
             return Ok(INVALID_SENDER_DID);
         }
 
-        if !Identity::<T>::verify_scope_claims_for_transfer(
-            ticker,
-            from_portfolio.did,
-            to_portfolio.did,
-        ) {
+        if !Identity::<T>::verify_iu_claim(*ticker, to_portfolio.did) {
             return Ok(SCOPE_CLAIM_MISSING);
         }
 
