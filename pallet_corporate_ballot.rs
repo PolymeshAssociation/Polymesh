3ecaba214803efd48b72b26e2d2542bc4d4e05dc
Mazdak Farrokhzad <twingoow@gmail.com>
2020-12-10 09:02:46 +0100

    mesh-1387: bench change_end

--- pallet_corporate_ballot.rs
+++ pallet_corporate_ballot.rs
@@ -8,10 +8,15 @@ use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 pub struct WeightInfo;
 impl pallet_corporate_ballot::WeightInfo for WeightInfo {
 	fn attach_ballot(i: u32, j: u32, ) -> Weight {
-		(48_436_000 as Weight)
-			.saturating_add((323_000 as Weight).saturating_mul(i as Weight))
-			.saturating_add((241_000 as Weight).saturating_mul(j as Weight))
+		(46_767_000 as Weight)
+			.saturating_add((422_000 as Weight).saturating_mul(i as Weight))
+			.saturating_add((344_000 as Weight).saturating_mul(j as Weight))
 			.saturating_add(DbWeight::get().reads(11 as Weight))
 			.saturating_add(DbWeight::get().writes(4 as Weight))
+	}
+	fn change_end() -> Weight {
+		(39_102_000 as Weight)
+			.saturating_add(DbWeight::get().reads(8 as Weight))
+			.saturating_add(DbWeight::get().writes(1 as Weight))
 	}
 }
--- pallets/corporate-actions/src/ballot/benchmarking.rs
+++ pallets/corporate-actions/src/ballot/benchmarking.rs
@@ -18,10 +18,31 @@
 use super::*;
 use core::iter;
 use crate::benchmarking::setup_ca;
+use pallet_identity::benchmarking::User;
 use frame_benchmarking::benchmarks;
+use frame_support::assert_ok;
 
 const MAX_MOTIONS: u32 = 10;
 const MAX_CHOICES: u32 = 10;
+
+const RANGE: BallotTimeRange = BallotTimeRange { start: 3000, end: 4000 };
+
+fn meta(i: u32, j: u32) -> BallotMeta {
+    let motion = Motion {
+        title: "".into(),
+        info_link: "".into(),
+        choices: iter::repeat("".into()).take(j as usize).collect(),
+    };
+    let motions = iter::repeat(motion).take(i as usize).collect();
+    BallotMeta { title: "".into(), motions }
+}
+
+fn attach<T: Trait>(i: u32, j: u32) -> (User<T>, CAId) {
+    let meta = meta(i, j);
+    let (owner, ca_id) = setup_ca::<T>(CAKind::IssuerNotice);
+    assert_ok!(<Module<T>>::attach_ballot(owner.origin().into(), ca_id, RANGE, meta, true));
+    (owner, ca_id)
+}
 
 benchmarks! {
     _ {}
@@ -30,18 +51,17 @@ benchmarks! {
         let i in 0..MAX_MOTIONS;
         let j in 0..MAX_CHOICES;
 
-        let motion = Motion {
-            title: "".into(),
-            info_link: "".into(),
-            choices: iter::repeat("".into()).take(j as usize).collect(),
-        };
-        let motions = iter::repeat(motion).take(i as usize).collect();
-
-        let meta = BallotMeta { title: "".into(), motions };
-        let range = BallotTimeRange { start: 3000, end: 4000 };
+        let meta = meta(i, j);
         let (owner, ca_id) = setup_ca::<T>(CAKind::IssuerNotice);
-    }: _(owner.origin(), ca_id, range, meta, true)
+    }: _(owner.origin(), ca_id, RANGE, meta, true)
     verify {
-        ensure!(TimeRanges::get(ca_id) == Some(range), "ballot not created");
+        ensure!(TimeRanges::get(ca_id) == Some(RANGE), "ballot not created");
+    }
+
+    change_end {
+        let (owner, ca_id) = attach::<T>(0, 0);
+    }: _(owner.origin(), ca_id, 5000)
+    verify {
+        ensure!(TimeRanges::get(ca_id).unwrap().end == 5000, "range not changed");
     }
 }
