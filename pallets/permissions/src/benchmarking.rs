6b2d2856cd22bd3eee45d340425fd3912ea2c757
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-12-21 19:56:13 +0100

    Add benchmarks to permissions

---
+++ pallets/permissions/src/benchmarking.rs
@@ -0,0 +1,45 @@
+use crate::*;
+
+use polymesh_primitives::{DispatchableName, PalletName};
+
+use frame_benchmarking::benchmarks;
+use sp_std::{iter, prelude::*};
+
+const MAX_PALLET_NAME: u32 = 512;
+const MAX_DISPATCHABLE_NAME: u32 = 1024;
+
+fn make_name(m: u32) -> Vec<u8> {
+    iter::repeat(b'x').take(m as usize).collect::<Vec<_>>()
+}
+
+benchmarks! {
+    _ {}
+
+    set_call_metadata {
+        let p in 1..MAX_PALLET_NAME;
+        let d in 1..MAX_DISPATCHABLE_NAME;
+
+        let pallet_name :PalletName = make_name(p).into();
+        let pallet_name_exp = pallet_name.clone();
+        let dispatchable_name :DispatchableName = make_name(p).into();
+        let dispatchable_name_exp = dispatchable_name.clone();
+
+    }: {
+        StoreCallMetadata::<T>::set_call_metadata(pallet_name, dispatchable_name);
+    }
+    verify {
+        assert_eq!( CurrentPalletName::get(), pallet_name_exp);
+        assert_eq!( CurrentDispatchableName::get(), dispatchable_name_exp);
+    }
+
+    clear_call_metadata {
+        let pallet_name :PalletName = make_name(MAX_PALLET_NAME).into();
+        let dispatchable_name :DispatchableName = make_name(MAX_DISPATCHABLE_NAME).into();
+        StoreCallMetadata::<T>::set_call_metadata(pallet_name, dispatchable_name);
+    }: {
+        StoreCallMetadata::<T>::clear_call_metadata();
+    }
+    verify {
+        assert_eq!(CurrentPalletName::exists(), false);
+    }
+}
--- pallets/runtime/develop/src/runtime.rs
+++ pallets/runtime/develop/src/runtime.rs
@@ -1310,6 +1310,7 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, pallet_treasury, Treasury);
             add_benchmark!(params, batches, pallet_im_online, ImOnline);
             add_benchmark!(params, batches, pallet_group, CddServiceProviders);
+            add_benchmark!(params, batches, pallet_permissions, Permissions);
 
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
             Ok(batches)
