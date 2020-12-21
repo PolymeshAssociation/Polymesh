761e9831b3682ffaebf90712ce48e0045a5aeab1
Mudit Gupta <guptamudit@ymail.com>
2020-12-21 14:15:07 +0530

    config changes

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -667,15 +667,17 @@ fn alcyone_testnet_genesis(
                 ..Default::default()
             },
         }),
-        group_Instance1: Some(committee_membership!(3, 4, 5)),
-        committee_Instance1: Some(committee!(6, (2, 3))),
-        group_Instance2: Some(cdd_membership!(1, 2, 6)), // sp1, sp2, sp3
+        // Governing council
+        group_Instance1: Some(committee_membership!(3, 4, 5)), //gc1,gc2,gc3
+        committee_Instance1: Some(committee!(3, (2, 3))),
+        // CDD providers
+        group_Instance2: Some(cdd_membership!(1, 2, 3)), // sp1, sp2, gc1
         // Technical Committee:
-        group_Instance3: Some(committee_membership!(3)),
+        group_Instance3: Some(committee_membership!(3)), //gc1
         committee_Instance3: Some(committee!(3)),
         // Upgrade Committee:
-        group_Instance4: Some(committee_membership!(4)),
-        committee_Instance4: Some(committee!(4)),
+        group_Instance4: Some(committee_membership!(3)), //gc1
+        committee_Instance4: Some(committee!(3)),
         protocol_fee: Some(protocol_fee!()),
         settlement: Some(Default::default()),
         multisig: Some(MULTISIG),
