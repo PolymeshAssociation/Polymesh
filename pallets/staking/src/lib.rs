5e55dda3c73915247f89e15a246d7e9aab4180ab
satyam <satyam@secureblocks.io>
2020-12-23 14:52:45 +0530

    port 2.0 release changes of substrate staking pallet

--- pallets/staking/src/lib.rs
+++ pallets/staking/src/lib.rs
@@ -463,16 +463,18 @@ pub enum StakerStatus<AccountId> {
 
 /// A destination account for payment.
 #[derive(PartialEq, Eq, Copy, Clone, Encode, Decode, RuntimeDebug)]
-pub enum RewardDestination {
+pub enum RewardDestination<AccountId> {
     /// Pay into the stash account, increasing the amount at stake accordingly.
     Staked,
     /// Pay into the stash account, not increasing the amount at stake.
     Stash,
     /// Pay into the controller account.
     Controller,
+    /// Pay into a specified account.
+    Account(AccountId),
 }
 
-impl Default for RewardDestination {
+impl<AccountId> Default for RewardDestination<AccountId> {
     fn default() -> Self {
         RewardDestination::Staked
     }
@@ -1215,7 +1217,7 @@ decl_storage! {
             => Option<StakingLedger<T::AccountId, BalanceOf<T>>>;
 
         /// Where the reward payment should be made. Keyed by stash.
-        pub Payee get(fn payee): map hasher(twox_64_concat) T::AccountId => RewardDestination;
+        pub Payee get(fn payee): map hasher(twox_64_concat) T::AccountId => RewardDestination<T::AccountId>;
 
         /// The map from (wannabe) validator stash key to the preferences of that validator.
         pub Validators get(fn validators):
@@ -1732,7 +1734,7 @@ decl_module! {
         pub fn bond(origin,
             controller: <T::Lookup as StaticLookup>::Source,
             #[compact] value: BalanceOf<T>,
-            payee: RewardDestination,
+            payee: RewardDestination<T::AccountId>,
         ) {
             let stash = ensure_signed(origin)?;
             ensure!(!<Bonded<T>>::contains_key(&stash), Error::<T>::AlreadyBonded);
@@ -2070,7 +2072,7 @@ decl_module! {
         ///     - Write: Payee
         /// # </weight>
         #[weight = 11 * WEIGHT_PER_MICROS + T::DbWeight::get().reads_writes(1, 1)]
-        pub fn set_payee(origin, payee: RewardDestination) {
+        pub fn set_payee(origin, payee: RewardDestination<T::AccountId>) {
             let controller = ensure_signed(origin)?;
             let ledger = Self::ledger(&controller).ok_or(Error::<T>::NotController)?;
             let stash = &ledger.stash;
@@ -2870,6 +2872,9 @@ impl<T: Trait> Module<T> {
                     Self::update_ledger(&controller, &l);
                     r
                 }),
+            RewardDestination::Account(dest_account) => {
+                Some(T::Currency::deposit_creating(&dest_account, amount))
+            }
         }
     }
 
