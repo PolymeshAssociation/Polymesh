d53d2eeb5badcde02bc981806b88b50178bcb47f
Mazdak Farrokhzad <twingoow@gmail.com>
2020-12-28 19:12:46 +0100

    mesh-1433: fix benchmarks

--- pallets/weights/src/pallet_capital_distribution.rs
+++ pallets/weights/src/pallet_capital_distribution.rs
@@ -8,31 +8,31 @@ use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 pub struct WeightInfo;
 impl pallet_corporate_actions::distribution::WeightInfo for WeightInfo {
 	fn distribute() -> Weight {
-		(60_889_000 as Weight)
+		(63_053_000 as Weight)
 			.saturating_add(DbWeight::get().reads(15 as Weight))
 			.saturating_add(DbWeight::get().writes(2 as Weight))
 	}
 	fn claim(i: u32, j: u32, ) -> Weight {
-		(194_656_000 as Weight)
-			.saturating_add((266_000 as Weight).saturating_mul(i as Weight))
-			.saturating_add((145_000 as Weight).saturating_mul(j as Weight))
+		(205_654_000 as Weight)
+			.saturating_add((246_000 as Weight).saturating_mul(i as Weight))
+			.saturating_add((128_000 as Weight).saturating_mul(j as Weight))
 			.saturating_add(DbWeight::get().reads(31 as Weight))
 			.saturating_add(DbWeight::get().writes(11 as Weight))
 	}
 	fn push_benefit(i: u32, j: u32, ) -> Weight {
-		(193_172_000 as Weight)
-			.saturating_add((272_000 as Weight).saturating_mul(i as Weight))
-			.saturating_add((148_000 as Weight).saturating_mul(j as Weight))
+		(210_363_000 as Weight)
+			.saturating_add((240_000 as Weight).saturating_mul(i as Weight))
+			.saturating_add((128_000 as Weight).saturating_mul(j as Weight))
 			.saturating_add(DbWeight::get().reads(31 as Weight))
 			.saturating_add(DbWeight::get().writes(11 as Weight))
 	}
 	fn reclaim() -> Weight {
-		(40_798_000 as Weight)
+		(40_246_000 as Weight)
 			.saturating_add(DbWeight::get().reads(7 as Weight))
 			.saturating_add(DbWeight::get().writes(2 as Weight))
 	}
 	fn remove_distribution() -> Weight {
-		(47_975_000 as Weight)
+		(47_413_000 as Weight)
 			.saturating_add(DbWeight::get().reads(9 as Weight))
 			.saturating_add(DbWeight::get().writes(2 as Weight))
 	}
--- pallets/weights/src/pallet_corporate_actions.rs
+++ pallets/weights/src/pallet_corporate_actions.rs
@@ -3,74 +3,75 @@
 #![allow(unused_parens)]
 #![allow(unused_imports)]
 
-use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
+use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 
 pub struct WeightInfo;
 impl pallet_corporate_actions::WeightInfo for WeightInfo {
-    fn set_max_details_length() -> Weight {
-        (9_668_000 as Weight).saturating_add(DbWeight::get().writes(1 as Weight))
-    }
-    fn reset_caa() -> Weight {
-        (34_516_000 as Weight)
-            .saturating_add(DbWeight::get().reads(6 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
-    }
-    fn set_default_targets(i: u32) -> Weight {
-        (36_723_000 as Weight)
-            .saturating_add((157_000 as Weight).saturating_mul(i as Weight))
-            .saturating_add(DbWeight::get().reads(7 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
-    }
-    fn set_default_withholding_tax() -> Weight {
-        (34_840_000 as Weight)
-            .saturating_add(DbWeight::get().reads(7 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
-    }
-    fn set_did_withholding_tax(i: u32) -> Weight {
-        (39_758_000 as Weight)
-            .saturating_add((175_000 as Weight).saturating_mul(i as Weight))
-            .saturating_add(DbWeight::get().reads(7 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
-    }
-    fn initiate_corporate_action_use_defaults(j: u32, k: u32) -> Weight {
-        (131_546_000 as Weight)
-            .saturating_add((141_000 as Weight).saturating_mul(j as Weight))
-            .saturating_add((128_000 as Weight).saturating_mul(k as Weight))
-            .saturating_add(DbWeight::get().reads(17 as Weight))
-            .saturating_add(DbWeight::get().writes(5 as Weight))
-    }
-    fn initiate_corporate_action_provided(j: u32, k: u32) -> Weight {
-        (111_594_000 as Weight)
-            .saturating_add((138_000 as Weight).saturating_mul(j as Weight))
-            .saturating_add((109_000 as Weight).saturating_mul(k as Weight))
-            .saturating_add(DbWeight::get().reads(14 as Weight))
-            .saturating_add(DbWeight::get().writes(5 as Weight))
-    }
-    fn link_ca_doc(i: u32) -> Weight {
-        (0 as Weight)
-            .saturating_add((4_895_000 as Weight).saturating_mul(i as Weight))
-            .saturating_add(DbWeight::get().reads(8 as Weight))
-            .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(i as Weight)))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
-    }
-    fn remove_ca_with_ballot() -> Weight {
-        (49_633_000 as Weight)
-            .saturating_add(DbWeight::get().reads(9 as Weight))
-            .saturating_add(DbWeight::get().writes(6 as Weight))
-    }
-    fn remove_ca_with_dist() -> Weight {
-        (53_916_000 as Weight)
-            .saturating_add(DbWeight::get().reads(10 as Weight))
-            .saturating_add(DbWeight::get().writes(4 as Weight))
-    }
-    fn change_record_date_with_ballot() -> Weight {
-        (64_530_000 as Weight)
-            .saturating_add(DbWeight::get().reads(14 as Weight))
-            .saturating_add(DbWeight::get().writes(4 as Weight))
-    }
-    fn change_record_date_with_dist() -> Weight {
-        (66_484_000 as Weight)
-            .saturating_add(DbWeight::get().reads(14 as Weight))
-            .saturating_add(DbWeight::get().writes(4 as Weight))
-    }
+	fn set_max_details_length() -> Weight {
+		(9_740_000 as Weight)
+			.saturating_add(DbWeight::get().writes(1 as Weight))
+	}
+	fn reset_caa() -> Weight {
+		(35_354_000 as Weight)
+			.saturating_add(DbWeight::get().reads(6 as Weight))
+			.saturating_add(DbWeight::get().writes(1 as Weight))
+	}
+	fn set_default_targets(i: u32, ) -> Weight {
+		(43_850_000 as Weight)
+			.saturating_add((144_000 as Weight).saturating_mul(i as Weight))
+			.saturating_add(DbWeight::get().reads(7 as Weight))
+			.saturating_add(DbWeight::get().writes(1 as Weight))
+	}
+	fn set_default_withholding_tax() -> Weight {
+		(35_105_000 as Weight)
+			.saturating_add(DbWeight::get().reads(7 as Weight))
+			.saturating_add(DbWeight::get().writes(1 as Weight))
+	}
+	fn set_did_withholding_tax(i: u32, ) -> Weight {
+		(39_678_000 as Weight)
+			.saturating_add((186_000 as Weight).saturating_mul(i as Weight))
+			.saturating_add(DbWeight::get().reads(7 as Weight))
+			.saturating_add(DbWeight::get().writes(1 as Weight))
+	}
+	fn initiate_corporate_action_use_defaults(j: u32, k: u32, ) -> Weight {
+		(103_340_000 as Weight)
+			.saturating_add((171_000 as Weight).saturating_mul(j as Weight))
+			.saturating_add((127_000 as Weight).saturating_mul(k as Weight))
+			.saturating_add(DbWeight::get().reads(17 as Weight))
+			.saturating_add(DbWeight::get().writes(5 as Weight))
+	}
+	fn initiate_corporate_action_provided(j: u32, k: u32, ) -> Weight {
+		(101_926_000 as Weight)
+			.saturating_add((150_000 as Weight).saturating_mul(j as Weight))
+			.saturating_add((97_000 as Weight).saturating_mul(k as Weight))
+			.saturating_add(DbWeight::get().reads(14 as Weight))
+			.saturating_add(DbWeight::get().writes(5 as Weight))
+	}
+	fn link_ca_doc(i: u32, ) -> Weight {
+		(0 as Weight)
+			.saturating_add((4_891_000 as Weight).saturating_mul(i as Weight))
+			.saturating_add(DbWeight::get().reads(8 as Weight))
+			.saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(i as Weight)))
+			.saturating_add(DbWeight::get().writes(1 as Weight))
+	}
+	fn remove_ca_with_ballot() -> Weight {
+		(50_362_000 as Weight)
+			.saturating_add(DbWeight::get().reads(9 as Weight))
+			.saturating_add(DbWeight::get().writes(6 as Weight))
+	}
+	fn remove_ca_with_dist() -> Weight {
+		(53_469_000 as Weight)
+			.saturating_add(DbWeight::get().reads(10 as Weight))
+			.saturating_add(DbWeight::get().writes(4 as Weight))
+	}
+	fn change_record_date_with_ballot() -> Weight {
+		(64_885_000 as Weight)
+			.saturating_add(DbWeight::get().reads(14 as Weight))
+			.saturating_add(DbWeight::get().writes(4 as Weight))
+	}
+	fn change_record_date_with_dist() -> Weight {
+		(66_906_000 as Weight)
+			.saturating_add(DbWeight::get().reads(14 as Weight))
+			.saturating_add(DbWeight::get().writes(4 as Weight))
+	}
 }
