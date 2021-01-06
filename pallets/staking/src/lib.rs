7e02565c801a4fcd98c0dd877693cf781c512cb4
Satyam Agrawal <satyam0499@gmail.com>
2021-01-07 02:04:38 +0530

    Allow ceiling multiplication in staking (#846)
    * allow ceiling multiplication
    
    * specialize the below than 1 case
    
    * fix linting
    
    * apply mudit's suggestion
    
    * fix bug
    
    Co-authored-by: satyam <satyam@secureblocks.io>
    Co-authored-by: Adam Dossa <adam.dossa@gmail.com>
    Co-authored-by: poly-auto-merge[bot] <65769705+poly-auto-merge[bot]@users.noreply.github.com>

--- pallets/staking/src/lib.rs
+++ pallets/staking/src/lib.rs
@@ -2758,7 +2758,7 @@ decl_module! {
 impl<T: Trait> Module<T> {
     /// Returns the allowed validator count.
     fn get_allowed_validator_count() -> u32 {
-        T::MaxValidatorPerIdentity::get() * Self::validator_count()
+        (T::MaxValidatorPerIdentity::get() * Self::validator_count()).max(1)
     }
 
     /// Returns the `T::Origin` for given target AccountId.
