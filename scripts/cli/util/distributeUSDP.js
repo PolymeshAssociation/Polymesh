c52f896ac920406227e402ba139688b106b8a6d9
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2021-01-19 18:22:19 -0400

    minor changes

--- scripts/cli/util/distributeUSDP.js
+++ scripts/cli/util/distributeUSDP.js
@@ -3,25 +3,28 @@ require = require("esm")(module /*, options*/);
 module.exports = require("../util/init.js");
 
 let { reqImports } = require("../util/init.js");
-const args = require('minimist')(process.argv.slice(2));
+const minimist = require('minimist');
+const args = minimist(process.argv.slice(2), {
+    string: 'account'
+});
 
 async function main() {
 
-    const ticker = args['ticker'];
-    const account = reqImports.generateEntityFromUri(args['account']);
-    const amount = args['amount'];
-    const venueId = args['venueId'];
+    const api = await reqImports.createApi(); 
+    const ticker = args.ticker; 
+    const account = await reqImports.generateEntityFromUri(api, args.account);  
+    const amount = args.amount; 
+    const venueId = args.venueId;
 
-    const api = await reqImports.createApi();
-
-    const listOfDids = await api.query.identity.didRecords();
-
+    const listOfDids = await api.query.identity.didRecords.entries();
+    
     await reqImports.issueTokenPerDid(api, [account], ticker, 100_000_000_000_000, null);
-
+    
     await reqImports.addComplianceRequirement(api, account, ticker);
-
+    
     await batchAtomic(api, account, listOfDids, amount, ticker, venueId);
 
+    process.exit();
 }
 
 async function batchAtomic(api, sender, receivers, amount, ticker, venueId) {
@@ -30,25 +33,26 @@ async function batchAtomic(api, sender, receivers, amount, ticker, venueId) {
     let senderDid = await api.query.identity.keyToIdentityIds(sender.publicKey);
 
     for (i = 0; i < receivers.length; i++) {
-        let tx = await addAndAffirmInstruction(api, venueId, senderDid, receivers[i], ticker, amount);
+        let tx = await addAndAffirmInstruction(api, venueId, senderDid, receivers[i][0], ticker, amount);
         txBatch.push(tx);
     }
 
-    let completeBatchTx = await api.tx.utility.batchAtomic(txBatch);
-    await reqImports.sendTx(sender, completeBatchTx);
+    let completeBatchTx = await api.tx.utility.batchAtomic(txBatch);  
+    await reqImports.sendTx(sender, completeBatchTx);  
 }
 
-async function addAndAffirmInstruction(api, venueId, senderDid, receiverDid, ticker, amount ) {
+async function addAndAffirmInstruction(api, venueId, senderDid, receiverDid, ticker, amount) {
 
-    let senderPortfolio = getDefaultPortfolio(senderDid);
-    
+    let senderPortfolio = reqImports.getDefaultPortfolio(senderDid);
+    let receiverPortfolio = reqImports.getDefaultPortfolio(receiverDid);
+
     let leg = {
         from: senderPortfolio,
-        to: getDefaultPortfolio(receiverDid),
+        to: receiverPortfolio,
         asset: ticker,
         amount: amount,
       };
-
+     
    return await api.tx.settlement.addAndAffirmInstruction(venueId, 0, null, null, [leg], [senderPortfolio]);
 }
 
