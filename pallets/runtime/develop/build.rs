7592ef0267989a995a71d62108faca62b5d81efb
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2021-01-19 19:23:32 +0100

    CodeReview: Remove commented code

--- pallets/runtime/develop/build.rs
+++ pallets/runtime/develop/build.rs
@@ -1,7 +1,6 @@
 use wasm_builder_runner::WasmBuilder;
 
 const BUILDER_REPO: &str = "https://github.com/PolymathNetwork/substrate-wasm-builder.git";
-// const BUILDER_REV: &str = "v2.0.0-1";
 const BUILDER_REV: &str = "b0303c15b662dd768bfe808e3ed47ec65305fe2a";
 
 fn main() {
