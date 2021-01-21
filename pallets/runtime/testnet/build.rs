a01100ccac863d07a1418fc8d70e121299118e98
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2021-01-21 16:07:53 +0100

    CodeReview: Dedup build

--- pallets/runtime/testnet/build.rs
+++ pallets/runtime/testnet/build.rs
@@ -1,13 +1,3 @@
-use wasm_builder_runner::WasmBuilder;
-
-const BUILDER_REPO: &str = "https://github.com/PolymathNetwork/substrate-wasm-builder.git";
-const BUILDER_REV: &str = "b0303c15b662dd768bfe808e3ed47ec65305fe2a";
-
 fn main() {
-    WasmBuilder::new()
-        .with_current_project()
-        .with_wasm_builder_from_git(BUILDER_REPO, BUILDER_REV)
-        .export_heap_base()
-        .import_memory()
-        .build()
+    polymesh_build_tool::build()
 }
