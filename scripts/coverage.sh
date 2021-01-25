e55775752fac59870447a38ea428b7a6f566e086
Mudit Gupta <guptamudit@ymail.com>
2021-01-25 18:28:17 +0530

    wip

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,19 +1,19 @@
-RUSTFLAGS="-Zinstrument-coverage" \
-LLVM_PROFILE_FILE="json5format-%m.profraw" \
-SKIP_WASM_BUILD=1 \
-cargo test --tests \
-    --package polymesh-runtime-tests \
-    --features default_identity \
-    --package pallet-staking \
-    --package pallet-balances:0.1.0 \
-    --package polymesh-primitives \
-    --package pallet-transaction-payment
+# RUSTFLAGS="-Zinstrument-coverage" \
+# LLVM_PROFILE_FILE="json5format-%m.profraw" \
+# SKIP_WASM_BUILD=1 \
+# cargo test --tests \
+#     --package polymesh-runtime-tests \
+#     --features default_identity \
+#     --package pallet-staking \
+#     --package pallet-balances:0.1.0 \
+#     --package polymesh-primitives \
+#     --package pallet-transaction-payment
 
-cargo profdata -- merge \
-    -sparse $(find . -name 'json5format-*.profraw') -o json5format.profdata
+# cargo profdata -- merge \
+#     -sparse $(find . -name 'json5format-*.profraw') -o json5format.profdata
 
 if [[ -z "${DEPLOY_ENV}" ]]; then
-    cargo cov -- show \
+    cargo cov -- export \
     $( \
         for file in \
             $( \
@@ -38,7 +38,9 @@ if [[ -z "${DEPLOY_ENV}" ]]; then
     --ignore-filename-regex='/.cargo/git/' \
     --ignore-filename-regex='/target/debug/' \
     --ignore-filename-regex='/tests/' \
-    --ignore-filename-regex='/rustc/' > coverage.txt
+    --ignore-filename-regex='/rustc/' > coverage.json
+
+    bash <(curl -s https://codecov.io/bash)
 else
     cargo cov -- report \
     $( \
