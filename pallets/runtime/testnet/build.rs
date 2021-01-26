838c5dd54d8148401aff0e40e583a4f4b6efd789
Francisco Miguel García <fmiguelgarcia@users.noreply.github.com>
2021-01-26 13:32:53 +0100

    Wasm Builder as external crate (#876)
    * Use external wasm builder
    
    * Use builder revision
    
    * Fix `syn::export::TokenStream2` error
    
    * Update WASM_BINARY generated type
    
    * CodeReview: Remove commented code
    
    * CodeReview: Panic if wasm binary was not generated
    
    * CodeReview: Dedup build

--- pallets/runtime/testnet/build.rs
+++ pallets/runtime/testnet/build.rs
@@ -1,10 +1,3 @@
-use wasm_builder_runner::WasmBuilder;
-
 fn main() {
-    WasmBuilder::new()
-        .with_current_project()
-        .with_wasm_builder_from_path("../../../utils/wasm-builder")
-        .export_heap_base()
-        .import_memory()
-        .build()
+    polymesh_build_tool::build()
 }
