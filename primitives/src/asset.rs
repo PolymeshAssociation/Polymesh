a23095fa860c3ac17f4ccc624da0928378fa3ec1
Vladimir Komendantskiy <komendantsky@gmail.com>
2021-02-03 16:36:52 +0000

    added asset in primitives

---
+++ primitives/src/asset.rs
@@ -0,0 +1,64 @@
+// This file is part of the Polymesh distribution (https://github.com/PolymathNetwork/Polymesh).
+// Copyright (c) 2020 Polymath
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+use super::IdentityId;
+use codec::{Decode, Encode};
+use polymesh_primitives_derive::VecU8StrongTyped;
+use sp_std::prelude::Vec;
+
+pub const GAS_LIMIT: u64 = 13_000_000_000;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq, Debug)]
+pub struct IssueAssetItem<U> {
+    pub investor_did: IdentityId,
+    pub value: U,
+}
+
+/// A wrapper for a token name.
+#[derive(
+    Decode, Encode, Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, VecU8StrongTyped,
+)]
+pub struct AssetName(pub Vec<u8>);
+
+/// The type of an asset represented by a token.
+#[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
+pub enum AssetType {
+    EquityCommon,
+    EquityPreferred,
+    Commodity,
+    FixedIncome,
+    REIT,
+    Fund,
+    RevenueShareAgreement,
+    StructuredProduct,
+    Derivative,
+    Custom(Vec<u8>),
+}
+
+impl Default for AssetType {
+    fn default() -> Self {
+        Self::Custom(b"undefined".to_vec())
+    }
+}
+
+/// A wrapper for a funding round name.
+#[derive(Decode, Encode, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, VecU8StrongTyped)]
+pub struct FundingRoundName(pub Vec<u8>);
+
+impl Default for FundingRoundName {
+    fn default() -> Self {
+        Self(Vec::new())
+    }
+}
