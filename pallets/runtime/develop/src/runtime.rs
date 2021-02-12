090013fc1c043a9eb37d16593b297eafeb7f0c9b
CJP10 <cjpet10@gmail.com>
2021-02-12 02:31:41 -0500

    MESH-1533/Require minimum 1 POLYX bond (#910)
    * WIP
    
    * minor changes
    
    Co-authored-by: Mudit Gupta <guptamudit@ymail.com>

--- pallets/runtime/develop/src/runtime.rs
+++ pallets/runtime/develop/src/runtime.rs
@@ -359,6 +359,7 @@ parameter_types! {
     pub MinSolutionScoreBump: Perbill = Perbill::from_rational_approximation(5u32, 10_000);
     pub const MaxVariableInflationTotalIssuance: Balance = 1_000_000_000 * POLY;
     pub const FixedYearlyReward: Balance = 200_000_000 * POLY;
+    pub const MinimumBond: Balance = 1 * POLY;
 }
 
 impl pallet_staking::Trait for Runtime {
@@ -392,6 +393,7 @@ impl pallet_staking::Trait for Runtime {
     type MaxValidatorPerIdentity = MaxValidatorPerIdentity;
     type MaxVariableInflationTotalIssuance = MaxVariableInflationTotalIssuance;
     type FixedYearlyReward = FixedYearlyReward;
+    type MinimumBond = MinimumBond;
     type PalletsOrigin = OriginCaller;
 }
 
