f160cc52aaec0a869f1de795f397cc6f4a0514b6
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2021-03-24 19:13:34 -0400

    minor change

--- scripts/cli/test.sh
+++ scripts/cli/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-scriptTests=$(ls -1 ./dist/tests | sed -e 's/\.js$//' | sort -n | tr '\n' ' ' | sed 's/^/ schema_test /')
+scriptTests=$(ls -1 ./dist/tests | sed -e 's/\.js$//' |  sed -e 's/\.d.ts$//' | sort -n | uniq | tr '\n' ' ' | sed 's/^/ schema_test /')
 
 for s in ${scriptTests[@]}; do
-    npm run $s
+    yarn $s
 
 done
