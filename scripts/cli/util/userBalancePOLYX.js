9b4ca85978266dfe21a600d21a9b3ba0d4ceb13e
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2021-05-27 00:40:53 -0400

    added secondary keys and progress bar

--- scripts/cli/util/userBalancePOLYX.js
+++ scripts/cli/util/userBalancePOLYX.js
@@ -3,6 +3,7 @@ require = require("esm")(module /*, options*/);
 module.exports = require("./init.js");
 
 const { ExportToCsv } = require("export-to-csv");
+const cliProgress = require("cli-progress");
 let { reqImports } = require("./init.js");
 
 async function main() {
@@ -11,14 +12,44 @@ async function main() {
 	process.exit();
 }
 
-// Gets user balances and stores them in an array
+// Gets user balance and stores them in an array
 async function getUserBalances(api) {
+	// create new progress bar
+	const progressBar = new cliProgress.SingleBar({
+		format: "progress [{bar}] {percentage}% || {value}/{total} || ETA: {eta_formatted}",
+		barCompleteChar: "\u2588",
+		barIncompleteChar: "\u2591",
+		hideCursor: true,
+	});
 	const listOfDids = await api.query.identity.didRecords.entries();
 	let empty_did = "0x0000000000000000000000000000000000000000000000000000000000000000";
 	let data = [];
 
+	// initializes the progress bar
+	progressBar.start(listOfDids.length - 1, 0, {
+		speed: "N/A",
+	});
+
 	for (let i = 0; i < listOfDids.length; i++) {
 		let pk = listOfDids[i][1]["primary_key"];
+		let listOfSecondaryKeyObjects = listOfDids[i][1]["secondary_keys"];
+
+		for (let j = 0; j < listOfSecondaryKeyObjects.length; j++) {
+			// secondary key
+			let sk = JSON.parse(listOfSecondaryKeyObjects[j]["signer"]).Account;
+			// get sk did
+			let skDid = await api.query.identity.keyToIdentityIds(sk);
+			// getting secondary key balance info
+			let skAccountData = await api.query.system.account(sk);
+			data.push({
+				AccountId: sk,
+				Identity: skDid,
+				free: JSON.stringify(skAccountData.data.free),
+				reserved: JSON.stringify(skAccountData.data.reserved),
+				miscFrozen: JSON.stringify(skAccountData.data.miscFrozen),
+			});
+		}
+
 		let did = await api.query.identity.keyToIdentityIds(pk);
 		let accountData = await api.query.system.account(pk);
 
@@ -31,15 +62,20 @@ async function getUserBalances(api) {
 				miscFrozen: JSON.stringify(accountData.data.miscFrozen),
 			});
 		}
+		// update the progress bar
+		progressBar.increment();
 	}
+
 	await createCSV(data);
+	// stop the progress bar
+	progressBar.stop();
 }
 
 // Creates CSV file using an array of user balances
 async function createCSV(data) {
 	const options = {
 		fieldSeparator: ",",
-		quoteStrings: '',
+		quoteStrings: "",
 		decimalSeparator: ".",
 		showLabels: false,
 		showTitle: false,
@@ -50,7 +86,7 @@ async function createCSV(data) {
 
 	const csvExporter = new ExportToCsv(options);
 	const csvData = csvExporter.generateCsv(data, true);
-  reqImports.fs.writeFileSync('usersPolyxBalance.csv',csvData);
+	reqImports.fs.writeFileSync("usersPolyxBalance.csv", csvData);
 }
 
 main().catch(console.error);
