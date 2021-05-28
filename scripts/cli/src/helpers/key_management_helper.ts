8096fb180acb8378f468b11f3418c1cf08544457
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2021-05-28 07:30:01 -0400

    Mesh 1196/refactor JS integration tests to TS (#915)
    * Typed init.ts and modifying tsconfig
    
    * modified init and create identities scripts
    
    * added 1_poly_transfer.ts and helper files
    
    * added 2_key_management .ts and helper files
    
    * added 3_auth_join_did.ts and minor changes
    
    * added 4_permission_management.ts, helper files, and formatted scripts
    
    * added 5_claim_management.ts
    
    * added 6_create_assets.ts
    
    * minor change to fix complianceRequirement issue
    
    * added 7_create_claim_compliance.ts and helper file
    
    * added 8_asset_minting.ts
    
    * added 9_bridge_transfer.ts and helper file
    
    * added 10_governance_management.ts and help files
    
    * added 11_A_settlement.ts and helper file
    
    * added 11_B_settlement.ts
    
    * general improvements
    
    * improved error handling for identity script and helper
    
    * minor changes
    
    * improved error handling for poly_transfer script and asset helper
    
    * improved error handling for key_management script and helper
    
    * add catch to promises
    
    * added @polkadot/typegen
    
    * changes based around @polkadot/typegen
    
    * minor change
    
    * removed handle function
    
    * minor changes
    
    * Added Api Singleton Class and removed Api as a parameter from the scripts
    
    * removed usage of unknown
    
    * standardized on using signer and formatting
    
    * removed .catch from lower levels
    
    * added type safety to catch clause in scripts
    
    * added offchain_worker_script.ts and made minor changes
    
    * minor changes to comments and package.json
    
    * minor change in asset_helper
    
    * minor change
    
    * removed old JS test scripts
    
    Co-authored-by: Adam Dossa <adam.dossa@gmail.com>

---
+++ scripts/cli/src/helpers/key_management_helper.ts
@@ -0,0 +1,42 @@
+import type { KeyringPair } from "@polkadot/keyring/types";
+import type { AccountId } from "@polkadot/types/interfaces";
+import type { Expiry, Permissions, Signatory } from "../types";
+import { sendTx, ApiSingleton } from "../util/init";
+
+/**
+ * @description Attaches a secondary key to each DID
+ */
+export async function addSecondaryKeys(signers: KeyringPair[], receivers: KeyringPair[]): Promise<void> {
+	const api = await ApiSingleton.getInstance();
+	let totalPermissions: Permissions = {
+		asset: [],
+		extrinsic: [],
+		portfolio: [],
+	};
+
+	for (let i in signers) {
+		let target = {
+			Account: receivers[i].publicKey as AccountId,
+		};
+		let authData = {
+			JoinIdentity: totalPermissions,
+		};
+		let expiry: Expiry = null;
+		// 1. Add Secondary Item to identity.
+		const transaction = api.tx.identity.addAuthorization(target, authData, expiry);
+		await sendTx(signers[i], transaction);
+	}
+}
+
+/**
+ * @description Attaches a secondary key to each DID
+ */
+export async function createMultiSig(
+	signer: KeyringPair,
+	signatories: Signatory[],
+	numOfSigners: number
+): Promise<void> {
+	const api = await ApiSingleton.getInstance();
+	const transaction = api.tx.multiSig.createMultisig(signatories, numOfSigners);
+	await sendTx(signer, transaction);
+}
