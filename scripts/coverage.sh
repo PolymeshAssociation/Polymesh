8bd3e8519e46981ed570ab34bcfc713494543c10
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2021-06-07 17:12:32 +0800

    Update nightly toolchain version to fix coverage.
    Rust nightly 2021-01-25 was failing to build the llvm tools needed for
    code coverage reports.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,43 +1,39 @@
-RUSTFLAGS="-Zinstrument-coverage" \
-LLVM_PROFILE_FILE="json5format-%m.profraw" \
-BUILD_DUMMY_WASM_BINARY=1 \
-cargo test --tests \
-    --package pallet-staking \
-    --package pallet-group \
-    --package pallet-sudo \
-    --package polymesh-primitives \
-    --package node-rpc-runtime-api \
-    --package pallet-transaction-payment \
-    --package polymesh-runtime-tests \
-    --package pallet-balances:0.1.0 \
-    --features default_identity
+#!/bin/bash
+
+function run_tests() {
+    RUSTFLAGS="-Zinstrument-coverage" \
+    LLVM_PROFILE_FILE="json5format-%m.profraw" \
+    BUILD_DUMMY_WASM_BINARY=1 \
+    cargo test --tests \
+        --package pallet-staking \
+        --package pallet-group \
+        --package pallet-sudo \
+        --package polymesh-primitives \
+        --package node-rpc-runtime-api \
+        --package pallet-transaction-payment \
+        --package polymesh-runtime-tests \
+        --package pallet-balances:0.1.0 \
+        --features default_identity $*
+}
+
+function get_tests_filenames() {
+    for file in \
+        $( run_tests --no-run --message-format=json \
+            | jq -r "select(.profile.test == true) | .filenames[]" \
+            | grep -v dSYM - \
+        );
+    do
+        printf "%s %s " -object $file;
+    done
+}
+
+#run_tests
 
 cargo profdata -- merge -sparse $(find . -name 'json5format-*.profraw') -o json5format.profdata
 
 if [[ -v CIRCLECI ]]; then
     cargo cov -- export \
-    $( \
-        for file in \
-            $( \
-            RUSTFLAGS="-Zinstrument-coverage" BUILD_DUMMY_WASM_BINARY=1 LLVM_PROFILE_FILE="json5format-%m.profraw" \
-                cargo test --tests \
-                    --package pallet-staking \
-                    --package pallet-group \
-                    --package pallet-sudo \
-                    --package polymesh-primitives \
-                    --package node-rpc-runtime-api \
-                    --package pallet-transaction-payment \
-                    --package polymesh-runtime-tests \
-                    --package pallet-balances:0.1.0 \
-                    --features default_identity \
-                    --no-run --message-format=json \
-                | jq -r "select(.profile.test == true) | .filenames[]" \
-                | grep -v dSYM - \
-            ); \
-        do \
-            printf "%s %s " -object $file; \
-        done \
-    ) \
+    $( get_tests_filenames ) \
     --format='lcov' \
     --instr-profile=json5format.profdata \
     --ignore-filename-regex='/.cargo/registry/' \
@@ -52,28 +48,7 @@ if [[ -v CIRCLECI ]]; then
     bash <(curl -s https://codecov.io/bash)
 else
     cargo cov -- report \
-    $( \
-        for file in \
-            $( \
-            RUSTFLAGS="-Zinstrument-coverage" BUILD_DUMMY_WASM_BINARY=1 LLVM_PROFILE_FILE="json5format-%m.profraw" \
-                cargo test --tests \
-                    --package pallet-staking \
-                    --package pallet-group \
-                    --package pallet-sudo \
-                    --package polymesh-primitives \
-                    --package node-rpc-runtime-api \
-                    --package pallet-transaction-payment \
-                    --package polymesh-runtime-tests \
-                    --package pallet-balances:0.1.0 \
-                    --features default_identity \
-                    --no-run --message-format=json \
-                | jq -r "select(.profile.test == true) | .filenames[]" \
-                | grep -v dSYM - \
-            ); \
-        do \
-            printf "%s %s " -object $file; \
-        done \
-    ) \
+    $( get_tests_filenames ) \
     --instr-profile=json5format.profdata \
     --use-color \
     --ignore-filename-regex='/.cargo/registry/' \
