a1a0ece4737a0b583a1209fb867853b9a7d005fa
Mazdak Farrokhzad <twingoow@gmail.com>
2021-06-15 15:07:16 +0200

    bench: remove sp-io/disable_*

--- pallets/runtime/develop/src/runtime.rs
+++ pallets/runtime/develop/src/runtime.rs
@@ -403,7 +403,7 @@ polymesh_runtime_common::runtime_apis! {
             config: frame_benchmarking::BenchmarkConfig
         ) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
             use frame_benchmarking::{Benchmarking, BenchmarkBatch, add_benchmark, TrackedStorageKey};
-            use frame_system_benchmarking::Module as SystemBench;
+            //use frame_system_benchmarking::Module as SystemBench;
             use crate::benchmarks::pallet_session::Module as SessionBench;
 
             impl frame_system_benchmarking::Config for Runtime {}
