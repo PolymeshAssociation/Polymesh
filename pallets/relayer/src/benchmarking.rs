388c51fa8d6c3c5c441315222b83243add51c692
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2021-06-21 20:47:16 +0800

    Fixup benchmarks.

--- pallets/relayer/src/benchmarking.rs
+++ pallets/relayer/src/benchmarking.rs
@@ -17,7 +17,7 @@ use crate::*;
 
 use frame_benchmarking::benchmarks;
 use polymesh_common_utilities::{
-    benchs::{user, AccountIdOf},
+    benchs::{user, AccountIdOf, User},
     traits::{relayer::Config, TestUtilsFn},
 };
 use sp_std::prelude::*;
@@ -26,7 +26,7 @@ type Relayer<T> = crate::Module<T>;
 
 pub(crate) const SEED: u32 = 0;
 
-fn setup_users() -> (User, User) {
+fn setup_users<T: Config + TestUtilsFn<AccountIdOf<T>>>() -> (User<T>, User<T>) {
     let payer = user::<T>("payer", SEED);
     let user = user::<T>("user", SEED);
     (payer, user)
@@ -35,14 +35,12 @@ fn setup_users() -> (User, User) {
 benchmarks! {
     where_clause { where T: Config, T: TestUtilsFn<AccountIdOf<T>> }
 
-    _ {}
-
     set_paying_key {
-        let (payer, user) = setup_users();
+        let (payer, user) = setup_users::<T>();
     }: _(payer.origin(), user.account())
 
     accept_paying_key {
-        let (payer, user) = setup_users();
+        let (payer, user) = setup_users::<T>();
         // setup authorization
         let auth_id = <Relayer<T>>::unsafe_add_auth_for_paying_key(
             payer.did(), user.account(), payer.account()
@@ -50,7 +48,7 @@ benchmarks! {
     }: _(user.origin(), auth_id)
 
     remove_paying_key {
-        let (payer, user) = setup_users();
+        let (payer, user) = setup_users::<T>();
         let user_signer = Signatory::Account(user.account());
         // accept paying key
         <Relayer<T>>::auth_accept_paying_key(user_signer, payer.did(), user.account(), payer.account()).unwrap();
