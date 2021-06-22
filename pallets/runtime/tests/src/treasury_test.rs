9f953b41ebb3d59c402db8dc44e0ede2bb387f2b
Mazdak Farrokhzad <twingoow@gmail.com>
2021-06-22 12:47:23 +0200

    simplify treasury pallet

--- pallets/runtime/tests/src/treasury_test.rs
+++ pallets/runtime/tests/src/treasury_test.rs
@@ -1,12 +1,12 @@
 use super::{
-    storage::{register_keyring_account, root, TestStorage},
+    storage::{root, TestStorage, User},
     ExtBuilder,
 };
 
 use frame_support::{assert_noop, assert_ok};
 use pallet_balances as balances;
 use pallet_identity as identity;
-use pallet_treasury::{self as treasury, TreasuryTrait};
+use pallet_treasury::{self as treasury};
 use polymesh_common_utilities::Context;
 use polymesh_primitives::Beneficiary;
 use sp_runtime::DispatchError;
@@ -26,54 +26,42 @@ fn reimbursement_and_disbursement() {
 }
 
 fn reimbursement_and_disbursement_we() {
-    let alice = register_keyring_account(AccountKeyring::Alice).unwrap();
-    let alice_acc = Origin::signed(AccountKeyring::Alice.to_account_id());
-    let alice_pub = AccountKeyring::Alice.to_account_id();
-    let bob_pub = AccountKeyring::Bob.to_account_id();
-    let bob = register_keyring_account(AccountKeyring::Bob).unwrap();
+    let alice = User::new(AccountKeyring::Alice);
+    let bob = User::new(AccountKeyring::Bob);
 
     let total_issuance = Balances::total_issuance();
 
-    // Verify reimburstement.
+    // Verify reimbursement.
     assert_eq!(Treasury::balance(), 0);
-    assert_ok!(Treasury::reimbursement(alice_acc.clone(), 1_000));
+    assert_ok!(Treasury::reimbursement(alice.origin(), 1_000));
     assert_eq!(Treasury::balance(), 1_000);
     assert_eq!(total_issuance, Balances::total_issuance());
 
     // Disbursement: Only root can do that.
-    let beneficiaries = vec![
-        Beneficiary {
-            id: alice,
-            amount: 100,
-        },
-        Beneficiary {
-            id: bob,
-            amount: 500,
-        },
-    ];
+    assert_noop!(
+        Treasury::disbursement(alice.origin(), vec![]),
+        DispatchError::BadOrigin
+    );
+
+    let beneficiary = |u: User, amount| Beneficiary { id: u.did, amount };
+    let beneficiaries = vec![beneficiary(alice, 100), beneficiary(bob, 500)];
 
     // Providing a random DID to Root, In an ideal world root will have a valid DID
-    let before_alice_balance = Balances::free_balance(&alice_pub);
-    let before_bob_balance = Balances::free_balance(&bob_pub);
+    let before_alice_balance = Balances::free_balance(&alice.acc());
+    let before_bob_balance = Balances::free_balance(&bob.acc());
     assert_ok!(Treasury::disbursement(root(), beneficiaries));
     Context::set_current_identity::<Identity>(None);
     assert_eq!(Treasury::balance(), 400);
     assert_eq!(
-        Balances::free_balance(&alice_pub),
+        Balances::free_balance(&alice.acc()),
         before_alice_balance + 100
     );
-    assert_eq!(Balances::free_balance(&bob_pub), before_bob_balance + 500);
+    assert_eq!(Balances::free_balance(&bob.acc()), before_bob_balance + 500);
     assert_eq!(total_issuance, Balances::total_issuance());
 
     // Alice cannot make a disbursement to herself.
     assert_noop!(
-        Treasury::disbursement(
-            alice_acc,
-            vec![Beneficiary {
-                id: alice,
-                amount: 500
-            }]
-        ),
+        Treasury::disbursement(alice.origin(), vec![beneficiary(alice, 500)]),
         DispatchError::BadOrigin
     );
     assert_eq!(total_issuance, Balances::total_issuance());
