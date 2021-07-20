e87e418685bc808987bb5e3636b37fb8b096145b
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2021-07-20 18:36:07 +0800

    Add more relayer tests.

--- pallets/runtime/tests/src/relayer_test.rs
+++ pallets/runtime/tests/src/relayer_test.rs
@@ -3,12 +3,15 @@ use super::{
     ExtBuilder,
 };
 use frame_support::{assert_noop, assert_ok, StorageMap};
+use pallet_relayer::Subsidy;
 use polymesh_common_utilities::traits::transaction_payment::CddAndFeeDetails;
-use polymesh_primitives::Signatory;
+use polymesh_primitives::{AccountId, Balance, Signatory};
 use polymesh_runtime_develop::{fee_details::CddHandler, runtime::Call};
 use test_client::AccountKeyring;
 
 type Relayer = pallet_relayer::Module<TestStorage>;
+type Subsidies = pallet_relayer::Subsidies<TestStorage>;
+
 type Identity = pallet_identity::Module<TestStorage>;
 type AccountKeyRefCount = pallet_identity::AccountKeyRefCount<TestStorage>;
 
@@ -17,10 +20,21 @@ type Error = pallet_relayer::Error<TestStorage>;
 // Relayer Test Helper functions
 // =======================================
 
-macro_rules! assert_key_usage {
-    ($user:expr, $usage:expr) => {
-        assert_eq!(AccountKeyRefCount::get(&$user.acc()), $usage);
-    };
+#[track_caller]
+fn assert_key_usage(user: User, usage: u64) {
+    assert_eq!(AccountKeyRefCount::get(&user.acc()), usage);
+}
+
+fn get_subsidy(user: User) -> Option<Subsidy<AccountId, Balance>> {
+    Subsidies::get(&user.acc())
+}
+
+#[track_caller]
+fn assert_subsidy(user: User, subsidy: Option<(User, Balance)>) {
+    assert_eq!(
+        get_subsidy(user).map(|s| (s.paying_key, s.remaining)),
+        subsidy.map(|s| (s.0.acc(), s.1))
+    );
 }
 
 #[test]
@@ -36,26 +50,28 @@ fn do_basic_relayer_paying_key_test() {
     let dave = User::new(AccountKeyring::Dave);
 
     // add authorization for using Alice as the paying key for bob
-    assert_ok!(Relayer::set_paying_key(
-        alice.origin(),
-        bob.acc(),
-        0u128.into()
-    ));
+    assert_ok!(Relayer::set_paying_key(alice.origin(), bob.acc(), 10u128));
 
-    // No paying key is used yet.
-    assert_key_usage!(alice, 0);
-    assert_key_usage!(dave, 0);
-    assert_key_usage!(bob, 0);
+    // The keys are not used yet.
+    assert_key_usage(alice, 0);
+    assert_key_usage(dave, 0);
+    assert_key_usage(bob, 0);
+
+    // No subsidy yet.
+    assert_subsidy(bob, None);
 
     // Bob accept's the paying key
     TestStorage::set_current_identity(&bob.did);
     let auth_id = get_last_auth_id(&Signatory::Account(bob.acc()));
     assert_ok!(Relayer::accept_paying_key(bob.origin(), auth_id));
 
-    // check alice's key is be used.
-    assert_key_usage!(alice, 1);
-    // check bob's key is be used.
-    assert_key_usage!(bob, 1);
+    // Bob now has a subsidy of 10 POLYX.
+    assert_subsidy(bob, Some((alice, 10u128)));
+
+    // Alice's and Bob's keys are now used, but Dave's key is still unused.
+    assert_key_usage(alice, 1);
+    assert_key_usage(bob, 1);
+    assert_key_usage(dave, 0);
 
     // Bob tries to increase his Polyx limit.  Not allowed
     TestStorage::set_current_identity(&bob.did);
@@ -72,20 +88,26 @@ fn do_basic_relayer_paying_key_test() {
         10_000u128
     ));
 
+    // Bob now has a subsidy of 10,000 POLYX.
+    assert_subsidy(bob, Some((alice, 10_000u128)));
+
     // Dave tries to remove the paying key from Bob's key.  Not allowed.
     TestStorage::set_current_identity(&dave.did);
     assert_noop!(
-        Relayer::remove_paying_key(dave.origin(), bob.acc(), alice.acc(),),
+        Relayer::remove_paying_key(dave.origin(), bob.acc(), alice.acc()),
         Error::NotAuthorizedForUserKey
+    );
+
+    // Dave tries to remove the wrong paying key from Bob's key.  Not allowed.
+    TestStorage::set_current_identity(&dave.did);
+    assert_noop!(
+        Relayer::remove_paying_key(dave.origin(), bob.acc(), dave.acc()),
+        Error::NotPayingKey
     );
 
     // add authorization for using Dave as the paying key for bob
     TestStorage::set_current_identity(&dave.did);
-    assert_ok!(Relayer::set_paying_key(
-        dave.origin(),
-        bob.acc(),
-        0u128.into()
-    ));
+    assert_ok!(Relayer::set_paying_key(dave.origin(), bob.acc(), 0u128));
 
     // Bob tries to accept the new paying key, but he already has a paying key
     TestStorage::set_current_identity(&bob.did);
@@ -103,9 +125,30 @@ fn do_basic_relayer_paying_key_test() {
         alice.acc(),
     ));
 
-    // check alice's key is not used any more.
-    assert_key_usage!(alice, 0);
+    // Bob no longer has a subsidy.
+    assert_subsidy(bob, None);
+
+    // Check alice's key is not used any more.
+    assert_key_usage(alice, 0);
+
+    // Alice tries to update the poly limit for Bob,
+    // but Bob no longer has a subsiy.
+    TestStorage::set_current_identity(&alice.did);
+    assert_noop!(
+        Relayer::update_polyx_limit(alice.origin(), bob.acc(), 42u128),
+        Error::NoPayingKey
+    );
+
+    // Alice tries to remove the paying key a second time,
+    // but Bob no longer has a subsiy.
+    TestStorage::set_current_identity(&alice.did);
+    assert_noop!(
+        Relayer::remove_paying_key(alice.origin(), bob.acc(), alice.acc()),
+        Error::NoPayingKey
+    );
 }
+
+/// TODO: Add tests for subsidiser interface.
 
 #[test]
 fn relayer_accept_cdd_and_fees_test() {
@@ -120,11 +163,7 @@ fn do_relayer_accept_cdd_and_fees_test() {
     let bob_sign = Signatory::Account(bob.acc());
 
     // Alice creates authoration to subsidise for Bob.
-    assert_ok!(Relayer::set_paying_key(
-        alice.origin(),
-        bob.acc(),
-        0u128.into()
-    ));
+    assert_ok!(Relayer::set_paying_key(alice.origin(), bob.acc(), 0u128));
     let bob_auth_id = get_last_auth_id(&bob_sign);
 
     // Check that Bob can accept the subsidy with Alice paying for the transaction.
