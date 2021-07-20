21906f66253f890c52c62af71708dde8dfeaf6b2
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2021-07-20 20:41:16 +0800

    Add tests for missing cdd.

--- pallets/runtime/tests/src/relayer_test.rs
+++ pallets/runtime/tests/src/relayer_test.rs
@@ -1,5 +1,5 @@
 use super::{
-    storage::{get_last_auth_id, TestStorage, User},
+    storage::{get_last_auth_id, make_account_without_cdd, TestStorage, User},
     ExtBuilder,
 };
 use frame_support::{assert_noop, assert_ok, StorageMap};
@@ -49,7 +49,7 @@ fn do_basic_relayer_paying_key_test() {
     let alice = User::new(AccountKeyring::Alice);
     let dave = User::new(AccountKeyring::Dave);
 
-    // add authorization for using Alice as the paying key for bob
+    // Add authorization for using Alice as the paying key for Bob.
     assert_ok!(Relayer::set_paying_key(alice.origin(), bob.acc(), 10u128));
 
     // The keys are not used yet.
@@ -60,7 +60,7 @@ fn do_basic_relayer_paying_key_test() {
     // No subsidy yet.
     assert_subsidy(bob, None);
 
-    // Bob accept's the paying key
+    // Bob accept's the paying key.
     TestStorage::set_current_identity(&bob.did);
     let auth_id = get_last_auth_id(&Signatory::Account(bob.acc()));
     assert_ok!(Relayer::accept_paying_key(bob.origin(), auth_id));
@@ -74,7 +74,6 @@ fn do_basic_relayer_paying_key_test() {
     assert_key_usage(dave, 0);
 
     // Bob tries to increase his Polyx limit.  Not allowed
-    TestStorage::set_current_identity(&bob.did);
     assert_noop!(
         Relayer::update_polyx_limit(bob.origin(), bob.acc(), 12345u128),
         Error::NotPayingKey
@@ -105,11 +104,10 @@ fn do_basic_relayer_paying_key_test() {
         Error::NotPayingKey
     );
 
-    // add authorization for using Dave as the paying key for bob
-    TestStorage::set_current_identity(&dave.did);
+    // Add authorization for using Dave as the paying key for Bob.
     assert_ok!(Relayer::set_paying_key(dave.origin(), bob.acc(), 0u128));
 
-    // Bob tries to accept the new paying key, but he already has a paying key
+    // Bob tries to accept the new paying key, but he already has a paying key.
     TestStorage::set_current_identity(&bob.did);
     let auth_id = get_last_auth_id(&Signatory::Account(bob.acc()));
     assert_noop!(
@@ -133,7 +131,6 @@ fn do_basic_relayer_paying_key_test() {
 
     // Alice tries to update the poly limit for Bob,
     // but Bob no longer has a subsiy.
-    TestStorage::set_current_identity(&alice.did);
     assert_noop!(
         Relayer::update_polyx_limit(alice.origin(), bob.acc(), 42u128),
         Error::NoPayingKey
@@ -141,10 +138,62 @@ fn do_basic_relayer_paying_key_test() {
 
     // Alice tries to remove the paying key a second time,
     // but Bob no longer has a subsiy.
-    TestStorage::set_current_identity(&alice.did);
     assert_noop!(
         Relayer::remove_paying_key(alice.origin(), bob.acc(), alice.acc()),
         Error::NoPayingKey
+    );
+}
+
+#[test]
+fn relayer_user_key_missing_cdd_test() {
+    ExtBuilder::default()
+        .monied(true)
+        .build()
+        .execute_with(&do_relayer_user_key_missing_cdd_test);
+}
+fn do_relayer_user_key_missing_cdd_test() {
+    let alice = User::new(AccountKeyring::Alice);
+    let bob_acc = AccountKeyring::Bob.to_account_id();
+    let (bob_sign, bob_did) = make_account_without_cdd(bob_acc.clone()).unwrap();
+
+    // Add authorization for using Alice as the paying key for Bob.
+    assert_ok!(Relayer::set_paying_key(
+        alice.origin(),
+        bob_acc.clone(),
+        10u128
+    ));
+
+    // Bob tries to accept the paying key, without having a CDD.
+    TestStorage::set_current_identity(&bob_did);
+    let auth_id = get_last_auth_id(&Signatory::Account(bob_acc.clone()));
+    assert_noop!(
+        Relayer::accept_paying_key(bob_sign, auth_id),
+        Error::UserKeyCddMissing
+    );
+}
+
+#[test]
+fn relayer_paying_key_missing_cdd_test() {
+    ExtBuilder::default()
+        .monied(true)
+        .build()
+        .execute_with(&do_relayer_paying_key_missing_cdd_test);
+}
+fn do_relayer_paying_key_missing_cdd_test() {
+    let alice = User::new(AccountKeyring::Alice);
+    let bob_acc = AccountKeyring::Bob.to_account_id();
+    let (bob_sign, bob_did) = make_account_without_cdd(bob_acc.clone()).unwrap();
+
+    // Add authorization for using Bob as the paying key for Alice.
+    assert_ok!(Relayer::set_paying_key(bob_sign, alice.acc(), 10u128));
+
+    // Alice tries to accept the paying key, but the paying key
+    // is without a CDD.
+    TestStorage::set_current_identity(&alice.did);
+    let auth_id = get_last_auth_id(&Signatory::Account(alice.acc()));
+    assert_noop!(
+        Relayer::accept_paying_key(alice.origin(), auth_id),
+        Error::PayingKeyCddMissing
     );
 }
 
