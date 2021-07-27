c825d02b4386c2170cbe2f2b52ad064bc3c61743
CJP10 <cjpet10@gmail.com>
2021-07-27 15:52:53 -0400

    WIP

--- pallets/runtime/tests/src/rewards_test.rs
+++ pallets/runtime/tests/src/rewards_test.rs
@@ -3,7 +3,6 @@ use super::{
     ExtBuilder,
 };
 use codec::Encode;
-use frame_support::dispatch::DispatchResult;
 use frame_support::{assert_noop, assert_ok};
 use frame_system::RawOrigin;
 use polymesh_common_utilities::constants::currency::POLY;
@@ -30,44 +29,39 @@ fn basic_itn_claim() {
     let alice_init_balance = Balances::free_balance(alice.acc());
     let bob_init_balance = Balances::free_balance(bob.acc());
 
-    let claim_itn_reward =
-        |reward_user: &User, itn_user: &User, sig_user: Option<&User>, expected: DispatchResult| {
-            let sig_user = sig_user.unwrap_or(itn_user);
-            let mut msg = [0u8; 48];
-            msg[..32].copy_from_slice(&reward_user.acc().encode());
-            msg[32..].copy_from_slice(b"claim_itn_reward");
-            let result = Rewards::claim_itn_reward(
-                RawOrigin::None.into(),
-                reward_user.acc(),
-                itn_user.acc(),
-                sig_user.ring.sign(&msg).into(),
-            );
-            match expected {
-                Ok(_) => {
-                    assert_ok!(result);
-                }
-                Err(e) => {
-                    assert_noop!(result, e);
-                }
-            };
-        };
+    let claim_itn_reward = |reward_user: &User, itn_user: &User, sig_user: Option<&User>| {
+        let sig_user = sig_user.unwrap_or(itn_user);
+        let mut msg = [0u8; 48];
+        msg[..32].copy_from_slice(&reward_user.acc().encode());
+        msg[32..].copy_from_slice(b"claim_itn_reward");
+        Rewards::claim_itn_reward(
+            RawOrigin::None.into(),
+            reward_user.acc(),
+            itn_user.acc(),
+            sig_user.ring.sign(&msg).into(),
+        )
+    };
 
     // Check that an address not in the table errors.
-    claim_itn_reward(&alice, &alice, None, Err(Error::UnknownItnAddress.into()));
+    assert_noop!(
+        claim_itn_reward(&alice, &alice, None),
+        Error::UnknownItnAddress
+    );
 
     // Check that claiming a reward fails when itn wallet is empty.
-    claim_itn_reward(&alice, &bob, None, Err(Error::InsufficientBalance.into()));
+    assert_noop!(
+        claim_itn_reward(&alice, &bob, None),
+        Error::InsufficientBalance
+    );
 
     // Give itn wallet some balance.
     let _ = Balances::deposit_into_existing(&Rewards::account_id(), 2 * POLY);
     assert_eq!(Rewards::balance(), 2 * POLY);
 
     // Check sig.
-    claim_itn_reward(
-        &alice,
-        &bob,
-        Some(&alice),
-        Err(Error::InvalidSignature.into()),
+    assert_noop!(
+        claim_itn_reward(&alice, &bob, Some(&alice)),
+        Error::InvalidSignature
     );
 
     // Check balances have not changed.
@@ -75,7 +69,7 @@ fn basic_itn_claim() {
     assert_eq!(Balances::free_balance(bob.acc()), bob_init_balance);
 
     // Claim reward successfully.
-    claim_itn_reward(&alice, &bob, None, Ok(()));
+    assert_ok!(claim_itn_reward(&alice, &bob, None));
 
     // Check balances were updated.
     assert_eq!(
@@ -85,10 +79,8 @@ fn basic_itn_claim() {
     assert_eq!(Balances::free_balance(bob.acc()), bob_init_balance);
 
     // Check double claim fails.
-    claim_itn_reward(
-        &alice,
-        &bob,
-        None,
-        Err(Error::ItnRewardAlreadyClaimed.into()),
+    assert_noop!(
+        claim_itn_reward(&alice, &bob, None),
+        Error::ItnRewardAlreadyClaimed
     );
 }
