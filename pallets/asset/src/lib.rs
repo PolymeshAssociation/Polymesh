0b08e4fecdb507e2cbe15045e6714294677ea9ed
Adam Dossa <adam.dossa@gmail.com>
2021-08-20 14:20:34 +0100

    Add explanatory comment

--- pallets/asset/src/lib.rs
+++ pallets/asset/src/lib.rs
@@ -2259,6 +2259,9 @@ impl<T: Config> Module<T> {
     }
 
     fn missing_scope_claim(ticker: &Ticker, from: &PortfolioId, to: &PortfolioId) -> bool {
+        // We want this function missing_scope_claim to return true iff:
+        // - ticker enforces scope claims (i.e. DisableInvestorUniqueness::get(ticker) == false ) AND
+        // - to.did / from.did don’t have a scope claim (i.e. !Identity::<T>::verify_iu_claims_for_transfer(*ticker, to.did, from.did) )
         !DisableInvestorUniqueness::get(ticker)
             && !Identity::<T>::verify_iu_claims_for_transfer(*ticker, to.did, from.did)
     }
