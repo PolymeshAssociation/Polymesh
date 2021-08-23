d6a3275bbd2629463441dac3ce49e192f405d05a
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2021-08-24 01:01:20 +0800

    Cargo fmt

--- pallets/runtime/tests/src/ext_builder.rs
+++ pallets/runtime/tests/src/ext_builder.rs
@@ -11,8 +11,8 @@ use polymesh_common_utilities::{
     constants::currency::POLY, protocol_fee::ProtocolOp, SystematicIssuers, GC_DID,
 };
 use polymesh_primitives::{
-    cdd_id::InvestorUid, identity_id::GenesisIdentityRecord, AccountId, Identity,
-    IdentityId, PosRatio, Signatory,
+    cdd_id::InvestorUid, identity_id::GenesisIdentityRecord, AccountId, Identity, IdentityId,
+    PosRatio, Signatory,
 };
 use sp_io::TestExternalities;
 use sp_runtime::{Perbill, Storage};
