8de858983169fa94f04f5170fbb9195cb0334bc9
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2021-08-28 02:57:16 +0800

    Asset docs cleanup.

--- pallets/asset/src/lib.rs
+++ pallets/asset/src/lib.rs
@@ -342,13 +342,13 @@ decl_storage! {
         /// A map of a ticker name and asset identifiers.
         pub Identifiers get(fn identifiers): map hasher(blake2_128_concat) Ticker => Vec<AssetIdentifier>;
 
-        /// The next `AgentType::Custom` ID in the sequence.
+        /// The next `AssetType::Custom` ID in the sequence.
         ///
         /// Numbers in the sequence start from 1 rather than 0.
         pub CustomTypeIdSequence get(fn custom_type_id_seq): CustomAssetTypeId;
-        /// Maps custom agent type ids to the registered string contents.
+        /// Maps custom asset type ids to the registered string contents.
         pub CustomTypes get(fn custom_types): map hasher(twox_64_concat) CustomAssetTypeId => Vec<u8>;
-        /// Inverse map of `CustomTypes`, from registered string contents to custom agent type ids.
+        /// Inverse map of `CustomTypes`, from registered string contents to custom asset type ids.
         pub CustomTypesInverse get(fn custom_types_inverse): map hasher(blake2_128_concat) Vec<u8> => CustomAssetTypeId;
 
         /// The name of the current funding round.
