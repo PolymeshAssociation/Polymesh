cf7e96608e290a081f6f8f50d607ecb6297e1c91
Raycar5 <raycar5@users.noreply.github.com>
2021-08-31 11:10:15 +0200

    new claim tests

--- scripts/cli/src/tests/5_claim_management.ts
+++ scripts/cli/src/tests/5_claim_management.ts
@@ -3,31 +3,65 @@ import {
   generateRandomKey,
   generateKeys,
   transferAmount,
+  ApiSingleton,
+  sendTx,
+  generateRandomTicker,
 } from "../util/init";
 import { createIdentities, addClaimsToDids } from "../helpers/identity_helper";
 import { distributePolyBatch } from "../helpers/poly_helper";
 import PrettyError from "pretty-error";
 
 async function main(): Promise<void> {
+  const api = await ApiSingleton.getInstance();
+
   const testEntities = await initMain();
   const alice = testEntities[0];
-  const primaryDevSeed = generateRandomKey();
-  const claimDevSeed = generateRandomKey();
-  const primaryKeys = await generateKeys(2, primaryDevSeed);
-  const claimKeys = await generateKeys(2, claimDevSeed);
-  const issuerDids = await createIdentities(alice, primaryKeys);
-  const claimIssuerDids = await createIdentities(alice, claimKeys);
+
+  const issuerSeed1 = "fooIssuer";
+  const issuerSeed2 = "barIssuer";
+  const claimeeSeed = "theClaimee";
+
+  const issuerKeys1 = await generateKeys(2, issuerSeed1);
+  const issuerKeys2 = await generateKeys(2, issuerSeed2);
+  const claimeeKeys = await generateKeys(2, claimeeSeed);
+
+  await createIdentities(alice, issuerKeys1);
+  await createIdentities(alice, issuerKeys2);
+  const claimeeDids = await createIdentities(alice, claimeeKeys);
+
+  const ticker = "FOOBARTICKER";
+
   await distributePolyBatch(
     alice,
-    primaryKeys.concat(claimKeys),
+    issuerKeys1.concat(issuerKeys2).concat(claimeeKeys),
     transferAmount
   );
-  await addClaimsToDids(
-    claimKeys[0],
-    issuerDids[0],
-    "Exempted",
-    { Identity: claimIssuerDids[1] },
-    null
+
+  await sendTx(
+    issuerKeys1[0],
+    api.tx.identity.addClaim(
+      claimeeDids[0],
+      { Exempted: { Identity: claimeeDids[1] } },
+      null
+    )
+  );
+
+  await sendTx(
+    issuerKeys1[0],
+    api.tx.identity.addClaim(
+      claimeeDids[0],
+      { SellLockup: { Ticker: ticker } },
+      null
+    )
+  );
+
+  await sendTx(
+    issuerKeys2[0],
+    api.tx.identity.addClaim(
+      claimeeDids[0],
+      { Accredited: { Ticker: ticker } },
+      null
+    )
   );
 }
 
