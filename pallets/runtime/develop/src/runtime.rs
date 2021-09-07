1681deae2c034a6a8d64f5a159868cb445ae2da0
Adam Dossa <adam.dossa@gmail.com>
2021-09-07 16:14:44 +0100

    MESH-1686: Update mainnet config (#1098)
    * WIP - add mainnet config
    
    * Update environment.config.js
    
    * Fix linting
    
    * Update schema inline with 3.3
    
    * Fix typos
    
    * Update src/chain_spec.rs
    
    Co-authored-by: F O'Brien <42175565+F-OBrien@users.noreply.github.com>
    
    Co-authored-by: F O'Brien <42175565+F-OBrien@users.noreply.github.com>

--- pallets/runtime/develop/src/runtime.rs
+++ pallets/runtime/develop/src/runtime.rs
@@ -178,7 +178,7 @@ pallet_staking_reward_curve::build! {
 parameter_types! {
     pub const SessionsPerEra: sp_staking::SessionIndex = 3;
     pub const BondingDuration: pallet_staking::EraIndex = 7;
-    pub const SlashDeferDuration: pallet_staking::EraIndex = 4; // 1/4 the bonding duration.
+    pub const SlashDeferDuration: pallet_staking::EraIndex = 4;
     pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
     pub const MaxNominatorRewardedPerValidator: u32 = 2048;
     pub const ElectionLookahead: BlockNumber = EPOCH_DURATION_IN_BLOCKS / 4;
