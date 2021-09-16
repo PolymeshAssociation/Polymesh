9fc057bdf8df7214f7361c48c8f5b713d82f5395
CJP10 <cjpet10@gmail.com>
2021-09-15 21:14:33 -0400

    WIP

--- pallets/weights/src/pallet_settlement.rs
+++ pallets/weights/src/pallet_settlement.rs
@@ -92,10 +92,10 @@ impl pallet_settlement::WeightInfo for WeightInfo {
     fn reject_instruction(l: u32) -> Weight {
         (25_334_000 as Weight)
             .saturating_add((19_902_000 as Weight).saturating_mul(l as Weight))
-            .saturating_add(T::DbWeight::get().reads(8 as Weight))
-            .saturating_add(T::DbWeight::get().reads((3 as Weight).saturating_mul(l as Weight)))
-            .saturating_add(T::DbWeight::get().writes(3 as Weight))
-            .saturating_add(T::DbWeight::get().writes((6 as Weight).saturating_mul(l as Weight)))
+            .saturating_add(DbWeight::get().reads(8 as Weight))
+            .saturating_add(DbWeight::get().reads((3 as Weight).saturating_mul(l as Weight)))
+            .saturating_add(DbWeight::get().writes(3 as Weight))
+            .saturating_add(DbWeight::get().writes((6 as Weight).saturating_mul(l as Weight)))
     }
     fn affirm_instruction(l: u32) -> Weight {
         (161_962_000 as Weight)
