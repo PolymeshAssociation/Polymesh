2474f330c60adca6cd15b2a95dcb63709f43507e
Adam Dossa <adam.dossa@gmail.com>
2021-09-23 14:43:42 +0100

    Update spec & tx version (#1126)

--- pallets/runtime/itn/src/runtime.rs
+++ pallets/runtime/itn/src/runtime.rs
@@ -55,10 +55,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to 0. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
-    spec_version: 2018,
+    spec_version: 3000,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 7,
+    transaction_version: 1,
 };
 
 parameter_types! {
