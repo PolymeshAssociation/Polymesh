e26b8ce58247e20e141a7fe373e72c6490a37f10
Jeremias Diaz <monitz87@gmail.com>
2021-10-07 16:17:35 -0300

    test: configure jest to for polkadot compatibility
    The latest polkadot versions do not play well with jest out of the box

--- scripts/cli/jest.config.js
+++ scripts/cli/jest.config.js
@@ -1,9 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+	preset: 'ts-jest/presets/js-with-babel',
+	transformIgnorePatterns: ['<rootDir>/node_modules/(?!@polkadot|@babel/runtime/helpers/esm/)'],
 	roots: ["<rootDir>/src"],
-	transform: {
-		".(ts|tsx)": "ts-jest",
-	},
 	testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$",
 	moduleFileExtensions: ["ts", "tsx", "js"],
 };
