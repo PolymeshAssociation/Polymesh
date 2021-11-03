b877bce78d8fc58c7dd14abea61ce478548a33a6
CJP10 <cjpet10@gmail.com>
2021-11-03 18:24:55 -0400

    WIP

--- pallets/settlement/src/lib.rs
+++ pallets/settlement/src/lib.rs
@@ -1175,7 +1175,7 @@ impl<T: Config> Module<T> {
         Ok(instructions_processed)
     }
 
-    pub fn prune_instruction(id: InstructionId) {
+    fn prune_instruction(id: InstructionId) {
         let legs = InstructionLegs::drain_prefix(id).collect::<Vec<_>>();
         let details = <InstructionDetails<T>>::take(id);
         VenueInstructions::remove(details.venue_id, id);
