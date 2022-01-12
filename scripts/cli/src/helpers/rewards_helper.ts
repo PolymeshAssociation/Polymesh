2f93f670fa3f62575639be4ceb48a83144075694
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2022-01-12 11:07:19 -0400

    formatting

--- scripts/cli/src/helpers/rewards_helper.ts
+++ scripts/cli/src/helpers/rewards_helper.ts
@@ -6,18 +6,15 @@ import { u8aToHex } from "@polkadot/util";
 /**
  * @description Claim an ITN reward with a valid signature.
  */
-export async function claimItnReward(
-	itn: KeyringPair,
-    reward: KeyringPair
-) {
-	const api = await ApiSingleton.getInstance();
-	let claim_msg_hex = "636c61696d5f69746e5f726577617264"; // "claim_itn_reward" as hex.
-	let msg = (u8aToHex(reward.publicKey, 512)).toString() + claim_msg_hex;
-	let signature = itn.sign(msg);
-	const transaction = api.tx.rewards.claimItnReward(msg, itn.address, {
-		Sr25519: signature,
-	});
-	return transaction.send();
+export async function claimItnReward(itn: KeyringPair, reward: KeyringPair) {
+  const api = await ApiSingleton.getInstance();
+  let claim_msg_hex = "636c61696d5f69746e5f726577617264"; // "claim_itn_reward" as hex.
+  let msg = u8aToHex(reward.publicKey, 512).toString() + claim_msg_hex;
+  let signature = itn.sign(msg);
+  const transaction = api.tx.rewards.claimItnReward(msg, itn.address, {
+    Sr25519: signature,
+  });
+  return transaction.send();
 }
 
 /**
@@ -25,11 +22,13 @@ export async function claimItnReward(
  * can only be called by root.
  */
 export async function setItnRewardStatus(
-	signer: KeyringPair,
-	itnAddress: KeyringPair,
-	status: ItnRewardStatus
+  signer: KeyringPair,
+  itnAddress: KeyringPair,
+  status: ItnRewardStatus
 ) {
-	const api = await ApiSingleton.getInstance();
-    const transaction = api.tx.sudo.sudo(api.tx.rewards.setItnRewardStatus(itnAddress.publicKey, status));
-	return sendTx(signer, transaction);
+  const api = await ApiSingleton.getInstance();
+  const transaction = api.tx.sudo.sudo(
+    api.tx.rewards.setItnRewardStatus(itnAddress.publicKey, status)
+  );
+  return sendTx(signer, transaction);
 }
