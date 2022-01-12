7db280fde93614c03800eff570f7d4e38b9c629a
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2022-01-12 16:12:47 -0400

    created 9_bridge_transfer Jest Test

---
+++ scripts/cli/src/__tests__/9_bridge_transfer.ts
@@ -0,0 +1,28 @@
+import { initMain, sleep, disconnect } from "../util/init";
+import {
+  bridgeTransfer,
+  freezeTransaction,
+  unfreezeTransaction,
+} from "../helpers/bridge_helper";
+import { createTable } from "../util/sqlite3";
+
+beforeAll(() => {
+  createTable();
+});
+
+// Disconnects api after all the tests have completed
+afterAll(async () => {
+  await disconnect();
+});
+
+describe("9 - Bridge Transfer Unit Test", () => {
+  test("Bridging Transaction", async () => {
+    const testEntities = await initMain();
+    const relay = testEntities[1];
+    const admin = testEntities[2];
+    await expect(bridgeTransfer(relay, admin)).resolves.not.toThrow();
+    await expect(freezeTransaction(admin)).resolves.not.toThrow();
+    await sleep(5000);
+    await expect(unfreezeTransaction(admin)).resolves.not.toThrow();
+  });
+});
