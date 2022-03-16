cc27ccdcc570edbc0c5d934f51096ad013a5720c
Mazdak Farrokhzad <twingoow@gmail.com>
2022-03-16 15:38:41 +0100

    cargo fmt

--- pallets/weights/src/polymesh_contracts.rs
+++ pallets/weights/src/polymesh_contracts.rs
@@ -8,8 +8,16 @@ use polymesh_runtime_common::{RocksDbWeight as DbWeight, Weight};
 pub struct WeightInfo;
 impl polymesh_contracts::WeightInfo for WeightInfo {
     // TODO
-    fn call() -> Weight { 0 }
-    fn instantiate_with_code(_: u32, _: u32) -> Weight { 0 }
-    fn instantiate_with_hash(_: u32) -> Weight { 0 }
-    fn chain_extension(_: u32) -> Weight { 0 }
+    fn call() -> Weight {
+        0
+    }
+    fn instantiate_with_code(_: u32, _: u32) -> Weight {
+        0
+    }
+    fn instantiate_with_hash(_: u32) -> Weight {
+        0
+    }
+    fn chain_extension(_: u32) -> Weight {
+        0
+    }
 }
