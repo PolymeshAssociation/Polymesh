9008a3175f840527499f62e4925d788adecea269
Joel Moore <47923231+JMoore96@users.noreply.github.com>
2022-06-09 09:38:18 -0400

    minor change

--- pallets/identity/src/lib.rs
+++ pallets/identity/src/lib.rs
@@ -249,6 +249,10 @@ decl_module! {
         const InitialPOLYX: <T::Balances as Currency<T::AccountId>>::Balance = T::InitialPOLYX::get().into();
 
         fn on_runtime_upgrade() -> Weight {
+            storage_migrate_on!(StorageVersion::get(), 1, {
+                migration::migrate_v1::<T>();
+            });
+
             storage_migrate_on!(StorageVersion::get(), 2, {
                 migration::migrate_v2::<T>();
             });
