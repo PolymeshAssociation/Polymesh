77844be5131e1698857a7fb7e94f40239ebb8b25
Robert Gabriel Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-08-22 21:40:30 +0800

    Add more example contracts from the Ink! project. (#1286)

---
+++ contracts/delegator/accumulator/lib.rs
@@ -0,0 +1,37 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use self::accumulator::{
+    Accumulator,
+    AccumulatorRef,
+};
+
+use ink_lang as ink;
+
+#[ink::contract]
+pub mod accumulator {
+    /// Holds a simple `i32` value that can be incremented and decremented.
+    #[ink(storage)]
+    pub struct Accumulator {
+        value: i32,
+    }
+
+    impl Accumulator {
+        /// Initializes the value to the initial value.
+        #[ink(constructor)]
+        pub fn new(init_value: i32) -> Self {
+            Self { value: init_value }
+        }
+
+        /// Mutates the internal value.
+        #[ink(message)]
+        pub fn inc(&mut self, by: i32) {
+            self.value += by;
+        }
+
+        /// Returns the current state.
+        #[ink(message)]
+        pub fn get(&self) -> i32 {
+            self.value
+        }
+    }
+}
