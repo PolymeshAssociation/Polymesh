20e441787df3289a15a5c006f65ab01f1b949469
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-09-02 02:40:17 +0800

    Fix settlement benchmarks.

--- pallets/settlement/src/benchmarking.rs
+++ pallets/settlement/src/benchmarking.rs
@@ -133,8 +133,10 @@ fn setup_leg_and_portfolio<T: Config + TestUtilsFn<AccountIdOf<T>>>(
     legs.push(Leg {
         from: portfolio_from,
         to: portfolio_to,
-        asset: ticker,
-        amount: 100u32.into(),
+        kind: LegKind::NonConfidential(NonConfidentialLeg {
+            asset: ticker,
+            amount: 100u32.into(),
+        }),
     });
     receiver_portfolios.push(portfolio_to);
     sender_portfolios.push(portfolio_from);
@@ -173,8 +175,10 @@ fn populate_legs_for_instruction<T: Config + TestUtilsFn<AccountIdOf<T>>>(
     legs.push(Leg {
         from: generate_portfolio::<T>("from_did", index + 500, None),
         to: generate_portfolio::<T>("to_did", index + 800, None),
-        asset: Ticker::generate_into(index.into()),
-        amount: 100u32.into(),
+        kind: LegKind::NonConfidential(NonConfidentialLeg {
+            asset: Ticker::generate_into(index.into()),
+            amount: 100u32.into(),
+        }),
     });
 }
 
@@ -290,8 +294,10 @@ fn emulate_portfolios<T: Config + TestUtilsFn<AccountIdOf<T>>>(
     legs.push(Leg {
         from: sender_portfolio,
         to: receiver_portfolio,
-        asset: ticker,
-        amount: transacted_amount.into(),
+        kind: LegKind::NonConfidential(NonConfidentialLeg {
+            asset: ticker,
+            amount: transacted_amount.into(),
+        }),
     })
 }
 
@@ -429,8 +435,7 @@ fn create_receipt_details<T: Config + TestUtilsFn<AccountIdOf<T>>>(
         receipt_uid: index as u64,
         from: leg.from,
         to: leg.to,
-        asset: leg.asset,
-        amount: leg.amount,
+        kind: leg.kind,
     };
     let origin = RawOrigin::Signed(account.clone());
     let creator = User {
@@ -813,7 +818,8 @@ benchmarks! {
         // Keep the portfolio asset balance before the instruction execution to verify it later.
         let legs_count: u32 = legs.len().try_into().unwrap();
         let first_leg = legs.into_iter().nth(0).unwrap_or_default();
-        let before_transfer_balance = PortfolioAssetBalances::get(first_leg.from, first_leg.asset);
+        let (first_leg_asset, first_leg_amount) = Module::<T>::leg_asset_and_amount(&first_leg)?;
+        let before_transfer_balance = PortfolioAssetBalances::get(first_leg.from, first_leg_asset);
         // It always be one as no other instruction is already scheduled.
         let instruction_id = InstructionId(1);
         let origin = RawOrigin::Root;
@@ -834,9 +840,9 @@ benchmarks! {
     }: _(origin, instruction_id, l)
     verify {
         // Assert that any one leg processed through that give sufficient evidence of successful execution of instruction.
-        let after_transfer_balance = PortfolioAssetBalances::get(first_leg.from, first_leg.asset);
+        let after_transfer_balance = PortfolioAssetBalances::get(first_leg.from, first_leg_asset);
         let traded_amount = before_transfer_balance - after_transfer_balance;
-        let expected_transfer_amount = first_leg.amount;
+        let expected_transfer_amount = first_leg_amount;
         assert_eq!(traded_amount, expected_transfer_amount,"Settlement: Failed to execute the instruction");
     }
 
