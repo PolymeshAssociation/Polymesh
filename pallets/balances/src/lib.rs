3b5e53694f9271eec2c31af4a05d35b4ba3abfee
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-01-17 22:05:19 +0800

    Apply commit https://github.com/paritytech/substrate/commit/419924c4e252b9af75957d3112c9c7a8ea521034
    To our fork.

--- pallets/balances/src/lib.rs
+++ pallets/balances/src/lib.rs
@@ -1058,6 +1058,8 @@ impl<T: Config> ReservableCurrency<T::AccountId> for Module<T> {
     /// Unreserve some funds, returning any amount that was unable to be unreserved.
     ///
     /// Is a no-op if the value to be unreserved is zero.
+    ///
+    /// NOTE: returns amount value which wasn't successfully unreserved.
     fn unreserve(who: &T::AccountId, value: Self::Balance) -> Self::Balance {
         if value.is_zero() {
             return Zero::zero();
@@ -1101,6 +1103,8 @@ impl<T: Config> ReservableCurrency<T::AccountId> for Module<T> {
     /// Is a no-op if:
     /// - the value to be moved is zero; or
     /// - the `slashed` id equal to `beneficiary` and the `status` is `Reserved`.
+    ///
+    /// NOTE: returns actual amount of transferred value in `Ok` case.
     fn repatriate_reserved(
         slashed: &T::AccountId,
         beneficiary: &T::AccountId,
@@ -1113,7 +1117,7 @@ impl<T: Config> ReservableCurrency<T::AccountId> for Module<T> {
 
         if slashed == beneficiary {
             return match status {
-                Status::Free => Ok(Self::unreserve(slashed, value)),
+                Status::Free => Ok(value.saturating_sub(Self::unreserve(slashed, value))),
                 Status::Reserved => Ok(value.saturating_sub(Self::reserved_balance(slashed))),
             };
         }
