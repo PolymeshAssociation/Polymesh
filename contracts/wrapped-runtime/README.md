4e5a44bee792bcbc9e4ef70625e782af62b0521a
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-02-08 02:28:26 +0800

    Improve macro to remove duplicate code.

--- contracts/wrapped-runtime/README.md
+++ contracts/wrapped-runtime/README.md
@@ -12,17 +12,17 @@ without having to have "admin" support in each contract.
 1. Add more useful APIs.
 2. Add namespacing of the versioned APIs to allow suport multiple upgradable APIs (Asset, Portfolio, Settlement, etc..).
 3. Support custom "Release" version in the tracker.  Right now the upgrade tracker uses the chain version (spec, tx) to trigger upgrades.  But with namescaped APIs, the tracker could be use to sync multiple contract upgrades.
-4. Add events and errors.
+4. Add events and errors to `upgrade_tracker`.
 5. Publish crates `polymesh-ink` and `upgrade_tracker`.
 
 ## Setup.
 
 1. (Optional) Build and deploy the upgrade tracker contract `./upgrade_tracker/`.
 2. Build and deploy the test contract `./example/`.  Use the contract address from step #1 if used.
-3. (Optional) Build and upload (don't deploy) the `./runtime_v5/` contract for testing upgrades.
+3. (Optional) Build and upload (don't deploy) the `./polymesh_ink/` contract for testing upgrades.
 
 ## Usable
 
 The test contract's `system_remark` and `create_asset` calls can be used.
 
-If the `runtime_v5` contract code was uploaded, then the upgrade can be tested using `update_code_hash` (admin only) or `update_polymesh_ink` (anyone, only works if the tracker is used).
+If the `polymesh_ink` contract code was uploaded, then the upgrade can be tested using `update_code_hash` (admin only) or `update_polymesh_ink` (anyone, only works if the tracker is used).
--- contracts/wrapped-runtime/polymesh-ink/src/lib.rs
+++ contracts/wrapped-runtime/polymesh-ink/src/lib.rs
@@ -2,39 +2,12 @@
 
 extern crate alloc;
 
-pub use polymesh_ink::*;
-
-#[cfg(feature = "as-library")]
-pub const API_VERSION: u32 = 5;
-
-#[cfg(feature = "as-library")]
-macro_rules! upgradable_func {
-    ($func:ident ($($param:ident: $ty:ty),+) $code:expr) => {
-        pub fn $func(&self, $($param: $ty),+) -> PolymeshResult<()> {
-            use ink_env::call::{DelegateCall, ExecutionInput, Selector};
-            if let Some(hash) = self.hash {
-                const FUNC: &str = stringify!($func);
-                let selector: [u8; 4] = ::polymesh_api::ink::blake2_256(FUNC.as_bytes())[..4]
-                  .try_into().unwrap();
-                let ret = ink_env::call::build_call::<ink_env::DefaultEnvironment>()
-                    .call_type(DelegateCall::new().code_hash(hash))
-                    .exec_input(
-                        ExecutionInput::new(Selector::new(selector))
-                            .push_arg(($($param),+)),
-                    )
-                    .returns::<PolymeshResult<()>>()
-                    .fire()
-                    .unwrap_or_else(|err| panic!("delegate call to {:?} failed due to {:?}", hash, err))?;
-                Ok(ret)
-            } else {
-                $code
-            }
-        }
-    }
-}
+mod macro_rule;
 
 #[cfg(not(feature = "as-library"))]
 use ink_lang as ink;
+
+use alloc::{vec, vec::Vec};
 
 #[cfg(feature = "tracker")]
 pub use upgrade_tracker::UpgradeTrackerRef;
@@ -47,6 +20,9 @@ pub use polymesh_api::{
         ticker::Ticker,
     },
 };
+
+#[cfg(feature = "as-library")]
+pub const API_VERSION: u32 = 5;
 
 /// The contract error types.
 #[derive(Debug, scale::Encode, scale::Decode)]
@@ -76,143 +52,52 @@ pub type Balance = <PolymeshEnvironment as ink_env::Environment>::Balance;
 #[cfg(feature = "as-library")]
 pub type Hash = <PolymeshEnvironment as ink_env::Environment>::Hash;
 
-#[cfg_attr(not(feature = "as-library"), ink::contract(env = PolymeshEnvironment))]
-mod polymesh_ink {
-    use alloc::{vec, vec::Vec};
+upgradable_api! {
+    mod polymesh_ink {
+        impl PolymeshInk {
+            /// Wrap the `system.remark` extrinsic.  Only useful for testing.
+            #[ink(message)]
+            pub fn system_remark(&self, remark: Vec<u8>) -> PolymeshResult<()> {
+                let api = Api::new();
+                api.call().system().remark(remark).submit()?;
+                Ok(())
+            }
 
-    use super::*;
+            /// Asset issue.
+            #[ink(message)]
+            pub fn asset_issue(&self, ticker: Ticker, amount: Balance) -> PolymeshResult<()> {
+                let api = Api::new();
+                // Mint some tokens.
+                api.call().asset().issue(ticker.into(), amount).submit()?;
+                Ok(())
+            }
 
-    /// Contracts would store this a value of this type.
-    #[derive(Debug, Default, scale::Encode, scale::Decode)]
-    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-    #[derive(ink_storage::traits::SpreadLayout)]
-    #[derive(ink_storage::traits::PackedLayout)]
-    #[cfg_attr(feature = "std", derive(ink_storage::traits::StorageLayout))]
-    #[cfg(feature = "as-library")]
-    pub struct PolymeshInk {
-        hash: Option<Hash>,
-        #[cfg(feature = "tracker")]
-        tracker: Option<UpgradeTrackerRef>,
-    }
-
-    /// Wrap Polymesh runtime v5.x calls.
-    #[ink(storage)]
-    #[cfg(not(feature = "as-library"))]
-    pub struct PolymeshInk {
-    }
-
-    #[cfg(not(feature = "as-library"))]
-    impl PolymeshInk {
-        /// Creates a new contract.
-        #[ink(constructor)]
-        pub fn new() -> Self {
-            panic!("Only upload this contract, don't deploy it.");
-        }
-
-        /// Very simple create asset call.
-        #[ink(message)]
-        pub fn system_remark(&mut self, remark: Vec<u8>) -> PolymeshResult<()> {
-            let api = Api::new();
-            api.call().system().remark(remark).submit()?;
-            Ok(())
-        }
-
-        #[ink(message)]
-        pub fn asset_issue(&mut self, ticker: Ticker, amount: Balance) -> PolymeshResult<()> {
-            let api = Api::new();
-            // Mint some tokens.
-            api.call().asset().issue(ticker.into(), amount).submit()?;
-            Ok(())
-        }
-
-        /// Very simple create asset call.
-        #[ink(message)]
-        pub fn asset_create_and_issue(&mut self, ticker: Ticker, amount: Balance) -> PolymeshResult<()> {
-            let api = Api::new();
-            // Create asset.
-            api.call()
-                .asset()
-                .create_asset(
-                    AssetName(b"".to_vec()),
-                    ticker.into(),
-                    true, // Divisible token.
-                    AssetType::EquityCommon,
-                    vec![],
-                    None,
-                    true, // Disable Investor uniqueness requirements.
-                )
-                .submit()?;
-            // Mint some tokens.
-            api.call().asset().issue(ticker.into(), amount).submit()?;
-            // Pause compliance rules to allow transfers.
-            api.call()
-                .compliance_manager()
-                .pause_asset_compliance(ticker.into())
-                .submit()?;
-            Ok(())
-        }
-    }
-
-    #[cfg(feature = "as-library")]
-    impl PolymeshInk {
-        #[cfg(not(feature = "tracker"))]
-        pub fn new(hash: Option<Hash>) -> Self {
-            Self { hash }
-        }
-    
-        #[cfg(feature = "tracker")]
-        pub fn new(hash: Option<Hash>, tracker: Option<UpgradeTrackerRef>) -> Self {
-            Self { hash, tracker }
-        }
-    
-        /// Update code hash.
-        pub fn update_code_hash(&mut self, hash: Option<Hash>) {
-            self.hash = hash;
-        }
-    
-        #[cfg(feature = "tracker")]
-        pub fn check_for_upgrade(&mut self) {
-            if let Some(tracker) = &self.tracker {
-                self.hash = tracker.get_latest_upgrade(API_VERSION);
+            /// Very simple create asset and issue.
+            #[ink(message)]
+            pub fn asset_create_and_issue(&self, ticker: Ticker, amount: Balance) -> PolymeshResult<()> {
+                let api = Api::new();
+                // Create asset.
+                api.call()
+                    .asset()
+                    .create_asset(
+                        AssetName(b"".to_vec()),
+                        ticker.into(),
+                        true, // Divisible token.
+                        AssetType::EquityCommon,
+                        vec![],
+                        None,
+                        true, // Disable Investor uniqueness requirements.
+                    )
+                    .submit()?;
+                // Mint some tokens.
+                api.call().asset().issue(ticker.into(), amount).submit()?;
+                // Pause compliance rules to allow transfers.
+                api.call()
+                    .compliance_manager()
+                    .pause_asset_compliance(ticker.into())
+                    .submit()?;
+                Ok(())
             }
         }
-    
-        upgradable_func!(system_remark (remark: Vec<u8>) {
-            let api = Api::new();
-            api.call().system().remark(remark).submit()?;
-            Ok(())
-        });
-    
-        upgradable_func!(asset_issue (ticker: Ticker, amount: Balance) {
-            let api = Api::new();
-            // Mint some tokens.
-            api.call().asset().issue(ticker, amount).submit()?;
-            Ok(())
-        });
-    
-        upgradable_func!(asset_create_and_issue (ticker: Ticker, amount: Balance) {
-            let api = Api::new();
-            // Create asset.
-            api.call()
-                .asset()
-                .create_asset(
-                    AssetName(b"".to_vec()),
-                    ticker.into(),
-                    true, // Divisible token.
-                    AssetType::EquityCommon,
-                    vec![],
-                    None,
-                    true, // Disable Investor uniqueness requirements.
-                )
-                .submit()?;
-            // Mint some tokens.
-            api.call().asset().issue(ticker.into(), amount).submit()?;
-            // Pause compliance rules to allow transfers.
-            api.call()
-                .compliance_manager()
-                .pause_asset_compliance(ticker.into())
-                .submit()?;
-            Ok(())
-        });
     }
 }
