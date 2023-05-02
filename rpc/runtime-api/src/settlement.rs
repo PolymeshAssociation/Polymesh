67569e1f83f6f806eaf219f172fc5f703e413357
Henrique Nogara <nogara.henrique@gmail.com>
2023-05-02 16:23:50 -0300

    Fix doc; Move parameter to last struct position

--- rpc/runtime-api/src/settlement.rs
+++ rpc/runtime-api/src/settlement.rs
@@ -28,7 +28,7 @@ sp_api::decl_runtime_apis! {
         ///     "id":1,
         ///     "jsonrpc":"2.0",
         ///     "method": "settlement_GetExecuteInstructionInfo",
-        ///     "params":[]
+        ///     "params": [1]
         ///   }'
         /// ```
         fn get_execute_instruction_info(instruction_id: &InstructionId) -> ExecuteInstructionInfo;
