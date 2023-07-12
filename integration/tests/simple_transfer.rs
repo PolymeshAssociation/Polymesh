69e9b4885d59d59029cba7a3405a67ddeba23010
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-07-13 03:12:27 +0800

    cargo fmt

--- integration/tests/simple_transfer.rs
+++ integration/tests/simple_transfer.rs
@@ -4,16 +4,17 @@ use integration::*;
 
 #[tokio::test]
 async fn simple_polyx_transfer() -> Result<()> {
-  let mut tester = PolymeshTester::new().await?;
-  let mut users = tester.users(&["User1", "User2"]).await?;
+    let mut tester = PolymeshTester::new().await?;
+    let mut users = tester.users(&["User1", "User2"]).await?;
 
-  let mut res = tester.api
-    .call()
-    .balances()
-    .transfer(users[1].account().into(), 13 * ONE_POLYX)?
-    .execute(&mut users[0])
-    .await?;
-  let events = res.events().await?;
-  println!("call1 events = {:#?}", events);
-  Ok(())
+    let mut res = tester
+        .api
+        .call()
+        .balances()
+        .transfer(users[1].account().into(), 13 * ONE_POLYX)?
+        .execute(&mut users[0])
+        .await?;
+    let events = res.events().await?;
+    println!("call1 events = {:#?}", events);
+    Ok(())
 }
