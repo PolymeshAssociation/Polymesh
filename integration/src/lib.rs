672d3dadf58863d00151d15655e59b77a2ac7c99
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-06-18 03:21:37 +0800

    Add improved secondary key permissions tests.

--- integration/src/lib.rs
+++ integration/src/lib.rs
@@ -2,6 +2,8 @@
 pub use polymesh_api_tester::extras::*;
 pub use polymesh_api_tester::*;
 
+use std::collections::{BTreeMap, BTreeSet};
+
 use polymesh_api::types::polymesh_primitives::{
     identity_id::PortfolioId, secondary_key::PalletPermissions, subset::SubsetRestriction,
     DispatchableName, PalletName,
@@ -10,114 +12,233 @@ use polymesh_api::*;
 
 use anyhow::{anyhow, Result};
 
-/// Helper trait to add methods to `Permissions`
-pub trait IntegrationPermissions {
-    fn whole() -> Self;
-    fn empty() -> Self;
-    fn set_asset(&mut self, assets: &[Ticker], these: bool);
-    fn set_portfolio(&mut self, portfolios: &[PortfolioId], these: bool);
-    fn set_extrinsic(&mut self, pallets: &[PalletPermissions], these: bool);
-    fn allow_extrinsic(&mut self, pallet: &str, extrinsic: &str);
-    fn clear_asset(&mut self);
-    fn clear_portfolio(&mut self);
-    fn clear_extrinsic(&mut self);
-    fn clear_all(&mut self) {
-        self.clear_asset();
-        self.clear_portfolio();
-        self.clear_extrinsic();
+#[derive(Clone, Default, PartialEq, Eq)]
+pub enum RestrictionMode {
+    #[default]
+    Whole,
+    These,
+    Except,
+}
+
+#[derive(Clone)]
+pub struct SubsetBuilder<T> {
+    mode: RestrictionMode,
+    entries: Option<BTreeSet<T>>,
+}
+
+impl<T> Default for SubsetBuilder<T> {
+    fn default() -> Self {
+        Self {
+            mode: Default::default(),
+            entries: None,
+        }
     }
 }
 
-impl IntegrationPermissions for Permissions {
+impl<T: Clone + Ord> SubsetBuilder<T> {
     fn whole() -> Self {
-        Permissions {
-            asset: SubsetRestriction::Whole,
-            extrinsic: SubsetRestriction::Whole,
-            portfolio: SubsetRestriction::Whole,
+        Self {
+            mode: RestrictionMode::Whole,
+            entries: None,
         }
     }
 
-    fn empty() -> Permissions {
-        Permissions {
-            asset: SubsetRestriction::These(Default::default()),
-            extrinsic: SubsetRestriction::These(Default::default()),
-            portfolio: SubsetRestriction::These(Default::default()),
+    fn empty() -> Self {
+        Self {
+            mode: RestrictionMode::These,
+            entries: Some(Default::default()),
         }
     }
 
-    fn set_asset(&mut self, assets: &[Ticker], these: bool) {
+    pub fn set(&mut self, entries: &[T], these: bool) {
         if these {
-            self.asset = SubsetRestriction::These(assets.iter().cloned().collect())
+            self.mode = RestrictionMode::These;
         } else {
-            self.asset = SubsetRestriction::Except(assets.iter().cloned().collect())
+            self.mode = RestrictionMode::Except;
+        }
+        self.entries = Some(entries.iter().cloned().collect())
+    }
+
+    pub fn add(&mut self, entry: &T) {
+        let entries = self.entries.get_or_insert(Default::default());
+        entries.insert(entry.clone());
+    }
+
+    pub fn build(&self) -> SubsetRestriction<T> {
+        match &self.mode {
+            RestrictionMode::Whole => SubsetRestriction::Whole,
+            RestrictionMode::These => {
+                SubsetRestriction::These(self.entries.clone().unwrap_or_default())
+            }
+            RestrictionMode::Except => {
+                SubsetRestriction::Except(self.entries.clone().unwrap_or_default())
+            }
+        }
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct PalletPermissionsBuilder {
+    mode: RestrictionMode,
+    entries: Option<BTreeMap<String, SubsetBuilder<String>>>,
+}
+
+impl PalletPermissionsBuilder {
+    fn whole() -> Self {
+        Self {
+            mode: RestrictionMode::Whole,
+            entries: None,
         }
     }
 
-    fn set_portfolio(&mut self, portfolios: &[PortfolioId], these: bool) {
-        if these {
-            self.portfolio = SubsetRestriction::These(portfolios.iter().cloned().collect())
-        } else {
-            self.portfolio = SubsetRestriction::Except(portfolios.iter().cloned().collect())
+    fn empty() -> Self {
+        Self {
+            mode: RestrictionMode::These,
+            entries: Some(Default::default()),
         }
     }
 
-    fn set_extrinsic(&mut self, extrinsics: &[PalletPermissions], these: bool) {
+    pub fn set(&mut self, extrinsics: &[(String, String)], these: bool) {
         if these {
-            self.extrinsic = SubsetRestriction::These(extrinsics.iter().cloned().collect())
+            self.mode = RestrictionMode::These;
         } else {
-            self.extrinsic = SubsetRestriction::Except(extrinsics.iter().cloned().collect())
+            self.mode = RestrictionMode::Except;
         }
+        let entries = self.entries.get_or_insert(Default::default());
+        for (pallet, extrinsic) in extrinsics {
+            let pallet = entries.entry(pallet.clone()).or_default();
+            pallet.add(extrinsic);
+        }
+    }
+
+    fn allow_pallet(&mut self, pallet: &str) {
+        if self.mode != RestrictionMode::These {
+            self.mode = RestrictionMode::These;
+            self.entries = None;
+        }
+        let entries = self.entries.get_or_insert(Default::default());
+        let pallet = entries.entry(pallet.to_string()).or_default();
+        *pallet = SubsetBuilder::whole();
     }
 
     fn allow_extrinsic(&mut self, pallet: &str, extrinsic: &str) {
-        let pallet_name = PalletName(pallet.as_bytes().into());
-        let dispatchable = DispatchableName(extrinsic.as_bytes().into());
-        if let SubsetRestriction::These(pallets) = &mut self.extrinsic {
-            // Check if the pallet is already in the set.
-            for pallet in pallets.clone() {
-                if pallet.pallet_name == pallet_name {
-                    // Found it.
-                    let mut new_pallet = pallet.clone();
-                    if let SubsetRestriction::These(dispatchables) =
-                        &mut new_pallet.dispatchable_names
-                    {
-                        dispatchables.insert(dispatchable);
-                    } else {
-                        new_pallet.dispatchable_names =
-                            SubsetRestriction::These([dispatchable].into());
+        if self.mode != RestrictionMode::These {
+            self.mode = RestrictionMode::These;
+            self.entries = None;
+        }
+        let entries = self.entries.get_or_insert(Default::default());
+        let pallet = entries.entry(pallet.to_string()).or_default();
+        pallet.add(&extrinsic.to_string());
+    }
+
+    fn build_entries(&self) -> BTreeSet<PalletPermissions> {
+        if let Some(entries) = &self.entries {
+            entries
+                .iter()
+                .map(|(pallet, extrinsics)| {
+                    let dispatchable_names = match extrinsics.build() {
+                        SubsetRestriction::Whole => SubsetRestriction::Whole,
+                        SubsetRestriction::These(names) => SubsetRestriction::These(
+                            names
+                                .into_iter()
+                                .map(|n| DispatchableName(n.as_bytes().into()))
+                                .collect(),
+                        ),
+                        SubsetRestriction::Except(names) => SubsetRestriction::Except(
+                            names
+                                .into_iter()
+                                .map(|n| DispatchableName(n.as_bytes().into()))
+                                .collect(),
+                        ),
+                    };
+                    PalletPermissions {
+                        pallet_name: PalletName(pallet.as_bytes().into()),
+                        dispatchable_names,
                     }
-                    pallets.remove(&pallet);
-                    pallets.insert(new_pallet);
-                    return;
-                }
-            }
-            // Need to add the pallet.
-            pallets.insert(PalletPermissions {
-                pallet_name,
-                dispatchable_names: SubsetRestriction::These([dispatchable].into()),
-            });
+                })
+                .collect()
         } else {
-            // Convert from `Whole` or `Except`.
-            self.extrinsic = SubsetRestriction::These(
-                [PalletPermissions {
-                    pallet_name,
-                    dispatchable_names: SubsetRestriction::These([dispatchable].into()),
-                }]
-                .into(),
-            );
+            Default::default()
         }
     }
 
-    fn clear_asset(&mut self) {
-        self.asset = SubsetRestriction::These(Default::default())
+    pub fn build(&self) -> SubsetRestriction<PalletPermissions> {
+        match &self.mode {
+            RestrictionMode::Whole => SubsetRestriction::Whole,
+            RestrictionMode::These => SubsetRestriction::These(self.build_entries()),
+            RestrictionMode::Except => SubsetRestriction::Except(self.build_entries()),
+        }
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct PermissionsBuilder {
+    asset: SubsetBuilder<Ticker>,
+    portfolio: SubsetBuilder<PortfolioId>,
+    extrinsic: PalletPermissionsBuilder,
+}
+
+impl PermissionsBuilder {
+    pub fn whole() -> Self {
+        Self {
+            asset: SubsetBuilder::whole(),
+            portfolio: SubsetBuilder::whole(),
+            extrinsic: PalletPermissionsBuilder::whole(),
+        }
     }
 
-    fn clear_portfolio(&mut self) {
-        self.portfolio = SubsetRestriction::These(Default::default())
+    pub fn empty() -> Self {
+        Self {
+            asset: SubsetBuilder::empty(),
+            portfolio: SubsetBuilder::empty(),
+            extrinsic: PalletPermissionsBuilder::empty(),
+        }
     }
 
-    fn clear_extrinsic(&mut self) {
-        self.extrinsic = SubsetRestriction::These(Default::default())
+    pub fn set_asset(&mut self, assets: &[Ticker], these: bool) {
+        self.asset.set(assets, these);
+    }
+
+    pub fn set_portfolio(&mut self, portfolios: &[PortfolioId], these: bool) {
+        self.portfolio.set(portfolios, these);
+    }
+
+    pub fn set_extrinsic(&mut self, extrinsics: &[(String, String)], these: bool) {
+        self.extrinsic.set(extrinsics, these);
+    }
+
+    pub fn allow_pallet(&mut self, pallet: &str) {
+        self.extrinsic.allow_pallet(pallet);
+    }
+
+    pub fn allow_extrinsic(&mut self, pallet: &str, extrinsic: &str) {
+        self.extrinsic.allow_extrinsic(pallet, extrinsic);
+    }
+
+    pub fn clear_asset(&mut self) {
+        self.asset = SubsetBuilder::empty()
+    }
+
+    pub fn clear_portfolio(&mut self) {
+        self.portfolio = SubsetBuilder::empty()
+    }
+
+    pub fn clear_extrinsic(&mut self) {
+        self.extrinsic = PalletPermissionsBuilder::empty()
+    }
+
+    pub fn build(&self) -> Permissions {
+        Permissions {
+            asset: self.asset.build(),
+            portfolio: self.portfolio.build(),
+            extrinsic: self.extrinsic.build(),
+        }
+    }
+}
+
+impl From<&PermissionsBuilder> for Permissions {
+    fn from(builder: &PermissionsBuilder) -> Self {
+        builder.build()
     }
 }
 
@@ -126,13 +247,37 @@ impl IntegrationPermissions for Permissions {
 pub trait IntegrationUser: Signer {
     fn get_sk(&self, sk: usize) -> Result<&AccountSigner>;
 
+    fn get_sk_count(&self) -> usize;
+
     fn get_sk_mut(&mut self, sk: usize) -> Result<&mut AccountSigner>;
 
     async fn set_key_permissions(
         &mut self,
         sk: usize,
-        permissions: &Permissions,
+        permissions: impl Into<Permissions> + Send,
     ) -> Result<TransactionResults>;
+
+    async fn set_all_keys_permissions(
+        &mut self,
+        permissions: impl Into<Permissions> + Send,
+    ) -> Result<()> {
+        let permissions = permissions.into();
+        let sk_count = self.get_sk_count();
+        let mut results = Vec::new();
+        for sk in 0..sk_count {
+            results.push(self.set_key_permissions(sk, permissions.clone()).await?);
+        }
+        for mut res in results {
+            res.ok().await?;
+        }
+        Ok(())
+    }
+
+    async fn ensure_key_permissions(
+        &self,
+        sk: usize,
+        permissions: impl Into<Permissions> + Send,
+    ) -> Result<()>;
 
     async fn create_child_identity(&mut self, sk: usize) -> Result<User>;
 }
@@ -145,6 +290,10 @@ impl IntegrationUser for User {
             .ok_or_else(|| anyhow!("Missing secondary key: {sk}"))
     }
 
+    fn get_sk_count(&self) -> usize {
+        self.secondary_keys.len()
+    }
+
     fn get_sk_mut(&mut self, sk: usize) -> Result<&mut AccountSigner> {
         self.secondary_keys
             .get_mut(sk)
@@ -154,17 +303,52 @@ impl IntegrationUser for User {
     async fn set_key_permissions(
         &mut self,
         sk: usize,
-        permissions: &Permissions,
+        permissions: impl Into<Permissions> + Send,
     ) -> Result<TransactionResults> {
+        let permissions = permissions.into();
         let sk = self.get_sk(sk)?.account();
         let res = self
             .api
             .call()
             .identity()
-            .set_secondary_key_permissions(sk, permissions.clone())?
+            .set_secondary_key_permissions(sk, permissions)?
             .submit_and_watch(self)
             .await?;
         Ok(res)
+    }
+
+    async fn ensure_key_permissions(
+        &self,
+        sk: usize,
+        permissions: impl Into<Permissions> + Send,
+    ) -> Result<()> {
+        let permissions = permissions.into();
+        let sk = self.get_sk(sk)?.account();
+        let asset = self
+            .api
+            .query()
+            .identity()
+            .key_asset_permissions(sk)
+            .await?
+            .ok_or_else(|| anyhow!("Missing asset permissions"))?;
+        assert_eq!(permissions.asset, asset);
+        let portfolio = self
+            .api
+            .query()
+            .identity()
+            .key_portfolio_permissions(sk)
+            .await?
+            .ok_or_else(|| anyhow!("Missing portfolio permissions"))?;
+        assert_eq!(permissions.portfolio, portfolio);
+        let extrinsic = self
+            .api
+            .query()
+            .identity()
+            .key_extrinsic_permissions(sk)
+            .await?
+            .ok_or_else(|| anyhow!("Missing extrinsic permissions"))?;
+        assert_eq!(permissions.extrinsic, extrinsic);
+        Ok(())
     }
 
     async fn create_child_identity(&mut self, sk: usize) -> Result<User> {
--- integration/tests/secondary_keys.rs
+++ integration/tests/secondary_keys.rs
@@ -1,32 +1,207 @@
-use anyhow::Result;
+use std::sync::Arc;
+
+use anyhow::{anyhow, Result};
+
+use tokio::task::JoinHandle;
+
+use polymesh_api::{
+    types::polymesh_primitives::{
+        authorization::AuthorizationData,
+        secondary_key::Signatory,
+        settlement::{VenueDetails, VenueType},
+    },
+    TransactionResults, WrappedCall,
+};
 
 use integration::*;
 
-#[tokio::test]
-async fn secondary_keys_permissions() -> Result<()> {
-    let mut tester = PolymeshTester::new().await?;
-    let users = tester
-        .users_with_secondary_keys(&[("SecondaryKeysPermissions", 10)])
-        .await?;
-
+async fn test_sk_call(
+    users: &mut [User],
+    call: Arc<WrappedCall>,
+    expect_ok: bool,
+) -> Result<Vec<TransactionResults>> {
     let mut results = Vec::new();
-    for mut user in users {
-        let pk = user.account();
+    for user in users {
         for sk in &mut user.secondary_keys {
             // Send some POLYX back to the primary key from the secondary key.
-            let res = tester
-                .api
-                .call()
-                .balances()
-                .transfer(pk.into(), ONE_POLYX)?
-                .submit_and_watch(sk)
-                .await?;
+            let res = call.submit_and_watch(sk).await?;
             results.push(res);
         }
     }
     // Wait for all results.
-    for mut res in results {
-        println!("transfer res = {:#?}", res.ok().await);
+    for res in &mut results {
+        let result = res.ok().await;
+        if expect_ok {
+            assert!(result.is_ok());
+        } else {
+            assert!(result.is_err());
+        }
     }
+    Ok(results)
+}
+
+async fn test_sk_calls(
+    api: &Api,
+    users: &mut Vec<User>,
+    calls: &[(&Arc<WrappedCall>, bool)],
+) -> Result<()> {
+    let mut tasks: Vec<JoinHandle<Result<()>>> = Vec::new();
+
+    let mut expect_all_ok = true;
+    let mut batch = Vec::with_capacity(calls.len());
+    let mut expected = Vec::with_capacity(calls.len());
+    // Test each call.
+    for (call, expect_ok) in calls {
+        if !expect_ok {
+            expect_all_ok = false;
+        }
+        expected.push(*expect_ok);
+        batch.push(call.runtime_call().clone());
+        let mut users = users.clone();
+        let call = (*call).clone();
+        let expect_ok = *expect_ok;
+        tasks.push(tokio::spawn(async move {
+            test_sk_call(&mut users, call, expect_ok).await?;
+            Ok(())
+        }));
+    }
+    if expect_all_ok {
+        let call = Arc::new(api.call().utility().batch_all(batch)?);
+        let mut users = users.clone();
+        tasks.push(tokio::spawn(async move {
+            test_sk_call(&mut users, call, true).await?;
+            Ok(())
+        }));
+    } else {
+        // `batch_all` should fail, since at least one call is expected to fail.
+        {
+            let call = Arc::new(api.call().utility().batch_all(batch.clone())?);
+            let mut users = users.clone();
+            tasks.push(tokio::spawn(async move {
+                test_sk_call(&mut users, call, false).await?;
+                Ok(())
+            }));
+        }
+        // Use `force_batch` and check the results for each call in the batch.
+        let call = Arc::new(api.call().utility().force_batch(batch)?);
+        let results = test_sk_call(users, call, true).await?;
+        for mut res in results {
+            let events = res
+                .events()
+                .await?
+                .ok_or_else(|| anyhow!("Failed to get batch events"))?;
+            let calls_ok = events
+                .0
+                .iter()
+                .filter_map(|rec| match rec.event {
+                    RuntimeEvent::Utility(UtilityEvent::ItemCompleted) => Some(true),
+                    RuntimeEvent::Utility(UtilityEvent::ItemFailed { .. }) => Some(false),
+                    _ => None,
+                })
+                .collect::<Vec<bool>>();
+            assert_eq!(calls_ok, expected);
+        }
+    }
+    // Wait for tasks to finish.
+    for task in tasks {
+        task.await??;
+    }
+    Ok(())
+}
+
+#[tokio::test]
+async fn secondary_keys_permissions() -> Result<()> {
+    let mut tester = PolymeshTester::new().await?;
+    const SK_COUNT: usize = 10;
+    let mut users = tester
+        .users_with_secondary_keys(&[("SecondaryKeysPermissions", SK_COUNT)])
+        .await?;
+
+    // Prepare a POLYX transfer to one user.
+    let user0_pk = users[0].account();
+    let balance_transfer_call =
+        Arc::new(tester.api.call().balances().transfer(user0_pk.into(), 1)?);
+    // Prepare `system.remark` call.
+    let remark_call = Arc::new(tester.api.call().system().remark(vec![])?);
+    // Prepare `settlement.create_venue` call.
+    let create_venue_call = Arc::new(tester.api.call().settlement().create_venue(
+        VenueDetails(vec![]),
+        vec![],
+        VenueType::Other,
+    )?);
+    // Prepare `identity.add_authorization` call.
+    let add_auth_call = Arc::new(tester.api.call().identity().add_authorization(
+        Signatory::Account(user0_pk),
+        AuthorizationData::RotatePrimaryKey,
+        None,
+    )?);
+
+    // Ensure all secondary keys start with `Whole` permissions.
+    let whole = PermissionsBuilder::whole();
+    for user in &users {
+        for sk in 0..SK_COUNT {
+            user.ensure_key_permissions(sk, &whole).await?;
+        }
+    }
+
+    test_sk_calls(
+        &tester.api,
+        &mut users,
+        &[
+            // Ensure the secndary keys can do simple calls.
+            (&balance_transfer_call, true),
+            (&remark_call, true),
+            // The keys should be allowed to create a venue.
+            (&create_venue_call, true),
+            // The keys should be allowed to add an authorization.
+            (&add_auth_call, true),
+        ],
+    )
+    .await?;
+
+    // Remove all permissions from the secondary keys.
+    let mut perms = PermissionsBuilder::empty();
+    for user in &mut users {
+        user.set_all_keys_permissions(&perms).await?;
+    }
+
+    test_sk_calls(
+        &tester.api,
+        &mut users,
+        &[
+            // Ensure the secndary keys can still do `balance.transfer` and `system.remark` calls.
+            // These calls are not restricted by key permissions.
+            (&balance_transfer_call, true),
+            (&remark_call, true),
+            // The keys shouldn't be allowed to create venues.
+            (&create_venue_call, false),
+            // The keys shouldn't be allowed to add an authorization.
+            (&add_auth_call, false),
+        ],
+    )
+    .await?;
+
+    // Allow the `Settlement` pallet.
+    perms.allow_pallet("Settlement");
+    for user in &mut users {
+        user.set_all_keys_permissions(&perms).await?;
+    }
+
+    test_sk_calls(
+        &tester.api,
+        &mut users,
+        &[
+            // Ensure the secndary keys can still do `balance.transfer` and `system.remark` calls.
+            // These calls are not restricted by key permissions.
+            (&balance_transfer_call, true),
+            (&remark_call, true),
+            // The keys should be allowed to create venues.
+            (&create_venue_call, true),
+            // The keys shouldn't be allowed to add an authorization.
+            (&add_auth_call, false),
+        ],
+    )
+    .await?;
+
     Ok(())
 }
