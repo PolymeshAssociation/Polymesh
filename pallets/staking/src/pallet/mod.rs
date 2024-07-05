aaecf1e47ddf9cf4281dff9afac5486764e60ecf
Henrique Nogara <nogara.henrique@gmail.com>
2024-07-05 09:48:47 -0300

    Fix set-controller and ref count update

--- pallets/staking/src/pallet/mod.rs
+++ pallets/staking/src/pallet/mod.rs
@@ -1428,9 +1428,12 @@ pub mod pallet {
             if <Ledger<T>>::contains_key(&controller) {
                 return Err(Error::<T>::AlreadyPaired.into());
             }
-            // Prevents stashes which are controllers from calling the extrinsic
-            if <Ledger<T>>::contains_key(&stash) {
-                return Err(Error::<T>::BadState.into());
+
+            // Prevents stashes which are controllers of another ledger from calling the extrinsic
+            if let Some(bonded_ledger) = <Ledger<T>>::get(&stash) {
+                if bonded_ledger.stash != stash {
+                    return Err(Error::<T>::BadState.into());
+                }
             }
 
             if controller != old_controller {
