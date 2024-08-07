7857fcc8b14ff8a88880857ad69e8cced763a41b
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-08-07 23:12:34 +0800

    Use GC_DID in treasury event.

--- pallets/treasury/src/lib.rs
+++ pallets/treasury/src/lib.rs
@@ -235,9 +235,7 @@ impl<T: Config> OnUnbalanced<NegativeImbalanceOf<T>> for Module<T> {
         let numeric_amount = amount.peek();
 
         let _ = T::Currency::resolve_creating(&Self::account_id(), amount);
-        // TODO: Should we use a fixed value here?
-        let current_did = Context::current_identity::<Identity<T>>().unwrap_or_default();
-        Self::deposit_event(RawEvent::TreasuryReimbursement(current_did, numeric_amount));
+        Self::deposit_event(RawEvent::TreasuryReimbursement(GC_DID, numeric_amount));
     }
 }
 
