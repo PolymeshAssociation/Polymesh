8db72a7825b52bc736ecb068cfe8467a18d523f7
Adam Dossa <adam.dossa@gmail.com>
2024-09-17 10:17:05 +0100

    Update tests

--- pallets/identity/src/lib.rs
+++ pallets/identity/src/lib.rs
@@ -646,7 +646,7 @@ decl_error! {
         NotASigner,
         /// Cannot convert a `T::AccountId` to `AnySignature::Signer::AccountId`.
         CannotDecodeSignerAccountId,
-        /// Multisig can not be unlinked from an identity while it still holds POLYX
+        /// Multisig can not be unlinked from an identity while it still holds POLYX - Not Used
         MultiSigHasBalance,
         /// The account key is being used, it can't be unlinked.
         AccountKeyIsBeingUsed,
