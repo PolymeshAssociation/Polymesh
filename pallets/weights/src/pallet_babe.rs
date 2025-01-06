93721c045b1e9f968df8b7a6f06a2cf317af2b85
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-01-07 00:53:15 +0800

    Update weights.

--- pallets/weights/src/pallet_babe.rs
+++ pallets/weights/src/pallet_babe.rs
@@ -1,13 +1,13 @@
 // This file is part of Substrate.
 
-// Copyright (C) 2021 Parity Technologies (UK) Ltd.
+// Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// 	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,23 +21,17 @@
 use polymesh_primitives::{
     RocksDbWeight as DbWeight, Weight, WEIGHT_REF_TIME_PER_MICROS, WEIGHT_REF_TIME_PER_NANOS,
 };
-
 /// Weights for pallet_babe using the Substrate node and recommended hardware.
 pub struct SubstrateWeight;
 impl pallet_babe::WeightInfo for SubstrateWeight {
     fn plan_config_change() -> Weight {
-        Weight::zero()
+        DbWeight::get().writes(1)
     }
 
-    // WARNING! Some components were not used: ["x"]
-    fn report_equivocation(validator_count: u32) -> Weight {
+    fn report_equivocation(validator_count: u32, max_nominators_per_validator: u32) -> Weight {
         // we take the validator set count from the membership proof to
         // calculate the weight but we set a floor of 100 validators.
         let validator_count = validator_count.max(100) as u64;
-
-        // worst case we are considering is that the given offender
-        // is backed by 200 nominators
-        const MAX_NOMINATORS: u64 = 200;
 
         // checking membership proof
         Weight::from_parts(35u64 * WEIGHT_REF_TIME_PER_MICROS, 0)
@@ -51,10 +45,10 @@ impl pallet_babe::WeightInfo for SubstrateWeight {
             // report offence
             .saturating_add(Weight::from_parts(110u64 * WEIGHT_REF_TIME_PER_MICROS, 0))
             .saturating_add(Weight::from_parts(
-                25u64 * WEIGHT_REF_TIME_PER_MICROS * MAX_NOMINATORS,
+                25u64 * WEIGHT_REF_TIME_PER_MICROS * max_nominators_per_validator as u64,
                 0,
             ))
-            .saturating_add(DbWeight::get().reads(14 + 3 * MAX_NOMINATORS))
-            .saturating_add(DbWeight::get().writes(10 + 3 * MAX_NOMINATORS))
+            .saturating_add(DbWeight::get().reads(14 + 3 * max_nominators_per_validator as u64))
+            .saturating_add(DbWeight::get().writes(10 + 3 * max_nominators_per_validator as u64))
     }
 }
