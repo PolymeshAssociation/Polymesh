3821c18615f276b8b65c3b31a5f5efb525d0d22d
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-01-07 01:07:29 +0800

    Bump recursion_limit

--- pallets/runtime/mainnet/src/lib.rs
+++ pallets/runtime/mainnet/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
-#![recursion_limit = "256"]
+#![recursion_limit = "1024"]
 
 pub mod constants;
 pub mod runtime;
