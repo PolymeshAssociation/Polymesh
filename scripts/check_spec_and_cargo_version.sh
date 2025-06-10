bdd313f1812b9f969b0ea30a2bb6d4e6228c38d7
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-06-11 01:50:31 +0800

    Add support for release candidates.

--- scripts/check_spec_and_cargo_version.sh
+++ scripts/check_spec_and_cargo_version.sh
@@ -8,10 +8,14 @@ else
 fi
 POLYMESH_VERSION=`grep -h -r ' spec_version: ' pallets/runtime/*/src/runtime.rs | sort -u | sed -e 's/.*spec_version: \([0-9]\+\)_[0]*\([0-9]\+\)_[0]*\([0-9]\+\)[0-9],/\1.\2.\3/g'`
 
-if head Cargo.toml | grep -q $POLYMESH_VERSION; then
+NODE_PKG_VERSION=`cargo info -q --color never polymesh | grep "^version:" | sed -e 's/version: \([0-9a-z.-]*\) .*/\1/g'`
+
+if echo "$NODE_PKG_VERSION" | grep -q $POLYMESH_VERSION; then
 	echo "Spec version matches Polymesh version."
 	exit 0
 else
 	echo "Spec version doesn't match Polymesh version."
+	echo "Version from spec_version=$POLYMESH_VERSION"
+	echo "Version from node package=$NODE_PKG_VERSION"
 	exit 1
 fi
