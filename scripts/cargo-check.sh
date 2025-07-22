c8e6ed2c544d51cf9807bf6c03caab8414ffbd86
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-07-22 17:23:00 +0800

    Remove old scripts.

--- scripts/cargo-check.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-script_dir=$(dirname $0)
-pushd $script_dir/../ 1>/dev/null
-	# Do linting, the custom script has to account for wasm gimmicks
-	cargo check || (echo "cargo-check FAIL" && false)
-	echo cargo-check OK
-popd 1>/dev/null
