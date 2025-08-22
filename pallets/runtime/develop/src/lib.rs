6b7971e44437d924e3fe172a903ecbc90daf8437
Henrique Nogara <nogara.henrique@gmail.com>
2025-08-22 11:22:18 -0300

    Update develop runtime

--- pallets/runtime/develop/src/lib.rs
+++ pallets/runtime/develop/src/lib.rs
@@ -18,10 +18,10 @@ pub use pallet_staking::StakerStatus;
 #[cfg(feature = "std")]
 pub use runtime::{native_version, WASM_BINARY};
 
+pub use sp_runtime::{Perbill, Permill};
+
 pub use runtime::{
     api, Asset, Authorship, Balances, BalancesCall, CheckedExtrinsic, MinimumPeriod, ProtocolFee,
     Runtime, RuntimeApi, RuntimeCall, SessionKeys, System, SystemCall, TransactionPayment,
     UncheckedExtrinsic,
 };
-
-pub use sp_runtime::{Perbill, Permill};
