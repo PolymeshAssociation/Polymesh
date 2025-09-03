f1f19ec5d351ce89563cc23bc5c5558178a62caa
Henrique Nogara <nogara.henrique@gmail.com>
2025-09-03 10:06:31 -0300

    Add missing GenesisBuilder impl; Use camel case for json genesis; Add validators genesis config

--- src/chain_spec/mainnet_runtime.rs
+++ src/chain_spec/mainnet_runtime.rs
@@ -3,6 +3,7 @@ use sc_chain_spec::ChainType;
 use sc_network::config::MultiaddrWithPeerId;
 use sc_telemetry::TelemetryEndpoints;
 use sp_runtime::traits::AccountIdConversion;
+use sp_runtime::PerThing;
 
 use polymesh_primitives::constants::TREASURY_PALLET_ID;
 use polymesh_primitives::identity_id::GenesisIdentityRecord;
@@ -11,12 +12,13 @@ use polymesh_primitives::{Balance, IdentityId, SystematicIssuers};
 use polymesh_runtime_mainnet::constants::time::DAYS;
 use polymesh_runtime_mainnet::runtime::{SessionKeys, BABE_GENESIS_EPOCH_CONFIG};
 
+use crate::chain_spec::common::asset_genesis_config;
 use crate::chain_spec::common::{adjust_last, get_authority_keys_from_seed};
-use crate::chain_spec::common::{asset_genesis_config, protocol_fee_genesis_config};
 use crate::chain_spec::common::{checkpoint_genesis_config, committee_genesis_config};
 use crate::chain_spec::common::{corporate_actions_genesis_config, staking_genesis_config};
 use crate::chain_spec::common::{group_genesis_config, polymesh_properties, seeded_acc_id};
 use crate::chain_spec::common::{pips_genesis_config, polymesh_contracts_genesis_config};
+use crate::chain_spec::common::{protocol_fee_genesis_config, validators_genesis_config};
 use crate::chain_spec::common::{ChainSpec, ChainSpecMode};
 use crate::chain_spec::common::{GenesisData, InitialAuth, StakersData};
 use crate::chain_spec::common::{BOOTSTRAP_KEYS, BOOTSTRAP_TREASURY};
@@ -118,25 +120,26 @@ fn mainnet_genesis_config(
         "session": {
             "keys": session_keys,
         },
+        "validators": validators_genesis_config(&genesis_data.stakers_data, PerThing::from_rational(1u64, 4u64)),
         "staking": staking_genesis_config(&genesis_data.stakers_data),
         "pips": pips_genesis_config(DAYS * 30, MaybeBlock::Some(DAYS * 90), 1_000),
         "babe": {
             "epoch_config": Some(BABE_GENESIS_EPOCH_CONFIG),
         },
         // Governing council
-        "committee_membership": group_genesis_config(vec![identity_1, identity_2, identity_3]),  // three GC members
+        "committeeMembership": group_genesis_config(vec![identity_1, identity_2, identity_3]),  // three GC members
         "polymesh_committee": committee_genesis_config((2, 3), identity_1), // RC = 1, 2/3 votes required
         // CDD providers
-        "cdd_service_providers": group_genesis_config(vec![identity_1]), // GC_1 is also a CDD provider
+        "cddServiceProviders": group_genesis_config(vec![identity_1]), // GC_1 is also a CDD provider
         // Technical Committee
-        "technical_committee_membership": group_genesis_config(vec![identity_1]), // One GC member
-        "technical_committee": committee_genesis_config((1, 2), identity_1), // 1/2 votes required
+        "technicalCommitteeMembership": group_genesis_config(vec![identity_1]), // One GC member
+        "technicalCommittee": committee_genesis_config((1, 2), identity_1), // 1/2 votes required
         // Upgrade Committee
-        "upgrade_committee_membership": group_genesis_config(vec![identity_1]), // One GC member
-        "upgrade_committee": committee_genesis_config((1, 2), identity_1), // 1/2 votes required
-        "protocol_fee": protocol_fee_genesis_config(),
-        "corporate_action": corporate_actions_genesis_config(),
-        "polymesh_contracts": polymesh_contracts_genesis_config(root_key),
+        "upgradeCommitteeMembership": group_genesis_config(vec![identity_1]), // One GC member
+        "upgradeCommittee": committee_genesis_config((1, 2), identity_1), // 1/2 votes required
+        "protocolFee": protocol_fee_genesis_config(),
+        "corporateAction": corporate_actions_genesis_config(),
+        "polymeshContracts": polymesh_contracts_genesis_config(root_key),
     })
 }
 
