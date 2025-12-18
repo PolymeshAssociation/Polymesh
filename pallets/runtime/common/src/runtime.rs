7e362314dce637b9a298be5ef80b3aa6e622c858
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-12-18 21:30:10 +0800

    Add migration for SessionKeys.  Allow Beefy to be disabled.

--- pallets/runtime/common/src/runtime.rs
+++ pallets/runtime/common/src/runtime.rs
@@ -107,6 +107,7 @@ macro_rules! misc_pallet_impls {
             type MaxConsumers = frame_support::traits::ConstU32<16>;
             /// The set code logic, just the default since we're not a parachain.
             type SingleBlockMigrations = (
+                UpgradeSessionKeys,
                 pallet_contracts::Migration<Runtime>,
                 pallet_grandpa::migrations::MigrateV4ToV5<Runtime>,
                 pallet_im_online::migration::v1::Migration<Runtime>,
@@ -275,12 +276,55 @@ macro_rules! misc_pallet_impls {
         }
 
         impl_opaque_keys! {
+            pub struct OldSessionKeys {
+                pub grandpa: Grandpa,
+                pub babe: Babe,
+                pub im_online: ImOnline,
+                pub authority_discovery: AuthorityDiscovery,
+            }
+        }
+
+        impl_opaque_keys! {
             pub struct SessionKeys {
                 pub grandpa: Grandpa,
                 pub babe: Babe,
                 pub im_online: ImOnline,
                 pub authority_discovery: AuthorityDiscovery,
                 pub beefy: Beefy,
+            }
+        }
+
+        // remove this when removing `OldSessionKeys`
+        fn transform_session_keys(v: polymesh_primitives::AccountId, old: OldSessionKeys) -> SessionKeys {
+            SessionKeys {
+                grandpa: old.grandpa,
+                babe: old.babe,
+                im_online: old.im_online,
+                authority_discovery: old.authority_discovery,
+                beefy: {
+                    // From Session::upgrade_keys():
+                    //
+                    // Care should be taken that the raw versions of the
+                    // added keys are unique for every `ValidatorId, KeyTypeId` combination.
+                    // This is an invariant that the session pallet typically maintains internally.
+                    //
+                    // So, produce a dummy value that's unique for the `ValidatorId, KeyTypeId` combination.
+                    let mut id: BeefyId = sp_core::ecdsa::Public::from_raw([0u8; 33]).into();
+                    let id_raw: &mut [u8] = id.as_mut();
+                    id_raw[1..33].copy_from_slice(v.as_ref());
+                    id_raw[0..4].copy_from_slice(b"beef");
+                    id
+                },
+            }
+        }
+
+        /// Upgrade Session keys to include BEEFY key.
+        /// When this is removed, should also remove `OldSessionKeys`.
+        pub struct UpgradeSessionKeys;
+        impl frame_support::traits::OnRuntimeUpgrade for UpgradeSessionKeys {
+            fn on_runtime_upgrade() -> Weight {
+                Session::upgrade_keys::<OldSessionKeys, _>(transform_session_keys);
+                Perbill::from_percent(50) * polymesh_runtime_common::RuntimeBlockWeights::get().max_block
             }
         }
 
