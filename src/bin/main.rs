2ef49f1fcd4bafbc41b1ed0398f05f0688459316
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2026-01-09 21:18:38 +0800

    Add Revive/EVM support.
    Give ETH dev keys some POLYX on dev chain.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,8 @@
 //! Polymesh Node CLI binary.
 
 fn main() -> sc_cli::Result<()> {
-    polymesh::command::run()
+    let args = std::env::args_os()
+        .map(|s| s.to_string_lossy().to_string())
+        .collect::<Vec<_>>();
+    polymesh::command::run_with_args(args)
 }
