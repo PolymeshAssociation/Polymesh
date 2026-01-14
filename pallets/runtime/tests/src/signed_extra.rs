29e8b1d79d05b69f59361c004220e56aa032415a
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2026-01-15 01:16:31 +0800

    Update transaction fees for Revive's WeightToFee impl.

--- pallets/runtime/tests/src/signed_extra.rs
+++ pallets/runtime/tests/src/signed_extra.rs
@@ -197,7 +197,7 @@ fn operational_tx() -> Result<(), String> {
             TransactionSource::InBlock,
         )
         .expect("Tx should be valid");
-    assert_eq!(tx_validity.0.priority as u128, 162794569728);
+    assert_eq!(tx_validity.0.priority as u128, 162789326848);
 
     // Operational TX without any tip.
     let sign_extra = make_signed_extra(0, 10, 0, 0u128.into());
@@ -212,6 +212,6 @@ fn operational_tx() -> Result<(), String> {
             TransactionSource::InBlock,
         )
         .expect("Tx should be valid");
-    assert_eq!(tx_validity.0.priority, 162530328576);
+    assert_eq!(tx_validity.0.priority, 162525085696);
     Ok(())
 }
