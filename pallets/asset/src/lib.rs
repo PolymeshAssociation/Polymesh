cc372ef9e66a64a44edb73ae8b10436ca4916cff
Tugay Emin <tgntrr@gmail.com>
2026-03-26 13:45:59 +0200

    reject self transfers in controller_transfer

--- pallets/asset/src/lib.rs
+++ pallets/asset/src/lib.rs
@@ -2210,6 +2210,10 @@ impl<T: AssetConfig> Pallet<T> {
             false,
         )?;
 
+        let sender_did = pallet_identity::Pallet::<T>::asset_holder_did(&sender)?;
+        let holder_did = pallet_identity::Pallet::<T>::asset_holder_did(&caller_holding_ctx)?;
+        ensure!(sender_did != holder_did, Error::<T>::SenderSameAsReceiver);
+
         Self::validate_asset_transfer(
             asset_id,
             &sender,
@@ -2218,8 +2222,6 @@ impl<T: AssetConfig> Pallet<T> {
             true,
             weight_meter,
         )?;
-
-        let holder_did = pallet_identity::Pallet::<T>::asset_holder_did(&caller_holding_ctx)?;
         Self::unverified_transfer_asset(
             sender.clone(),
             caller_holding_ctx,
