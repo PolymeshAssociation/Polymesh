fd0919e12277c755686d1c8c0e69ba38f7506d50
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2026-07-29 21:46:43 +0800

    Remove old example Ink! contracts.

--- contracts/delegator/build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-cargo +nightly contract build $* --manifest-path accumulator/Cargo.toml
-cargo +nightly contract build $* --manifest-path adder/Cargo.toml
-cargo +nightly contract build $* --manifest-path subber/Cargo.toml
-cargo +nightly contract build $*
