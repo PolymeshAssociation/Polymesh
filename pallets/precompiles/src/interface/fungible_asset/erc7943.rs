df4f67fd19dc041bc275ac850587618fb9bb8901
Henrique Nogara <nogara.henrique@gmail.com>
2026-08-26 16:50:41 -0300

    Move fungible asset interface

---
+++ pallets/precompiles/src/interface/fungible_asset/erc7943.rs
@@ -0,0 +1,223 @@
+// This file is part of the Polymesh distribution (https://github.com/PolymeshAssociation/Polymesh).
+// Copyright (c) 2020 Polymesh Association
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+use alloc::vec::Vec;
+
+use pallet_revive::precompiles::alloy::sol_types::SolCall;
+use pallet_revive::precompiles::Error;
+use pallet_revive::precompiles::Ext;
+
+use pallet_asset::WeightInfo;
+use polymesh_precompiles::{IFungibleAsset, IFungibleAssetEvents};
+use polymesh_primitives::asset::{AssetHolderKind, AssetId};
+use polymesh_primitives::WeightMeter;
+
+use crate::common::Common;
+use crate::interface::FungibleAssetInterface;
+use crate::Config;
+
+impl<T: Config> FungibleAssetInterface<T> {
+    /// Checks if a transfer is possible according to token rules. It includes compliance checks.
+    pub(crate) fn can_transfer(
+        asset_id: AssetId,
+        call: &IFungibleAsset::canTransferCall,
+        env: &mut impl Ext<T = T>,
+    ) -> Result<Vec<u8>, Error> {
+        let transfer_report_worst_case_weight =
+            <T as pallet_asset::Config>::WeightInfo::asset_transfer_report_worst_case();
+        let charged = env.charge(transfer_report_worst_case_weight)?;
+
+        let from = Common::<T>::asset_holder(call.from)?;
+        let to = Common::<T>::asset_holder(call.to)?;
+
+        let mut weight_meter = WeightMeter::max_limit_no_minimum();
+        let errors = pallet_asset::Pallet::<T>::asset_transfer_report(
+            &from,
+            &to,
+            &asset_id,
+            Common::<T>::to_balance(call.value)?,
+            false,
+            &mut weight_meter,
+        );
+
+        let transfer_report_weight =
+            <T as pallet_asset::Config>::WeightInfo::asset_transfer_report_best_case();
+        let compliance_and_statistics_weight = weight_meter.consumed();
+        let real_consumed_weight =
+            transfer_report_weight.saturating_add(compliance_and_statistics_weight);
+
+        if real_consumed_weight.ref_time() < transfer_report_worst_case_weight.ref_time() {
+            env.adjust_gas(charged, real_consumed_weight);
+        }
+
+        Ok(IFungibleAsset::canTransferCall::abi_encode_returns(
+            &errors.is_empty(),
+        ))
+    }
+
+    /// Takes tokens from one address and transfers them to the caller's account.
+    pub(crate) fn forced_transfer(
+        asset_id: AssetId,
+        call: &IFungibleAsset::forcedTransferCall,
+        env: &mut impl Ext<T = T>,
+    ) -> Result<Vec<u8>, Error> {
+        let caller = Common::<T>::caller(env)?;
+        let source = Common::<T>::asset_holder(call.from)?;
+        let value = Common::<T>::to_balance(call.amount)?;
+
+        Common::<T>::call_runtime(
+            env,
+            caller.runtime_origin(),
+            pallet_asset::Call::<T>::controller_transfer {
+                asset_id,
+                value,
+                source,
+                destination_kind: AssetHolderKind::Account,
+            },
+        )?;
+
+        Common::<T>::deposit_event(
+            env,
+            IFungibleAssetEvents::ForcedTransfer(IFungibleAsset::ForcedTransfer {
+                from: call.from.into(),
+                to: caller.address.0.into(),
+                amount: call.amount,
+            }),
+        )?;
+
+        Ok(IFungibleAsset::forcedTransferCall::abi_encode_returns(
+            &true,
+        ))
+    }
+
+    /// Freezes a specific amount of tokens for a given account.
+    pub(crate) fn set_frozen_tokens(
+        asset_id: AssetId,
+        call: &IFungibleAsset::setFrozenTokensCall,
+        env: &mut impl Ext<T = T>,
+    ) -> Result<Vec<u8>, Error> {
+        let caller = Common::<T>::caller(env)?;
+        let acc_to_freeze = Common::<T>::asset_holder(call.account)?;
+        let amount = Common::<T>::to_balance(call.amount)?;
+
+        Common::<T>::call_runtime(
+            env,
+            caller.runtime_origin(),
+            pallet_asset::Call::<T>::set_frozen_tokens {
+                asset_id,
+                asset_holder: acc_to_freeze,
+                amount,
+            },
+        )?;
+
+        Common::<T>::deposit_event(
+            env,
+            IFungibleAssetEvents::Frozen(IFungibleAsset::Frozen {
+                account: call.account.into(),
+                amount: call.amount,
+            }),
+        )?;
+
+        Ok(IFungibleAsset::setFrozenTokensCall::abi_encode_returns(
+            &true,
+        ))
+    }
+
+    /// Returns the amount of frozen tokens for a given account.
+    pub(crate) fn get_frozen_tokens(
+        asset_id: AssetId,
+        call: &IFungibleAsset::getFrozenTokensCall,
+        env: &mut impl Ext<T = T>,
+    ) -> Result<Vec<u8>, Error> {
+        env.charge(<T as pallet_asset::Config>::WeightInfo::get_holders_frozen_balance())?;
+
+        let from = Common::<T>::asset_holder(call.account)?;
+
+        let frozen_tokens = pallet_asset::Pallet::<T>::get_holders_frozen_balance(&from, &asset_id);
+        let frozen_tokens = Common::<T>::to_u256(frozen_tokens)?;
+
+        Ok(IFungibleAsset::getFrozenTokensCall::abi_encode_returns(
+            &frozen_tokens,
+        ))
+    }
+
+    /// Returns `true` if the account is allowed to send tokens according to token rules.
+    pub(crate) fn can_send(
+        asset_id: AssetId,
+        call: &IFungibleAsset::canSendCall,
+        env: &mut impl Ext<T = T>,
+    ) -> Result<Vec<u8>, Error> {
+        let transfer_is_allowed_for_holder_worst_case_weight =
+            <T as pallet_asset::Config>::WeightInfo::transfer_is_allowed_for_holder_worst_case();
+        let charged = env.charge(transfer_is_allowed_for_holder_worst_case_weight)?;
+
+        let sender = Common::<T>::asset_holder(call.account)?;
+
+        let mut weight_meter = WeightMeter::max_limit_no_minimum();
+        let allowed = pallet_asset::Pallet::<T>::transfer_is_allowed_for_holder(
+            &sender,
+            &asset_id,
+            true,
+            &mut weight_meter,
+        );
+
+        let transfer_is_allowed_weight =
+            <T as pallet_asset::Config>::WeightInfo::transfer_is_allowed_for_holder_best_case();
+        let compliance_weight = weight_meter.consumed();
+        let real_consumed_weight = transfer_is_allowed_weight.saturating_add(compliance_weight);
+
+        if real_consumed_weight.ref_time()
+            < transfer_is_allowed_for_holder_worst_case_weight.ref_time()
+        {
+            env.adjust_gas(charged, real_consumed_weight);
+        }
+
+        Ok(IFungibleAsset::canSendCall::abi_encode_returns(&allowed))
+    }
+
+    /// Returns `true` if the account is allowed to receive tokens according to token rules.
+    pub(crate) fn can_receive(
+        asset_id: AssetId,
+        call: &IFungibleAsset::canReceiveCall,
+        env: &mut impl Ext<T = T>,
+    ) -> Result<Vec<u8>, Error> {
+        let transfer_is_allowed_for_holder_worst_case_weight =
+            <T as pallet_asset::Config>::WeightInfo::transfer_is_allowed_for_holder_worst_case();
+        let charged = env.charge(transfer_is_allowed_for_holder_worst_case_weight)?;
+
+        let receiver = Common::<T>::asset_holder(call.account)?;
+
+        let mut weight_meter = WeightMeter::max_limit_no_minimum();
+        let allowed = pallet_asset::Pallet::<T>::transfer_is_allowed_for_holder(
+            &receiver,
+            &asset_id,
+            false,
+            &mut weight_meter,
+        );
+
+        let transfer_is_allowed_weight =
+            <T as pallet_asset::Config>::WeightInfo::transfer_is_allowed_for_holder_best_case();
+        let compliance_weight = weight_meter.consumed();
+        let real_consumed_weight = transfer_is_allowed_weight.saturating_add(compliance_weight);
+
+        if real_consumed_weight.ref_time()
+            < transfer_is_allowed_for_holder_worst_case_weight.ref_time()
+        {
+            env.adjust_gas(charged, real_consumed_weight);
+        }
+
+        Ok(IFungibleAsset::canReceiveCall::abi_encode_returns(&allowed))
+    }
+}
---
+++ pallets/precompiles/src/interface/fungible_asset/mod.rs
@@ -0,0 +1,161 @@
+// This file is part of the Polymesh distribution (https://github.com/PolymeshAssociation/Polymesh).
+// Copyright (c) 2020 Polymesh Association
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+//! Polymesh ERC20 Precompile
+//!
+//! Routes ABI-encoded function calls to domain modules for ERC20 operations.
+
+use alloc::vec::Vec;
+use core::marker::PhantomData;
+use core::num::NonZero;
+
+use frame_support::traits::Get;
+use pallet_revive::precompiles::{AddressMatcher, Error, Ext, Precompile};
+use pallet_revive::H160;
+
+use polymesh_precompiles::{IFungibleAssetCalls, FUNGIBLE_ASSET_CODE};
+use polymesh_primitives::asset::AssetId;
+
+use crate::common::{revert, revert_err, Common};
+use crate::Config;
+
+mod erc20;
+mod erc3643;
+mod erc7943;
+mod polymesh_specific;
+
+// ==================== Error Messages ====================
+pub(crate) const ERR_ASSET_NOT_FOUND: &str = "Asset not found";
+pub(crate) const ERR_ASSET_NOT_FUNGIBLE: &str = "Asset is not fungible";
+pub(crate) const ERR_INST_NOT_EXECUTED: &str = "Instruction was not executed; Most likely the instruction is missing an affirmation from the receiver/mediator";
+pub(crate) const ERR_INVALID_SYMBOL: &str = "Invalid symbol; Ticker is too long";
+// ========================================================
+
+pub const DECIMALS: u8 = 6;
+
+/// All precompile calls exposed by the Polymesh runtime.
+pub struct FungibleAssetInterface<T>(PhantomData<T>);
+
+impl<T: Config> Precompile for FungibleAssetInterface<T> {
+    type T = T;
+    type Interface = IFungibleAssetCalls;
+
+    const MATCHER: AddressMatcher = AddressMatcher::VarPrefix {
+        id: NonZero::new(8).unwrap(),
+        data_bytes: 16,
+    };
+    const HAS_CONTRACT_INFO: bool = false;
+    const CODE: &[u8] = FUNGIBLE_ASSET_CODE;
+
+    fn call(
+        address: &[u8; 20],
+        input: &Self::Interface,
+        env: &mut impl Ext<T = Self::T>,
+    ) -> Result<Vec<u8>, Error> {
+        Common::<T>::ensure_direct_call(env)?;
+
+        let asset_id = Self::asset_id_from_address(address, env)?;
+        let contract_addr = H160::from(*address);
+
+        match input {
+            // State-changing calls - check read-only
+            IFungibleAssetCalls::transfer(_)
+            | IFungibleAssetCalls::mint(_)
+            | IFungibleAssetCalls::approve(_)
+            | IFungibleAssetCalls::transferFrom(_)
+            | IFungibleAssetCalls::burn(_)
+            | IFungibleAssetCalls::permit(_)
+            | IFungibleAssetCalls::forcedTransfer(_)
+            | IFungibleAssetCalls::setFrozenTokens(_)
+            | IFungibleAssetCalls::setName(_)
+            | IFungibleAssetCalls::setSymbol(_)
+            | IFungibleAssetCalls::pause(_)
+            | IFungibleAssetCalls::unpause(_)
+            | IFungibleAssetCalls::setAddressFrozen(_)
+                if env.is_read_only() =>
+            {
+                Err(Common::<T>::state_change_denied())
+            }
+
+            // ERC20 functions
+            IFungibleAssetCalls::transfer(call) => Self::transfer(asset_id, call, env),
+            IFungibleAssetCalls::totalSupply(_) => Self::total_supply(asset_id, env),
+            IFungibleAssetCalls::balanceOf(call) => Self::balance_of(asset_id, call, env),
+            IFungibleAssetCalls::allowance(call) => Self::allowance(asset_id, call, env),
+            IFungibleAssetCalls::approve(call) => Self::approve(asset_id, call, env),
+            IFungibleAssetCalls::transferFrom(call) => Self::transfer_from(asset_id, call, env),
+
+            // ERC20Permit functions (EIP-2612)
+            IFungibleAssetCalls::permit(call) => Self::permit(asset_id, contract_addr, call, env),
+            IFungibleAssetCalls::nonces(call) => Self::nonces(contract_addr, call, env),
+            IFungibleAssetCalls::DOMAIN_SEPARATOR(_) => {
+                Self::domain_separator(asset_id, contract_addr, env)
+            }
+
+            // ERC20Metadata functions
+            IFungibleAssetCalls::name(_) => Self::name(asset_id, env),
+            IFungibleAssetCalls::symbol(_) => Self::symbol(asset_id, env),
+            IFungibleAssetCalls::decimals(_) => Self::decimals(asset_id, env),
+
+            // Polymesh-specific functions
+            IFungibleAssetCalls::mint(call) => Self::issue(asset_id, call, env),
+            IFungibleAssetCalls::burn(call) => Self::redeem(asset_id, call, env),
+
+            // ERC7943 functions
+            IFungibleAssetCalls::canTransfer(call) => Self::can_transfer(asset_id, call, env),
+            IFungibleAssetCalls::forcedTransfer(call) => Self::forced_transfer(asset_id, call, env),
+            IFungibleAssetCalls::canSend(call) => Self::can_send(asset_id, call, env),
+            IFungibleAssetCalls::canReceive(call) => Self::can_receive(asset_id, call, env),
+            IFungibleAssetCalls::getFrozenTokens(call) => {
+                Self::get_frozen_tokens(asset_id, call, env)
+            }
+            IFungibleAssetCalls::setFrozenTokens(call) => {
+                Self::set_frozen_tokens(asset_id, call, env)
+            }
+
+            // ERC3643 functions
+            IFungibleAssetCalls::pause(_) => Self::pause(asset_id, env),
+            IFungibleAssetCalls::unpause(_) => Self::unpause(asset_id, env),
+            IFungibleAssetCalls::setName(call) => Self::set_name(asset_id, call, env),
+            IFungibleAssetCalls::setSymbol(call) => Self::set_symbol(asset_id, call, env),
+            IFungibleAssetCalls::setAddressFrozen(call) => {
+                Self::set_address_frozen(asset_id, call, env)
+            }
+        }
+    }
+}
+
+impl<T: Config> FungibleAssetInterface<T> {
+    /// Returns the [`AssetId`] from the address.
+    pub(crate) fn asset_id_from_address(
+        address: &[u8; 20],
+        env: &mut impl Ext<T = T>,
+    ) -> Result<AssetId, Error> {
+        env.charge(<T as frame_system::Config>::DbWeight::get().reads(1))?;
+
+        let bytes: [u8; 16] = address[0..16].try_into().expect("slice is 16 bytes; qed");
+        let asset_id = AssetId::from_raw(bytes);
+
+        match pallet_asset::Assets::<T>::try_get(asset_id) {
+            Ok(asset_details) => {
+                if asset_details.asset_type.is_non_fungible() {
+                    return Err(revert(ERR_ASSET_NOT_FUNGIBLE));
+                }
+                Ok(asset_id)
+            }
+            Err(err) => Err(revert_err(err, ERR_ASSET_NOT_FOUND)),
+        }
+    }
+}
