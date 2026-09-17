68f55bd5fe759750dbc5a612986c99b96835c5c9
Henrique Nogara <nogara.henrique@gmail.com>
2026-09-17 09:11:28 -0300

    Add permissions_cost to create_multisig

--- pallets/multisig/src/lib.rs
+++ pallets/multisig/src/lib.rs
@@ -82,6 +82,7 @@ use sp_std::convert::TryFrom;
 use sp_std::prelude::*;
 
 use pallet_identity::Config as IdentityConfig;
+use pallet_identity::WeightInfo as IdentityWeightInfo;
 use pallet_permissions::with_call_metadata;
 use polymesh_primitives::multisig::{ProposalState, ProposalVoteCount};
 use polymesh_primitives::{
@@ -222,7 +223,13 @@ pub mod pallet {
         /// * `sigs_required` - Number of sigs required to process a multi-sig tx.
         /// * `permissions` - optional custom permissions.  Only the primary key can provide custom permissions.
         #[pallet::call_index(0)]
-        #[pallet::weight(<T as Config>::WeightInfo::create_multisig(signers.len() as u32))]
+        #[pallet::weight(
+            <T as Config>::WeightInfo::create_multisig(signers.len() as u32).saturating_add(
+                <T as IdentityConfig>::WeightInfo::permissions_cost_perms(
+                    &permissions.clone().unwrap_or(Permissions::empty()),
+                ),
+            ))
+        ]
         pub fn create_multisig(
             origin: OriginFor<T>,
             signers: BoundedVec<T::AccountId, T::MaxSigners>,
